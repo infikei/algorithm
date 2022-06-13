ac5bc44b6c6e0167b4620d00f774af901dd08c66
Infikei <hckhong@naver.com>
2022-06-13 23:52:36 +0900

    Update 2022-06-13

---
+++ baekjoon/boj_2476.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false); // C++와 C 두 표준 입출력 동기화를 해제한다.
+    cin.tie(NULL);                    // 입력과 출력이 묶여있는 것을 풀어준다.
+
+    int n, ans = 0;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        int a, b, c, temp = 0;
+        cin >> a >> b >> c;
+
+        if (a == b) {
+            if (b == c) {
+                temp = (10 + a) * 1000;
+            }
+            else {
+                temp = (10 + a) * 100;
+            }
+        }
+        else if (a == c || b == c) {
+            temp = (10 + c) * 100;
+        }
+        else {
+            temp = max(a, max(b, c)) * 100;
+        }
+
+        if (temp > ans) {
+            ans = temp;
+        }
+    }
+
+    cout << ans << "\n";
+
+    return 0;
+}
---
+++ baekjoon/boj_2576.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false); // C++와 C 두 표준 입출력 동기화를 해제한다.
+    cin.tie(NULL);                    // 입력과 출력이 묶여있는 것을 풀어준다.
+
+    int temp, ans_sum = 0, ans_min = 1000;
+
+    for (int i = 0; i < 7; i++) {
+        cin >> temp;
+        if (temp % 2 == 1) {
+            ans_sum += temp;
+            if (temp < ans_min) {
+                ans_min = temp;
+            }
+        }
+    }
+
+    if (ans_min == 1000) {
+        cout << -1 << "\n";
+    }
+    else {
+        cout << ans_sum << "\n";
+        cout << ans_min << "\n";
+    }
+
+    return 0;
+}
