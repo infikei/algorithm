8c318b59908316c354fbb8506ab251eba1a0bb01
Infikei <hckhong@naver.com>
2022-06-20 01:04:49 +0900

    Update 2022-06-19

---
+++ baekjoon/boj_1453.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false); // C++와 C 두 표준 입출력 동기화를 해제한다.
+    cin.tie(NULL);                    // 입력과 출력이 묶여있는 것을 풀어준다.
+
+    int N, ans = 0, temp = 0;
+    bool arr[101] = {false};
+
+    cin >> N;
+
+    for (int i = 0; i < N; i++) {
+        cin >> temp;
+        if (arr[temp]) {
+            ans++;
+        }
+        else {
+            arr[temp] = true;
+        }
+    }
+
+    cout << ans;
+
+    return 0;
+}
---
+++ baekjoon/boj_5532.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false); // C++와 C 두 표준 입출력 동기화를 해제한다.
+    cin.tie(NULL);                    // 입력과 출력이 묶여있는 것을 풀어준다.
+
+    int L, A, B, C, D;
+    cin >> L >> A >> B >> C >> D;
+
+    A = (A - 1) / C + 1;
+    B = (B - 1) / D + 1;
+
+    cout << (A >= B ? L - A : L - B) << "\n";
+
+    return 0;
+}
