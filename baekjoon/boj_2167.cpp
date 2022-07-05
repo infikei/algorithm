d5c452c90bbdc7a5cb809fe2fbe6c824830381e9
Infikei <hckhong@naver.com>
2022-07-06 05:33:02 +0900

    Update 2022-07-05

---
+++ baekjoon/boj_2167.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false); // C++와 C 두 표준 입출력 동기화를 해제한다.
+    cin.tie(NULL);                    // 입력과 출력이 묶여있는 것을 풀어준다.
+
+    int n, m;
+    cin >> n >> m;
+
+    int arr[301][301];
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            cin >> arr[i][j];
+        }
+    }
+
+    int arr2[301][301] = {0};
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            arr2[i][j] = arr[i][j] + arr2[i][j - 1] + arr2[i - 1][j] - arr2[i - 1][j - 1];
+        }
+    }
+
+    int k;
+    cin >> k;
+
+    for (int t = 0; t < k; t++) {
+        int i, j, x, y;
+        cin >> i >> j >> x >> y;
+
+        cout << arr2[x][y] - arr2[x][j - 1] - arr2[i - 1][y] + arr2[i - 1][j - 1] << "\n";
+    }
+
+    return 0;
+}
