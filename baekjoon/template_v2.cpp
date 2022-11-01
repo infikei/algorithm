7898bed46a40d50e6fe7c77a3b6fc0efb8ab4a8d
Infikei <hckhong@naver.com>
2022-11-01 19:51:29 +0900

    Update gitignore, template

---
+++ baekjoon/template_v2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+// #include <sstream>
+// #include <string>
+// #include <vector>
+// #include <algorithm>
+// #include <cmath>
+// #include <queue>
+// #include <stack>
+// #include <unordered_map>
+// #include <unordered_set>
+using namespace std;
+#define fastio ios_base::sync_with_stdio(false);cout.tie(NULL);cin.tie(NULL); // boj_15552.cpp
+// #define sz(v) ((int)v.size())
+// using uint = unsigned int;
+// using ll = long long;
+// using ull = unsigned long long;
+// using pii = pair<int, int>;
+// using iii = tuple<int, int, int>;
+
+int main() {
+    fastio;
+
+    return 0;
+}
