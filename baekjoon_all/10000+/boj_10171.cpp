2f12fc09ce8869ddc36c8b783947f5fc3a6d9881
Infikei <hckhong@naver.com>
2023-02-07 05:19:47 +0900

    Solve 2021-12-17 / Update 2023-02-07

--- baekjoon_all/10000+/boj_10171.cpp
+++ baekjoon_all/10000+/boj_10171.cpp
@@ -1,11 +1,14 @@
-#include <iostream>
+// Solve 2021-12-17
+// Update 2023-02-07
+
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-    cout << "\\    /\\" << endl;
-    cout << " )  ( ')" << endl;
-    cout << "(  /  )" << endl;
-    cout << " \\(__)|" << endl;
+    cout << "\\    /\\\n";
+    cout << " )  ( ')\n";
+    cout << "(  /  )\n";
+    cout << " \\(__)|\n";
 
     return 0;
 }
---
+++ baekjoon_all/10000+/boj_10172.cpp
@@ -0,0 +1,15 @@
+// Solve 2021-12-17
+// Update 2023-02-07
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cout << "|\\_/|\n";
+    cout << "|q p|   /}\n";
+    cout << "( 0 )\"\"\"\\\n";
+    cout << "|\"^\"`    |\n";
+    cout << "||_/=\\\\__|\n";
+
+    return 0;
+}
