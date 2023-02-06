df3575715ec2819ad8db135df5b63d052ace85c9
Infikei <hckhong@naver.com>
2023-02-07 04:47:54 +0900

    Solve 2022-06-02 / Update 2023-02-07

--- baekjoon_all/10000+/boj_10926_solve1.cpp
+++ baekjoon_all/10000+/boj_10926_solve1.cpp
@@ -1,13 +1,14 @@
-#include <iostream>
-#include <string>
+// Solve 2022-06-02
+// Update 2023-02-07
+
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
     string s;
-
     cin >> s;
     // ??!는 삼중 문자(삼중자, trigraph)로 사용되므로 회피 문자 \를 통해 회피한다.
-    cout << s << "?\?!" << endl;
+    cout << s << "?\?!\n";
 
     return 0;
 }
--- baekjoon_all/10000+/boj_10926_solve2.cpp
+++ baekjoon_all/10000+/boj_10926_solve2.cpp
@@ -1,12 +1,14 @@
-#include <iostream>
+// Solve 2022-06-02
+// Update 2023-02-07
+
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
     char s[51];
-
     cin >> s;
     // ??!는 삼중 문자(삼중자, trigraph)로 사용되므로 회피 문자 \를 통해 회피한다.
-    cout << s << "?\?!" << endl;
+    cout << s << "?\?!\n";
 
     return 0;
 }
