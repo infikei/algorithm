229662d1748f3abccb8a0160ca8b0fc412241a1a
Infikei <hckhong@naver.com>
2023-02-07 05:19:04 +0900

    Solve 2021-12-18 / Update 2023-02-07

--- baekjoon_all/10000+/boj_10430.cpp
+++ baekjoon_all/10000+/boj_10430.cpp
@@ -1,14 +1,16 @@
-#include <iostream>
+// Solve 2021-12-18
+// Update 2023-02-07
+
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
     int a, b, c;
-
     cin >> a >> b >> c;
-    cout << (a + b) % c << endl;
-    cout << (a % c + b % c) % c << endl;
-    cout << (a * b) % c << endl;
-    cout << ((a % c) * (b % c)) % c << endl;
+    cout << (a + b) % c << '\n';
+    cout << (a % c + b % c) % c << '\n';
+    cout << (a * b) % c << '\n';
+    cout << ((a % c) * (b % c)) % c << '\n';
 
     return 0;
 }
