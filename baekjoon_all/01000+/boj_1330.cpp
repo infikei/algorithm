e24c82bfb1db7886b8dd276810a2ecc05fb5d460
Infikei <hckhong@naver.com>
2023-02-07 05:27:52 +0900

    Solve 2021-12-18 / Update 2023-02-07

--- baekjoon_all/01000+/boj_1330.cpp
+++ baekjoon_all/01000+/boj_1330.cpp
@@ -1,11 +1,29 @@
-#include <iostream>
+// Solve 2021-12-18
+// Update 2023-02-07
+
+#include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    int a, b;
+#ifdef BOJ
+#define BOJTEST(x) ((void)0)
+#else
+#define BOJTEST(x) cout << "[Debug] " << #x << ':' << x << '\n'
+#endif
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL); // boj_15552.cpp
+#define SIZE(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
+#define INF (int)1e9
+#define LLINF (ll)4e18
+using ll = long long;
+using uint = unsigned int;
+using ull = unsigned long long;
 
+int main() {
+    FASTIO;
+
+    int a, b;
     cin >> a >> b;
-    cout << (a > b ? ">" : (a < b ? "<" : "==")) << endl;
+    cout << (a > b ? ">" : (a < b ? "<" : "==")) << '\n';
 
     return 0;
 }
