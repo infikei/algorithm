707a3e262102af0ab6d35f570ce254aa63f666de
Infikei <hckhong@naver.com>
2023-02-07 04:47:35 +0900

    Solve 2021-12-17 / Update 2023-02-07

--- baekjoon_all/02000+/boj_2557.cpp
+++ baekjoon_all/02000+/boj_2557.cpp
@@ -1,8 +1,11 @@
-#include <iostream>
+// Solve 2021-12-17
+// Update 2023-02-07
+
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-    cout << "Hello World!" << endl;
+    cout << "Hello World!\n";
 
     return 0;
 }
