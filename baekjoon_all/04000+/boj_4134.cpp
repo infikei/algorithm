f20180564702222c56234fac9f8139e5ca05f02b
Infikei <hckhong@naver.com>
2023-02-08 07:37:25 +0900

    Solve 2022-08-04 / Update 2023-02-08

--- baekjoon_all/04000+/boj_4134.cpp
+++ baekjoon_all/04000+/boj_4134.cpp
@@ -1,13 +1,31 @@
-#include <iostream>
-#include <cmath>
+// Solve 2022-08-04
+// Update 2023-02-08
+
+#include <bits/stdc++.h>
 using namespace std;
 
-bool isPrime(unsigned int N) {
-    if (N <= 1) {
+#ifdef BOJ
+#define BOJTEST(x) ((void)0)
+#else
+#define BOJTEST(x) cout << "[Debug] " << #x << ':' << x << '\n'
+#endif
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL); // boj_15552.cpp
+#define SIZE(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
+#define INF (int)1e9
+#define LLINF (ll)4e18
+using ll = long long;
+using uint = unsigned int;
+using ull = unsigned long long;
+
+bool is_prime(uint k) {
+    if (k <= 1) {
         return false;
     }
-    for (unsigned int i = 2; i <= sqrt(N); i++) {
-        if (N % i == 0) {
+
+    double i_end = sqrt(k);
+    for (uint i = 2; i <= i_end; i++) {
+        if (k % i == 0) {
             return false;
         }
     }
@@ -15,22 +33,18 @@ bool isPrime(unsigned int N) {
 }
 
 int main() {
-    ios_base::sync_with_stdio(false); // C++와 C 두 표준 입출력 동기화를 해제한다.
-    cout.tie(NULL);
-    cin.tie(NULL);                    // 입력과 출력이 묶여있는 것을 풀어준다.
+    FASTIO;
 
-    int T;
-    cin >> T;
+    int t;
+    cin >> t;
 
-    for (int t = 0; t < T; t++) {
-        unsigned int N;
-        cin >> N;
-
-        while (!isPrime(N)) {
-            N++;
+    for (int ti = 0; ti < t; ti++) {
+        uint n;
+        cin >> n;
+        while (!is_prime(n)) {
+            n++;
         }
-
-        cout << N << '\n';
+        cout << n << '\n';
     }
 
     return 0;
