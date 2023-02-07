d778ef40b76d802a0938e3f4c94b98ff7ce4aff7
Infikei <hckhong@naver.com>
2023-02-08 07:38:39 +0900

    Solve 2022-11-06 / Update 2023-02-08

--- baekjoon_all/04000+/boj_4299.cpp
+++ baekjoon_all/04000+/boj_4299.cpp
@@ -1,25 +1,36 @@
-#include <iostream>
+// Solve 2022-11-06
+// Update 2023-02-08
+
+#include <bits/stdc++.h>
 using namespace std;
-#define fastio ios_base::sync_with_stdio(false);cout.tie(NULL);cin.tie(NULL); // boj_15552.cpp
+
+#ifdef BOJ
+#define BOJTEST(x) ((void)0)
+#else
+#define BOJTEST(x) cout << "[Debug] " << #x << ':' << x << '\n'
+#endif
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL); // boj_15552.cpp
+#define SIZE(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
+#define INF (int)1e9
+#define LLINF (ll)4e18
+using ll = long long;
+using uint = unsigned int;
+using ull = unsigned long long;
 
 int main() {
-    fastio;
+    FASTIO;
 
-    int sum, diff;
+    int sum, diff, score[2];
     cin >> sum >> diff;
 
-    int ans[2];
-    ans[0] = (sum + diff) / 2;
-    ans[1] = (sum - diff) / 2;
-
-    if (ans[0] < 0 || ans[1] < 0) {
-        cout << -1 << '\n';
-    }
-    else if (ans[0] + ans[1] != sum || ans[0] - ans[1] != diff) {
+    score[0] = (sum + diff) / 2;
+    score[1] = (sum - diff) / 2;
+    if (score[0] < 0 || score[1] < 0 || score[0] + score[1] != sum || score[0] - score[1] != diff) {
         cout << -1 << '\n';
     }
     else {
-        cout << ans[0] << ' ' << ans[1] << '\n';
+        cout << score[0] << ' ' << score[1] << '\n';
     }
 
     return 0;
