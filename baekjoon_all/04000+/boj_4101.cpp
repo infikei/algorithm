5307e082808ef1bf6a68a8675cf9dd6f2a3f242b
Infikei <hckhong@naver.com>
2023-02-08 07:36:49 +0900

    Solve 2022-09-14 / Update 2023-02-08

--- baekjoon_all/04000+/boj_4101.cpp
+++ baekjoon_all/04000+/boj_4101.cpp
@@ -1,25 +1,33 @@
-#include <iostream>
+// Solve 2022-09-14
+// Update 2023-02-08
+
+#include <bits/stdc++.h>
 using namespace std;
 
+#ifdef BOJ
+#define BOJTEST(x) ((void)0)
+#else
+#define BOJTEST(x) cout << "[Debug] " << #x << ':' << x << '\n'
+#endif
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL); // boj_15552.cpp
+#define SIZE(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
+#define INF (int)1e9
+#define LLINF (ll)4e18
+using ll = long long;
+using uint = unsigned int;
+using ull = unsigned long long;
+
 int main() {
-    ios_base::sync_with_stdio(false); // C++와 C 두 표준 입출력 동기화를 해제한다.
-    cout.tie(NULL);
-    cin.tie(NULL);                    // 입력과 출력이 묶여있는 것을 풀어준다.
+    FASTIO;
 
+    int a, b;
     while (true) {
-        int x, y;
-        cin >> x >> y;
-
-        if (x == 0 && y == 0) {
+        cin >> a >> b;
+        if (a == 0 && b == 0) {
             break;
         }
-
-        if (x > y) {
-            cout << "Yes\n";
-        }
-        else {
-            cout << "No\n";
-        }
+        cout << (a > b ? "Yes\n" : "No\n");
     }
 
     return 0;
