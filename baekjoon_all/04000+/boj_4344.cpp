246bb1901823f27ae12ff46102f3483f3fc87612
Infikei <hckhong@naver.com>
2023-02-08 07:34:23 +0900

    Solve 2022-06-03 / Update 2023-02-08

--- baekjoon_all/04000+/boj_4344.cpp
+++ baekjoon_all/04000+/boj_4344.cpp
@@ -1,38 +1,47 @@
-#include <iostream>
+// Solve 2022-06-03
+// Update 2023-02-08
+
+#include <bits/stdc++.h>
 using namespace std;
 
+#ifdef BOJ
+#define BOJTEST(x) ((void)0)
+#else
+#define BOJTEST(x) cout << "[Debug] " << #x << ':' << x << '\n'
+#endif
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL); // boj_15552.cpp
+#define SIZE(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
+#define SETPRECISION(n) cout << fixed << setprecision(n); // boj_1008.cpp (먼저 iomanip을 include해야 한다)
+#define INF (int)1e9
+#define LLINF (ll)4e18
+using ll = long long;
+using uint = unsigned int;
+using ull = unsigned long long;
+
 int main() {
-    ios_base::sync_with_stdio(false); // C++와 C 두 표준 입출력 동기화를 해제한다.
-    cout.tie(NULL);
-    cin.tie(NULL);                    // 입력과 출력이 묶여있는 것을 풀어준다.
+    FASTIO;
 
-    int c, n, sum;
-    int score[1000] = {0};
-    float average;
-    cin >> c;
+    int t, score[1000];
+    cin >> t;
 
-    cout << fixed;
-    cout.precision(3);
+    SETPRECISION(3);
 
-    for (int i = 0; i < c; i++) {
+    for (int ti = 0; ti < t; ti++) {
+        int n, sum = 0, cnt = 0;
         cin >> n;
-        sum = 0;
-
-        for (int j = 0; j < n; j++) {
-            cin >> score[j];
-            sum += score[j];
+        for (int i = 0; i < n; i++) {
+            cin >> score[i];
+            sum += score[i];
         }
 
-        average = (sum * 1.0) / n;
-        sum = 0;
-
-        for (int j = 0; j < n; j++) {
-            if (score[j] > average) {
-                sum++;
+        double avr = (sum * 1.0) / n;
+        for (int i = 0; i < n; i++) {
+            if (score[i] > avr) {
+                cnt++;
             }
         }
-
-        cout << ((sum * 100.0) / n) << "%\n";
+        cout << ((cnt * 100.0) / n) << "%\n";
     }
 
     return 0;
