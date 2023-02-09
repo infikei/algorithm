343936e16db84a1907de18457d480d2bfff43247
Infikei <hckhong@naver.com>
2023-02-10 05:04:33 +0900

    Solve 2022-12-08 / Update 2023-02-10

--- baekjoon_all/12000+/boj_12015.cpp
+++ baekjoon_all/12000+/boj_12015.cpp
@@ -1,51 +1,47 @@
-#include <iostream>
-#include <vector>
-#define fastio ios_base::sync_with_stdio(false);cout.tie(NULL);cin.tie(NULL); // boj_15552.cpp
+// Solve 2022-12-08
+// Update 2023-02-10
+
+#include <bits/stdc++.h>
 using namespace std;
 
-int N, arr[1000000], ans;
-vector<int> v;
+#ifdef BOJ
+#define BOJTEST(x) ((void)0)
+#else
+#define BOJTEST(x) cout << "[Debug] " << #x << ':' << x << '\n'
+#endif
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL); // boj_15552.cpp
+#define SIZE(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
+#define INF (int)1e9
+#define LLINF (ll)4e18
+using ll = long long;
+using uint = unsigned int;
+using ull = unsigned long long;
 
-void solve() {
-    v.push_back(arr[0]);
-    ans = 1;
-
-    for (int i = 1; i < N; i++) {
-        if (v[ans - 1] < arr[i]) {
-            v.push_back(arr[i]);
-            ans++;
-        }
-        else {
-            int low = 0, high = ans - 1, mid;
-            int res = 10000000;
-            while (low <= high) {
-                mid = (low + high) / 2;
-
-                if (arr[i] <= v[mid]) {
-                    high = mid - 1;
-                    res = min(res, mid);
-                }
-                else {
-                    low = mid + 1;
-                }
-            }
-
-            v[res] = arr[i];
-        }
-    }
-}
+const int N_MAX = 1000000;
+int n, arr[N_MAX];
+vector<int> dp;
 
 int main() {
-    fastio;
+    FASTIO;
 
-    cin >> N;
-    for (int i = 0; i < N; i++) {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
         cin >> arr[i];
     }
 
-    solve();
+    dp.push_back(arr[0]);
+    for (int i = 1; i < n; i++) {
+        int pos = lower_bound(ALL(dp), arr[i]) - dp.begin();
+        if (pos == SIZE(dp)) {
+            dp.push_back(arr[i]);
+        }
+        else if (arr[i] < dp[pos]) {
+            dp[pos] = arr[i];
+        }
+    }
 
-    cout << ans << '\n';
+    cout << SIZE(dp) << '\n';
 
     return 0;
 }
