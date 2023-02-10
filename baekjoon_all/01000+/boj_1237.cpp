0ed25783436d44df61f1fe5703dd5b99cd49bbc1
Infikei <hckhong@naver.com>
2023-02-10 21:01:18 +0900

    Solve 2022-06-06

--- baekjoon_all/01000+/boj_1237.cpp
+++ baekjoon_all/01000+/boj_1237.cpp
@@ -1,7 +1,26 @@
-#include <iostream>
+// Solve 2022-06-06
+
+#include <bits/stdc++.h>
 using namespace std;
 
+#ifdef BOJ
+#define BOJTEST(x) ((void)0)
+#else
+#define BOJTEST(x) cout << "[Debug] " << #x << ':' << x << '\n'
+#endif
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL); // boj_15552.cpp
+#define SIZE(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
+#define INF (int)1e9
+#define LLINF (ll)4e18
+using ll = long long;
+using uint = unsigned int;
+using ull = unsigned long long;
+
 int main() {
-    cout << "문제의 정답";
+    FASTIO;
+
+    cout << "문제의 정답\n";
+
     return 0;
 }
