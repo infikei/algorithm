5e724be05b85834ba26f8836aa8522294745304b
Infikei <hckhong@naver.com>
2023-02-11 02:26:32 +0900

    Solve 2022-10-01 / Update 2023-02-10

--- baekjoon_all/01000+/boj_1697.cpp
+++ baekjoon_all/01000+/boj_1697.cpp
@@ -1,60 +1,76 @@
-#include <iostream>
-#include <queue>
+// Solve 2022-10-01
+// Update 2023-02-10
+
+#include <bits/stdc++.h>
 using namespace std;
 
-int N, K;
-const int MAX = 100001;
-bool visited[MAX];
+#ifdef BOJ
+#define BOJTEST(x) ((void)0)
+#else
+#define BOJTEST(x) cout << "[Debug] " << #x << ':' << x << '\n'
+#endif
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL); // boj_15552.cpp
+#define SIZE(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
+#define INF (int)1e9
+#define LLINF (ll)4e18
+using ll = long long;
+using uint = unsigned int;
+using ull = unsigned long long;
 
-int bfs() {
-    int res = 0;
-    queue<int> q;
-    q.push(N);
-    q.push(-1);
+const int MAX_N = 100000;
+int n, k, ans;
+queue<int> q;
+bool visited[MAX_N + 1];
 
-    while (true) {
-        int now = q.front();
-        q.pop();
+void bfs() {
+    if (n == k) return;
 
-        if (now == K) {
-            return res;
-        }
+    q.push(n);
+    visited[n] = true;
 
-        if (now == -1) {
-            res++;
-            q.push(-1);
-            continue;
-        }
+    while (!q.empty()) {
+        ans++;
 
-        int next;
-        next = now - 1;
-        if (next >= 0 && !visited[next]) {
-            visited[next] = true;
-            q.push(next);
-        }
-        next = now + 1;
-        if (next < MAX && !visited[next]) {
-            visited[next] = true;
-            q.push(next);
-        }
-        next = now * 2;
-        if (next < MAX && !visited[next]) {
-            visited[next] = true;
-            q.push(next);
+        int i_end = SIZE(q);
+        for (int i = 0; i < i_end; i++) {
+            int cur = q.front();
+            q.pop();
+
+            int next;
+
+            next = cur * 2;
+            if (next == k) return;
+            if (next <= MAX_N && !visited[next]) {
+                visited[next] = true;
+                q.push(next);
+            }
+
+            next = cur + 1;
+            if (next == k) return;
+            if (next <= MAX_N && !visited[next]) {
+                visited[next] = true;
+                q.push(next);
+            }
+
+            next = cur - 1;
+            if (next == k) return;
+            if (next >= 0 && !visited[next]) {
+                visited[next] = true;
+                q.push(next);
+            }
         }
     }
-
-    return -1;
 }
 
 int main() {
-    ios_base::sync_with_stdio(false); // C++와 C 두 표준 입출력 동기화를 해제한다.
-    cout.tie(NULL);
-    cin.tie(NULL);                    // 입력과 출력이 묶여있는 것을 풀어준다.
+    FASTIO;
 
-    cin >> N >> K;
+    cin >> n >> k;
 
-    cout << bfs() << '\n';
+    bfs();
+
+    cout << ans << '\n';
 
     return 0;
 }
