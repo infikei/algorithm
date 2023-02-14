df75d699c6ecad5642b042316973c735cf7a0a80
Infikei <hckhong@naver.com>
2023-02-15 01:15:45 +0900

    Solve 2022-09-17 / Update 2023-02-14

--- baekjoon_all/01000+/boj_1717.cpp
+++ baekjoon_all/01000+/boj_1717.cpp
@@ -1,46 +1,56 @@
-#include <iostream>
+// Solve 2022-09-17
+// Update 2023-02-14
+
+#include <bits/stdc++.h>
 using namespace std;
 
-int N, M;
-int A[1000001];
+#ifdef BOJ
+#define BOJTEST(x) ((void)0)
+#else
+#define BOJTEST(x) cout << "[Debug] " << #x << ':' << x << '\n'
+#endif
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL); // boj_15552.cpp
+#define SIZE(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
+#define INF (int)1e9
+#define LLINF (ll)4e18
+using ll = long long;
+using uint = unsigned int;
+using ull = unsigned long long;
 
-int parent(int x) {
-    if (A[x] == x) {
-        return x;
+const int MAX_N = 1000000;
+int n, m, parent[MAX_N + 1];
+
+int get_parent(int k) {
+    if (parent[k] == k) {
+        return k;
     }
 
-    A[x] = parent(A[x]);
-    return A[x];
+    parent[k] = get_parent(parent[k]);
+    return parent[k];
 }
 
 int main() {
-    ios_base::sync_with_stdio(false); // C++와 C 두 표준 입출력 동기화를 해제한다.
-    cout.tie(NULL);
-    cin.tie(NULL);                    // 입력과 출력이 묶여있는 것을 풀어준다.
+    FASTIO;
 
-    cin >> N >> M;
+    cin >> n >> m;
 
-    for (int i = 0; i <= N; i++) {
-        A[i] = i;
+    for (int i = 0; i <= n; i++) {
+        parent[i] = i;
     }
 
-    for (int i = 0; i < M; i++) {
+    for (int i = 0; i < m; i++) {
         int cmd, a, b;
         cin >> cmd >> a >> b;
 
-        int a_parent = parent(a);
-        int b_parent = parent(b);
+        int pa = get_parent(a);
+        int pb = get_parent(b);
 
         if (cmd == 0) {
-            if (a_parent < b_parent) {
-                A[b_parent] = a_parent;
-            }
-            else {
-                A[a_parent] = b_parent;
-            }
+            parent[pa] = pb;
         }
         else {
-            if (a_parent == b_parent) {
+            if (pa == pb) {
                 cout << "YES\n";
             }
             else {
