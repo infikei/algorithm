31202bb46a278643c5bf467bc97684608c1e21a4
Infikei <hckhong@naver.com>
2023-02-15 01:19:03 +0900

    Solve 2022-07-18 / Update 2023-02-14

--- baekjoon_all/02000+/boj_2523.cpp
+++ baekjoon_all/02000+/boj_2523.cpp
@@ -1,27 +1,39 @@
-#include <iostream>
+// Solve 2022-07-18
+// Update 2023-02-14
+
+#include <bits/stdc++.h>
 using namespace std;
 
+#ifdef BOJ
+#define BOJTEST(x) ((void)0)
+#else
+#define BOJTEST(x) cout << "[Debug] " << #x << ':' << x << '\n'
+#endif
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL); // boj_15552.cpp
+#define SIZE(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
+#define INF (int)1e9
+#define LLINF (ll)4e18
+using ll = long long;
+using uint = unsigned int;
+using ull = unsigned long long;
+
 int main() {
-    ios_base::sync_with_stdio(false); // C++와 C 두 표준 입출력 동기화를 해제한다.
-    cout.tie(NULL);
-    cin.tie(NULL);                    // 입력과 출력이 묶여있는 것을 풀어준다.
+    FASTIO;
 
-    int N;
-    cin >> N;
+    int n;
+    cin >> n;
 
-    for (int i = 1; i <= N; i++) {
-        for (int j = 0; j < i; j++) {
-            cout << '*';
-        }
-        cout << '\n';
+    string tmp = "*";
+    for (int iter = n - 1; iter > 0; iter--) {
+        cout << tmp << '\n';
+        tmp += "*";
     }
-
-    for (int i = N - 1; i >= 1; i--) {
-        for (int j = 0; j < i; j++) {
-            cout << '*';
-        }
-        cout << '\n';
+    for (int iter = n - 1; iter > 0; iter--) {
+        cout << tmp << '\n';
+        tmp.pop_back();
     }
+    cout << tmp << '\n';
 
     return 0;
 }
