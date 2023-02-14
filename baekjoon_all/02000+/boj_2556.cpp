848b8dec702c696be659ce85811cf053942930ba
Infikei <hckhong@naver.com>
2023-02-15 01:19:30 +0900

    Solve 2022-11-08 / Update 2023-02-14

--- baekjoon_all/02000+/boj_2556.cpp
+++ baekjoon_all/02000+/boj_2556.cpp
@@ -1,20 +1,33 @@
-#include <iostream>
-#include <string>
+// Solve 2022-11-08
+// Update 2023-02-14
+
+#include <bits/stdc++.h>
 using namespace std;
-#define fastio ios_base::sync_with_stdio(false);cout.tie(NULL);cin.tie(NULL); // boj_15552.cpp
+
+#ifdef BOJ
+#define BOJTEST(x) ((void)0)
+#else
+#define BOJTEST(x) cout << "[Debug] " << #x << ':' << x << '\n'
+#endif
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL); // boj_15552.cpp
+#define SIZE(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
+#define INF (int)1e9
+#define LLINF (ll)4e18
+using ll = long long;
+using uint = unsigned int;
+using ull = unsigned long long;
 
 int main() {
-    fastio;
+    FASTIO;
 
-    int N;
-    cin >> N;
+    int n;
+    cin >> n;
 
-    string S;
-    for (int i = 0; i < N; i++) {
-        S += '*';
-    }
-    for (int i = 0; i < N; i++) {
-        cout << S << '\n';
+    string tmp;
+    tmp.assign(n, '*');
+    for (int row = 1; row <= n; row++) {
+        cout << tmp << '\n';
     }
 
     return 0;
