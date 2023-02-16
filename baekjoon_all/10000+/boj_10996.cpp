cb060828268844aeaa9414693a024508810ca526
Infikei <hckhong@naver.com>
2023-02-16 15:45:03 +0900

    Solve 2022-11-07 / Update 2023-02-15

--- baekjoon_all/10000+/boj_10996.cpp
+++ baekjoon_all/10000+/boj_10996.cpp
@@ -1,23 +1,42 @@
-#include <iostream>
+// Solve 2022-11-07
+// Update 2023-02-15
+
+#include <bits/stdc++.h>
 using namespace std;
-#define fastio ios_base::sync_with_stdio(false);cout.tie(NULL);cin.tie(NULL); // boj_15552.cpp
+
+#ifdef BOJ
+#define BOJTEST(x) ((void)0)
+#else
+#define BOJTEST(x) cout << "[Debug] " << #x << ':' << x << '\n'
+#endif
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL); // boj_15552.cpp
+#define SIZE(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
+#define INF (int)1e9
+#define LLINF (ll)4e18
+using ll = long long;
+using uint = unsigned int;
+using ull = unsigned long long;
 
 int main() {
-    fastio;
+    FASTIO;
 
-    int N;
-    cin >> N;
+    int n;
+    cin >> n;
 
-    for (int i = 0; i < N; i++) {
+    for (int i = 0; i < n; i++) {
         cout << '*';
-        for (int j = 0; j < (N - 1) / 2; j++) {
+        for (int j = (n - 1) / 2; j > 0; j--) {
             cout << " *";
         }
         cout << '\n';
-        for (int j = 0; j < N / 2; j++) {
-            cout << " *";
+
+        if (n > 1) {
+            for (int j = n / 2; j > 0; j--) {
+                cout << " *";
+            }
+            cout << '\n';
         }
-        cout << '\n';
     }
 
     return 0;
