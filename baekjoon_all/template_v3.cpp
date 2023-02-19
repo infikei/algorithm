e86808e761bfd90193a6476021112f51b14abd6a
Infikei <hckhong@naver.com>
2023-02-20 03:24:51 +0900

    템플릿에 "define SETPRECISION" 베타에서 정식으로 업데이트

--- baekjoon_all/template_v3.cpp
+++ baekjoon_all/template_v3.cpp
@@ -7,13 +7,13 @@ using namespace std;
 #define BOJTEST(x) cout << "[Debug] " << #x << ':' << x << '\n'
 #endif
 #define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL); // boj_15552.cpp
+#define SETPRECISION(n) cout << fixed;cout.precision(n); // boj_1008.cpp
+// #define SETPRECISION(n) cout << fixed << setprecision(n); // boj_1008.cpp (먼저 iomanip을 include해야 한다)
 #define SIZE(v) (int)v.size()
 #define ALL(v) v.begin(),v.end()
 // #define FOR(i,a,b) for(int i=(a);i<=(b);i++)
 // #define ROF(i,a,b) for(int i=(a);i>=(b);i--)
 // #define UNIQUE(v) v.erase(unique(v.begin(),v.end()),v.end()); // 정렬이 이미 되어있는 상태에서 중복 제거
-// #define SETPRECISION(n) cout << fixed;cout.precision(n); // boj_1008.cpp
-// #define SETPRECISION(n) cout << fixed << setprecision(n); // boj_1008.cpp (먼저 iomanip을 include해야 한다)
 #define INF (int)1e9
 #define LLINF (ll)4e18
 using ll = long long;
