684bd14a235108a70b4f7b7be3ffde49dd25c92f
Infikei <hckhong@naver.com>
2023-02-21 01:37:03 +0900

    유니온 파인드

---
+++ my_library/union_find.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef BOJ
+#define BOJTEST(x) ((void)0)
+#else
+#define BOJTEST(x) cout << "[Debug] " << #x << ':' << x << '\n'
+#endif
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL); // boj_15552.cpp
+#define SETPRECISION(n) cout << fixed;cout.precision(n); // boj_1008.cpp
+#define SIZE(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
+#define INF (int)1e9
+#define LLINF (ll)4e18
+using ll = long long;
+using uint = unsigned int;
+using ull = unsigned long long;
+
+const int MAX_N = 1000000;
+int n;
+int parent[MAX_N];
+
+int get_parent(int k) {
+    if (parent[k] < 0) {
+        return k;
+    }
+
+    parent[k] = get_parent(parent[k]);
+    return parent[k];
+}
+
+void union_parents(int u, int v) {
+    int pu = get_parent(u);
+    int pv = get_parent(v);
+
+    if (pu != pv) {
+        if (parent[pu] < parent[pv]) {
+            parent[pu] += parent[pv];
+            parent[pv] = pu;
+        }
+        else {
+            parent[pv] += parent[pu];
+            parent[pu] = pv;
+        }
+    }
+}
+
+int main() {
+    FASTIO;
+
+    for (int i = 0; i < n; i++) {
+        parent[i] = -1;
+    }
+
+    // main code
+
+    return 0;
+}
