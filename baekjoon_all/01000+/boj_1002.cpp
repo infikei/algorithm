a93340bae8af9f7cb8c7e58a6b6e8da15c9ff05f
Infikei <hckhong@naver.com>
2023-02-21 04:02:02 +0900

    Solve 2022-06-03 / Update 2023-02-21

--- baekjoon_all/01000+/boj_1002.cpp
+++ baekjoon_all/01000+/boj_1002.cpp
@@ -1,32 +1,49 @@
-#include <iostream>
-#include <cmath>
+// Solve 2022-06-03
+// Update 2023-02-21
+
+#include <bits/stdc++.h>
 using namespace std;
 
+#ifdef BOJ
+#define BOJTEST(x) ((void)0)
+#else
+#define BOJTEST(x) cout << "[Debug] " << #x << ':' << x << '\n'
+#endif
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL); // boj_15552.cpp
+#define SETPRECISION(n) cout << fixed;cout.precision(n); // boj_1008.cpp
+#define SIZE(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
+#define INF (int)1e9
+#define LLINF (ll)4e18
+using ll = long long;
+using uint = unsigned int;
+using ull = unsigned long long;
+
 int main() {
-    ios_base::sync_with_stdio(false); // C++와 C 두 표준 입출력 동기화를 해제한다.
-    cout.tie(NULL);
-    cin.tie(NULL);                    // 입력과 출력이 묶여있는 것을 풀어준다.
+    FASTIO;
 
-    int T;
-    cin >> T;
+    int t;
+    cin >> t;
 
-    int x1, y1, r1, x2, y2, r2, ans;
-
-    for (int t = 0; t < T; t++) {
+    for (int ti = 0; ti < t; ti++) {
+        int x1, y1, r1, x2, y2, r2, ans;
         cin >> x1 >> y1 >> r1 >> x2 >> y2 >> r2;
 
         if (x1 == x2 && y1 == y2 && r1 == r2) {
             ans = -1;
         }
         else {
-            double d = sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2));
-            int d1_min = abs(r1 - r2);
-            int d1_max = r1 + r2;
+            int dx = x1 - x2;
+            int dy = y1 - y2;
+            double d = sqrt(dx * dx + dy * dy);
 
-            if (d < d1_min || d > d1_max) {
+            int min_d = abs(r1 - r2);
+            int max_d = r1 + r2;
+
+            if (d < min_d || d > max_d) {
                 ans = 0;
             }
-            else if (d == d1_min || d == d1_max) {
+            else if (d == min_d || d == max_d) {
                 ans = 1;
             }
             else {
