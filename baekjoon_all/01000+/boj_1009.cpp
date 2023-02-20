ebe9158600fca6b68304402282600d26523e60b3
Infikei <hckhong@naver.com>
2023-02-21 04:02:24 +0900

    Solve 2022-06-06 / Update 2023-02-21

--- baekjoon_all/01000+/boj_1009.cpp
+++ baekjoon_all/01000+/boj_1009.cpp
@@ -1,17 +1,35 @@
-#include <iostream>
+// Solve 2022-06-06
+// Update 2023-02-21
+
+#include <bits/stdc++.h>
 using namespace std;
 
+#ifdef BOJ
+#define BOJTEST(x) ((void)0)
+#else
+#define BOJTEST(x) cout << "[Debug] " << #x << ':' << x << '\n'
+#endif
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL); // boj_15552.cpp
+#define SETPRECISION(n) cout << fixed;cout.precision(n); // boj_1008.cpp
+#define SIZE(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
+#define INF (int)1e9
+#define LLINF (ll)4e18
+using ll = long long;
+using uint = unsigned int;
+using ull = unsigned long long;
+
 int main() {
-    ios_base::sync_with_stdio(false); // C++와 C 두 표준 입출력 동기화를 해제한다.
-    cout.tie(NULL);
-    cin.tie(NULL);                    // 입력과 출력이 묶여있는 것을 풀어준다.
+    FASTIO;
 
-    int T, a, b, ans;
-    cin >> T;
+    int t;
+    cin >> t;
 
-    for (int t = 0; t < T; t++) {
-        ans = 1;
+    for (int ti = 0; ti < t; ti++) {
+        int a, b;
         cin >> a >> b;
+
+        int ans = 1;
         a = (a - 1) % 10 + 1;
         b = (b - 1) % 4 + 1;
         while (b--) {
