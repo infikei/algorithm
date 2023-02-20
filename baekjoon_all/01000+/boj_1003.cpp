b8634b4790c4a8d46c70b57faffd2ed3fa569b2e
Infikei <hckhong@naver.com>
2023-02-21 04:03:05 +0900

    Solve 2022-07-29 / Update 2023-02-21

--- baekjoon_all/01000+/boj_1003.cpp
+++ baekjoon_all/01000+/boj_1003.cpp
@@ -1,27 +1,41 @@
-#include <iostream>
-#include <vector>
+// Solve 2022-07-29
+// Update 2023-02-21
+
+#include <bits/stdc++.h>
 using namespace std;
 
-vector<pair<int, int> > DP;
+#ifdef BOJ
+#define BOJTEST(x) ((void)0)
+#else
+#define BOJTEST(x) cout << "[Debug] " << #x << ':' << x << '\n'
+#endif
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL); // boj_15552.cpp
+#define SETPRECISION(n) cout << fixed;cout.precision(n); // boj_1008.cpp
+#define SIZE(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
+#define INF (int)1e9
+#define LLINF (ll)4e18
+using ll = long long;
+using uint = unsigned int;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+
+int dp[42] = { 1, 0, 1 };
 
 int main() {
-    ios_base::sync_with_stdio(false); // C++와 C 두 표준 입출력 동기화를 해제한다.
-    cout.tie(NULL);
-    cin.tie(NULL);                    // 입력과 출력이 묶여있는 것을 풀어준다.
+    FASTIO;
 
-    DP.push_back(make_pair(1, 0));
-    DP.push_back(make_pair(0, 1));
-
-    for (int i = 2; i <= 40; i++) {
-        DP.push_back(make_pair(DP[i - 2].first + DP[i - 1].first, DP[i - 2].second + DP[i - 1].second));
+    for (int i = 3; i < 42; i++) {
+        dp[i] = dp[i - 2] + dp[i - 1];
     }
 
-    int T, N;
-    cin >> T;
-    for (int t = 0; t < T; t++) {
-        cin >> N;
-        cout << DP[N].first << ' ';
-        cout << DP[N].second << '\n';
+    int t;
+    cin >> t;
+
+    for (int ti = 0; ti < t; ti++) {
+        int n;
+        cin >> n;
+        cout << dp[n] <<' ' << dp[n + 1] << '\n';
     }
 
     return 0;
