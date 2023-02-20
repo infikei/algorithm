e84f75ed27d759f5f67614051a3f4088918f0626
Infikei <hckhong@naver.com>
2023-02-21 01:37:12 +0900

    최소 신장 트리

---
+++ my_library/minimum_spanning_tree_kruskal.cpp
@@ -0,0 +1,96 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef BOJ
+#define BOJTEST(x) ((void)0)
+#else
+#define BOJTEST(x) cout << "[Debug] " << #x << ':' << x << '\n'
+#endif
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL); // boj_15552.cpp
+#define SETPRECISION(n) cout << fixed;cout.precision(n); // boj_1008.cpp
+#define SIZE(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
+#define INF (int)1e9
+#define LLINF (ll)4e18
+using ll = long long;
+using uint = unsigned int;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using pi3 = pair<int, pii>;
+
+const int MAX_V = 1000000;
+int v, e;
+int parent[MAX_V];
+priority_queue<pi3, vector<pi3>, greater<pi3> > edges;
+
+int get_parent(int k) {
+    if (parent[k] < 0) {
+        return k;
+    }
+
+    parent[k] = get_parent(parent[k]);
+    return parent[k];
+}
+
+void union_parents(int u, int v) {
+    int pu = get_parent(u);
+    int pv = get_parent(v);
+
+    if (pu != pv) {
+        if (parent[pu] < parent[pv]) {
+            parent[pu] += parent[pv];
+            parent[pv] = pu;
+        }
+        else {
+            parent[pv] += parent[pu];
+            parent[pu] = pv;
+        }
+    }
+}
+
+int main() {
+    FASTIO;
+
+    // 여기에서는 크루스칼 알고리즘을 이용하였다.
+
+    cin >> v >> e;
+
+    for (int i = 0; i < e; i++) {
+        int from, to, dist;
+        cin >> from >> to >> dist;
+        edges.push({ dist, { from, to } });
+    }
+
+    for (int i = 0; i < v; i++) {
+        parent[i] = -1;
+    }
+
+    int ans = 0; // 최소 비용 신장 트리를 만들기 위한 비용
+
+    while (!edges.empty()) {
+        pi3 edge = edges.top();
+        edges.pop();
+
+        int u = edge.second.first;
+        int v = edge.second.second;
+
+        int pu = get_parent(u);
+        int pv = get_parent(v);
+
+        if (pu != pv) {
+            ans += edge.first;
+
+            // 시간 단축을 위해 node 개수가 더 많은 쪽으로 합쳐준다.
+            if (parent[pu] < parent[pv]) {
+                parent[pu] += parent[pv];
+                parent[pv] = pu;
+            }
+            else {
+                parent[pv] += parent[pu];
+                parent[pu] = pv;
+            }
+        }
+    }
+
+    return 0;
+}
---
+++ my_library/minimum_spanning_tree_prim.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef BOJ
+#define BOJTEST(x) ((void)0)
+#else
+#define BOJTEST(x) cout << "[Debug] " << #x << ':' << x << '\n'
+#endif
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL); // boj_15552.cpp
+#define SETPRECISION(n) cout << fixed;cout.precision(n); // boj_1008.cpp
+#define SIZE(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
+#define INF (int)1e9
+#define LLINF (ll)4e18
+using ll = long long;
+using uint = unsigned int;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+
+const int MAX_V = 1000000;
+int v, e;
+vector<pii> edges[MAX_V];
+int dists[MAX_V];
+bool visited[MAX_V];
+
+int main() {
+    FASTIO;
+
+    // 여기에서는 프림 알고리즘을 이용하였다.
+
+    cin >> v >> e;
+
+    for (int i = 0; i < e; i++) {
+        int from, to, dist;
+        cin >> from >> to >> dist;
+        edges[from].push_back({ to, dist });
+        edges[to].push_back({ from, dist });
+    }
+
+    for (int i = 0; i < v; i++) {
+        dists[i] = INF;
+    }
+
+    int ans = 0; // 최소 비용 신장 트리를 만들기 위한 비용
+    visited[0] = true;
+    for (auto edge : edges[0]) {
+        dists[edge.first] = edge.second;
+    }
+
+    for (int iter = v - 1; iter > 0; iter--) {
+        int min_dist = INF;
+        int min_idx = -1;
+
+        for (int i = 0; i < v; i++) {
+            if (visited[i]) continue;
+            if (min_dist > dists[i]) {
+                min_dist = dists[i];
+                min_idx = i;
+            }
+        }
+
+        ans += min_dist;
+        visited[min_idx] = true;
+
+        for (auto edge : edges[min_idx]) {
+            if (visited[edge.first]) continue;
+            if (dists[edge.first] > edge.second) {
+                dists[edge.first] = edge.second;
+            }
+        }
+    }
+
+    return 0;
+}
