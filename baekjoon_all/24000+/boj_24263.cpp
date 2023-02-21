282d838f354e04c156379df77fb7f014a259cddf
Infikei <hckhong@naver.com>
2023-02-22 08:10:21 +0900

    Solve 2022-11-13 / Update 2023-02-22

--- baekjoon_all/24000+/boj_24263.cpp
+++ baekjoon_all/24000+/boj_24263.cpp
@@ -1,12 +1,30 @@
-#include <iostream>
+// Solve 2022-11-13
+// Update 2023-02-22
+
+#include <bits/stdc++.h>
 using namespace std;
-#define fastio ios_base::sync_with_stdio(false);cout.tie(NULL);cin.tie(NULL); // boj_15552.cpp
+
+#ifdef BOJ
+#define BOJTEST(x) ((void)0)
+#else
+#define BOJTEST(x) cout << "[Debug] " << #x << ':' << x << '\n'
+#endif
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL); // boj_15552.cpp
+#define SETPRECISION(n) cout << fixed;cout.precision(n); // boj_1008.cpp
+#define SIZE(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
+#define INF (int)1e9
+#define LLINF (ll)4e18
+using ll = long long;
+using uint = unsigned int;
+using ull = unsigned long long;
 
 int main() {
-    fastio;
+    FASTIO;
 
     int n;
     cin >> n;
+
     cout << n << '\n';
     cout << 1 << '\n';
 
