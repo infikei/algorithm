aa708799a754bca0b39626f4648ea75274e40874
Infikei <hckhong@naver.com>
2023-02-22 21:19:46 +0900

    Solve 2022-05-28 / Update 2023-02-22

--- baekjoon_all/10000+/boj_10953.py
+++ baekjoon_all/10000+/boj_10953.py
@@ -1,2 +1,7 @@
-for t in range(int(input())):
-    print(sum(map(int, input().split(','))))
+# Solve 2022-05-28
+# Update 2023-02-22
+
+t = int(input())
+
+for ti in range(t):
+    print(sum(map(int, input().split(","))))
