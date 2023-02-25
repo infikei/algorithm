161f4fa0af26f52929aceddedbbbfdee00901f35
Infikei <hckhong@naver.com>
2023-02-25 18:18:27 +0900

    다익스트라 알고리즘

--- my_library/dijkstra_faster.cpp
+++ my_library/dijkstra_faster.cpp
@@ -1,15 +1,22 @@
-#include <iostream>
-#include <vector>
-#include <queue>
+#include <bits/stdc++.h>
 using namespace std;
 
-#define INF (int)1e9
-#define LLINF (ll)4e18
+#ifdef BOJ
+#define BOJTEST(x) ((void)0)
+#else
+#define BOJTEST(x) cout << "[Debug] " << #x << ':' << x << '\n'
+#endif
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL); // boj_15552.cpp
+#define SETPRECISION(n) cout << fixed;cout.precision(n); // boj_1008.cpp
+#define SIZE(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
 using ll = long long;
 using uint = unsigned int;
 using ull = unsigned long long;
 using pii = pair<int, int>;
 
+const int INF = 1e9;
+const ll LLINF = 4e18;
 int vertex, edge, start, finish;
 vector<vector<pii> > edges;
 vector<int> costs;
@@ -18,7 +25,7 @@ void dijkstra() {
     priority_queue<pii, vector<pii>, greater<pii> > pq_mintop;
     costs.assign(vertex, INF);
     costs[start] = 0;
-    pq_mintop.push({0, start});
+    pq_mintop.push({ 0, start });
 
     while (!pq_mintop.empty()) {
         int now = pq_mintop.top().second;
@@ -33,21 +40,23 @@ void dijkstra() {
             int next_cost = now_cost + edge.second;
             if (costs[next] > next_cost) {
                 costs[next] = next_cost;
-                pq_mintop.push({next_cost, next});
+                pq_mintop.push({ next_cost, next });
             }
         }
     }
 }
 
 int main() {
+    FASTIO;
+
     cin >> vertex >> edge >> start >> finish;
 
     edges.assign(vertex, vector<pii>());
     for (int i = 0; i < edge; i++) {
         int u, v, c;
         cin >> u >> v >> c;
-        edges[u].push_back({v, c});
-        edges[v].push_back({u, c}); // 양방향일 경우
+        edges[u].push_back({ v, c });
+        edges[v].push_back({ u, c }); // 양방향일 경우
     }
 
     dijkstra();
--- my_library/dijkstra_with_path.cpp
+++ my_library/dijkstra_with_path.cpp
@@ -1,15 +1,22 @@
-#include <iostream>
-#include <vector>
-#include <queue>
+#include <bits/stdc++.h>
 using namespace std;
 
-#define INF (int)1e9
-#define LLINF (ll)4e18
+#ifdef BOJ
+#define BOJTEST(x) ((void)0)
+#else
+#define BOJTEST(x) cout << "[Debug] " << #x << ':' << x << '\n'
+#endif
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL); // boj_15552.cpp
+#define SETPRECISION(n) cout << fixed;cout.precision(n); // boj_1008.cpp
+#define SIZE(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
 using ll = long long;
 using uint = unsigned int;
 using ull = unsigned long long;
 using pii = pair<int, int>;
 
+const int INF = 1e9;
+const ll LLINF = 4e18;
 int vertex, edge, start;
 vector<vector<pii> > edges;
 vector<int> costs;
@@ -19,7 +26,7 @@ void dijkstra(bool flag_parents = false) {
     priority_queue<pii, vector<pii>, greater<pii> > pq_mintop;
     costs.assign(vertex, INF);
     costs[start] = 0;
-    pq_mintop.push({0, start});
+    pq_mintop.push({ 0, start });
     if (flag_parents) parents.assign(vertex, vector<int>());
 
     while (!pq_mintop.empty()) {
@@ -33,7 +40,7 @@ void dijkstra(bool flag_parents = false) {
             int next_cost = now_cost + edge.second;
             if (costs[next] > next_cost) {
                 costs[next] = next_cost;
-                pq_mintop.push({next_cost, next});
+                pq_mintop.push({ next_cost, next });
                 if (flag_parents) {
                     parents[next].clear();
                     parents[next].push_back(now);
@@ -47,14 +54,16 @@ void dijkstra(bool flag_parents = false) {
 }
 
 int main() {
+    FASTIO;
+
     cin >> vertex >> edge >> start;
 
     edges.assign(vertex, vector<pii>());
     for (int i = 0; i < edge; i++) {
         int u, v, c;
         cin >> u >> v >> c;
-        edges[u].push_back({v, c});
-        edges[v].push_back({u, c}); // 양방향일 경우
+        edges[u].push_back({ v, c });
+        edges[v].push_back({ u, c }); // 양방향일 경우
     }
 
     dijkstra(true);
