d76b3a4af8fa4ccef3f29aa156d225416473a3c1
Infikei <hckhong@naver.com>
2023-02-25 16:46:06 +0900

    Update 2023-02-13

--- baekjoon_all/11000+/boj_11657.cpp
+++ baekjoon_all/11000+/boj_11657.cpp
@@ -10,16 +10,17 @@ using namespace std;
 #define BOJTEST(x) cout << "[Debug] " << #x << ':' << x << '\n'
 #endif
 #define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL); // boj_15552.cpp
+#define SETPRECISION(n) cout << fixed;cout.precision(n); // boj_1008.cpp
 #define SIZE(v) (int)v.size()
 #define ALL(v) v.begin(),v.end()
-#define INF (int)1e9
-#define LLINF (ll)4e18
 using ll = long long;
 using uint = unsigned int;
 using ull = unsigned long long;
 using pii = pair<int, int>;
 using pi3 = pair<int, pii>;
 
+const int INF = 1e9;
+const ll LLINF = 4e18;
 const int MAX_N = 500;
 int n, m;
 ll dists[MAX_N + 1]; // int로 할 경우에는 음수 범위에서 오버플로우가 발생하므로 long long으로 설정해야 한다.
