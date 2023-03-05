9a0d1e4aa8c91fc27cc8a1b255df46727255a4a1
Infikei <hckhong@naver.com>
2023-03-06 01:48:11 +0900

    고속 푸리에 변환 (FFT)

--- my_library/fft_version2.cpp
+++ my_library/fft_version2.cpp
@@ -1,11 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#ifdef BOJ
-#define BOJTEST(x) ((void)0)
-#else
-#define BOJTEST(x) cout << "[Debug] " << #x << ':' << x << '\n'
-#endif
 #define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL); // boj_15552.cpp
 #define SETPRECISION(n) cout << fixed;cout.precision(n); // boj_1008.cpp
 #define SIZE(v) (int)v.size()
