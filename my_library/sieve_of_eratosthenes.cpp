f1db7663688f7b4e9fc4498228a4eb69528d98b7
Infikei <hckhong@naver.com>
2023-03-06 01:46:13 +0900

    소수 판별 알고리즘 - 에라토스테네스의 체

--- my_library/sieve_of_eratosthenes.cpp
+++ my_library/sieve_of_eratosthenes.cpp
@@ -3,9 +3,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL); // boj_15552.cpp
+#define SETPRECISION(n) cout << fixed;cout.precision(n); // boj_1008.cpp
+#define SIZE(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
+using ll = long long;
+using uint = unsigned int;
+using ull = unsigned long long;
+
 const int MAX_SIZE = 10001;
 
 int main() {
+    FASTIO;
+
     vector<bool> primes(MAX_SIZE, true);
     primes[0] = primes[1] = false;
     for (int i = 2; i < MAX_SIZE; i++) {
