e7ced81c574729c23adb5e0d3758f8875622a135
Infikei <hckhong@naver.com>
2023-03-06 01:44:44 +0900

    소수 판별 알고리즘

--- my_library/is_prime.cpp
+++ my_library/is_prime.cpp
@@ -3,6 +3,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL); // boj_15552.cpp
+#define SETPRECISION(n) cout << fixed;cout.precision(n); // boj_1008.cpp
+#define SIZE(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
+using ll = long long;
+using uint = unsigned int;
+using ull = unsigned long long;
+
 bool is_prime(int k) {
     if (k < 2) {
         return false;
@@ -19,6 +27,8 @@ bool is_prime(int k) {
 }
 
 int main() {
+    FASTIO;
+
     cout << "수를 입력하세요 : ";
     int n;
     cin >> n;
