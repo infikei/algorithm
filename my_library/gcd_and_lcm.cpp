6151392cc5ec0bf6dee9dc99063176e31779eba3
Infikei <hckhong@naver.com>
2023-03-06 01:47:47 +0900

    최대공약수와 최소공배수

--- my_library/gcd_and_lcm.cpp
+++ my_library/gcd_and_lcm.cpp
@@ -3,6 +3,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL); // boj_15552.cpp
+#define SETPRECISION(n) cout << fixed;cout.precision(n); // boj_1008.cpp
+#define SIZE(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
+using ll = long long;
+using uint = unsigned int;
+using ull = unsigned long long;
+
 int calc_gcd(int a, int b) {
     if (a % b == 0) {
         return b;
@@ -15,6 +23,8 @@ int calc_lcm(int a, int b) {
 }
 
 int main() {
+    FASTIO;
+
     cout << "수를 입력하세요 : ";
     int a;
     cin >> a;
