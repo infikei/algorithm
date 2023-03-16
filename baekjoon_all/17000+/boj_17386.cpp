77df295afc9f65f931cf18bac913847c5045e761
Infikei <hckhong@naver.com>
2023-03-17 00:30:15 +0900

    Update 2023-03-16

--- baekjoon_all/17000+/boj_17386.cpp
+++ baekjoon_all/17000+/boj_17386.cpp
@@ -1,38 +1,41 @@
-#include <iostream>
+// Solve 2022-09-05
+// Update 2023-03-16
+
+#include <bits/stdc++.h>
 using namespace std;
 
-int ccw(long long x0, long long y0, long long x1, long long y1, long long x2, long long y2) {
-    long long ccw_check = (x1 - x0) * (y2 - y0) - (y1 - y0) * (x2 - x0);
+#ifdef BOJ
+#define BOJTEST(x) ((void)0)
+#else
+#define BOJTEST(x) cout << "[Debug] " << #x << ':' << x << '\n'
+#endif
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL); // boj_15552.cpp
+#define SETPRECISION(n) cout << fixed;cout.precision(n); // boj_1008.cpp
+#define SIZE(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
+using ll = long long;
+using uint = unsigned int;
+using ull = unsigned long long;
+using pll = pair<ll, ll>;
 
-    if (ccw_check > 0) {
-        return 1;
-    }
-    else if (ccw_check == 0) {
-        return 0;
-    }
-    else {
-        return -1;
-    }
+inline int ccw(pll p0, pll p1, pll p2) {
+    ll res = (p1.first - p0.first) * (p2.second - p0.second) - (p2.first - p0.first) * (p1.second - p0.second);
+
+    if (res > 0) return 1;
+    else if (res < 0) return -1;
+    else return 0;
 }
 
 int main() {
-    ios_base::sync_with_stdio(false); // C++와 C 두 표준 입출력 동기화를 해제한다.
-    cout.tie(NULL);
-    cin.tie(NULL);                    // 입력과 출력이 묶여있는 것을 풀어준다.
+    FASTIO;
 
-    long long x[4], y[4];
-
+    pll pt[4];
     for (int i = 0; i < 4; i++) {
-        cin >> x[i] >> y[i];
+        cin >> pt[i].first >> pt[i].second;
     }
 
-    if (ccw(x[0], y[0], x[1], y[1], x[2], y[2]) + ccw(x[0], y[0], x[1], y[1], x[3], y[3]) == 0) {
-        if (ccw(x[2], y[2], x[3], y[3], x[0], y[0]) + ccw(x[2], y[2], x[3], y[3], x[1], y[1]) == 0) {
-            cout << 1 << '\n';
-        }
-        else {
-            cout << 0 << '\n';
-        }
+    if (ccw(pt[0], pt[1], pt[2]) + ccw(pt[0], pt[1], pt[3]) == 0 && ccw(pt[2], pt[3], pt[0]) + ccw(pt[2], pt[3], pt[1]) == 0) {
+        cout << 1 << '\n';
     }
     else {
         cout << 0 << '\n';
--- baekjoon_all/17000+/boj_17387.cpp
+++ baekjoon_all/17000+/boj_17387.cpp
@@ -1,100 +1,80 @@
-#include <iostream>
+// Solve 2022-09-05
+// Update 2023-03-16
+
+#include <bits/stdc++.h>
 using namespace std;
 
-int ccw(long long x0, long long y0, long long x1, long long y1, long long x2, long long y2) {
-    long long ccw_check = (x1 - x0) * (y2 - y0) - (y1 - y0) * (x2 - x0);
+#ifdef BOJ
+#define BOJTEST(x) ((void)0)
+#else
+#define BOJTEST(x) cout << "[Debug] " << #x << ':' << x << '\n'
+#endif
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL); // boj_15552.cpp
+#define SETPRECISION(n) cout << fixed;cout.precision(n); // boj_1008.cpp
+#define SIZE(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
+using ll = long long;
+using uint = unsigned int;
+using ull = unsigned long long;
+using pll = pair<ll, ll>;
 
-    if (ccw_check > 0) {
-        return 1;
+pll pt[4];
+
+inline int ccw(pll p0, pll p1, pll p2) {
+    ll res = (p1.first - p0.first) * (p2.second - p0.second) - (p2.first - p0.first) * (p1.second - p0.second);
+
+    if (res > 0) return 1;
+    else if (res < 0) return -1;
+    else return 0;
+}
+
+bool two_lines_intersect() {
+    int ccw012 = ccw(pt[0], pt[1], pt[2]);
+    int ccw013 = ccw(pt[0], pt[1], pt[3]);
+    int ccw230 = ccw(pt[2], pt[3], pt[0]);
+    int ccw231 = ccw(pt[2], pt[3], pt[1]);
+
+    if (ccw012 == 0 && ccw013 == 0) {
+        // 네 점이 한 직선 위에 있을 경우
+        if (pt[0] > pt[1]) swap(pt[0], pt[1]);
+        if (pt[2] > pt[3]) swap(pt[2], pt[3]);
+
+        if (pt[0] <= pt[3] && pt[2] <= pt[1]) {
+            return true;
+        }
+        return false;
     }
-    else if (ccw_check == 0) {
-        return 0;
+
+    if (ccw012 == 0 || ccw013 == 0) {
+        // 세 점이 한 직선 위에 있을 경우
+        if (ccw230 == ccw231) {
+            return false;
+        }
+        return true;
     }
-    else {
-        return -1;
+
+    if (ccw230 == 0 || ccw231 == 0) {
+        // 세 점이 한 직선 위에 있을 경우
+        if (ccw012 == ccw013) {
+            return false;
+        }
+        return true;
     }
+
+    if (ccw012 + ccw013 == 0 && ccw230 + ccw231 == 0) {
+        return true;
+    }
+    return false;
 }
 
 int main() {
-    ios_base::sync_with_stdio(false); // C++와 C 두 표준 입출력 동기화를 해제한다.
-    cout.tie(NULL);
-    cin.tie(NULL);                    // 입력과 출력이 묶여있는 것을 풀어준다.
-
-    long long x[4], y[4];
+    FASTIO;
 
     for (int i = 0; i < 4; i++) {
-        cin >> x[i] >> y[i];
+        cin >> pt[i].first >> pt[i].second;
     }
 
-    int ccw012 = ccw(x[0], y[0], x[1], y[1], x[2], y[2]);
-    int ccw013 = ccw(x[0], y[0], x[1], y[1], x[3], y[3]);
-    int ccw230 = ccw(x[2], y[2], x[3], y[3], x[0], y[0]);
-    int ccw231 = ccw(x[2], y[2], x[3], y[3], x[1], y[1]);
-
-    if (ccw012 * ccw013 == 0) {
-        if (ccw230 * ccw231 == 0) {
-            // 네 점이 모두 한 직선 위에 있는 경우에는 ccw 값이 모두 0이 나오게 되어 판별이 힘들다.
-            // 따라서 직선 외부의 점 (x5, y5)를 하나 더 지정해서 ccw 알고리즘을 사용하는 방식으로 문제를 해결했다.
-
-            // 4개의 점 중 하나의 점을 y축으로 +1만큼 이동시킨 점으로 5번째 점을 정했다.
-            // 그런데 만약 y축과 평행한 직선일 경우에는 5번째 점 또한 같은 직선 위에 있게 되므로
-            // y축이 아니라 x축으로 +1만큼 이동시킨 점으로 다시 정했다.
-            long long x5 = x[0];
-            long long y5 = y[0] + 1;
-            if (x[0] == x[1]) {
-                x5++;
-                y5--;
-            }
-
-            int ccw520 = ccw(x5, y5, x[2], y[2], x[0], y[0]);
-            int ccw521 = ccw(x5, y5, x[2], y[2], x[1], y[1]);
-            int ccw530 = ccw(x5, y5, x[3], y[3], x[0], y[0]);
-            int ccw531 = ccw(x5, y5, x[3], y[3], x[1], y[1]);
-
-            if (ccw520 == ccw521) {
-                if (ccw530 == ccw531) {
-                    if (ccw520 == ccw530) {
-                        cout << 0;
-                    }
-                    else {
-                        cout << 1;
-                    }
-                }
-                else {
-                    cout << 1;
-                }
-            }
-            else {
-                cout << 1;
-            }
-        }
-        else if (ccw230 == ccw231) {
-            cout << 0;
-        }
-        else {
-            cout << 1;
-        }
-    }
-    else if (ccw230 * ccw231 == 0) {
-        if (ccw012 == ccw013) {
-            cout << 0;
-        }
-        else {
-            cout << 1;
-        }
-    }
-    else if (ccw012 == ccw013) {
-        cout << 0;
-    }
-    else {
-        if (ccw230 == ccw231) {
-            cout << 0;
-        }
-        else {
-            cout << 1;
-        }
-    }
-    cout << '\n';
+    cout << two_lines_intersect() << '\n';
 
     return 0;
 }
