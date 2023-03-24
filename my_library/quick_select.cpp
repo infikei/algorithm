910765f41248b33f8050ad85ef79c4ec03bdad89
Infikei <hckhong@naver.com>
2023-03-25 01:40:20 +0900

    빠른 선택 알고리즘

--- my_library/quick_select.cpp
+++ my_library/quick_select.cpp
@@ -1,16 +1,23 @@
 // 빠른 선택 (quick select) 알고리즘
 
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
 
-int quick_select(const vector<int>& vt, int k) {
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL); // boj_15552.cpp
+#define SETPRECISION(n) cout << fixed;cout.precision(n); // boj_1008.cpp
+#define SIZE(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
+using ll = long long;
+using uint = unsigned int;
+using ull = unsigned long long;
+
+int quick_select(const vector<int> &vt, int k) {
     vector<int> small, same, big;
 
     int pivot = vt[0];
-    int vt_len = vt.size();
+    int i_end = SIZE(vt);
 
-    for (int i = 0; i < vt_len; i++) {
+    for (int i = 0; i < i_end; i++) {
         if (vt[i] < pivot) {
             small.push_back(vt[i]);
         }
@@ -22,11 +29,11 @@ int quick_select(const vector<int>& vt, int k) {
         }
     }
 
-    if ((int)small.size() >= k) {
+    if (SIZE(small) >= k) {
         return quick_select(small, k);
     }
-    else if ((int)small.size() + (int)same.size() < k) {
-        return quick_select(big, k - (int)small.size() - (int)same.size());
+    else if (SIZE(small) + SIZE(same) < k) {
+        return quick_select(big, k - SIZE(small) - SIZE(same));
     }
     else {
         return pivot;
