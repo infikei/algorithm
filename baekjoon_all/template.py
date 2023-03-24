45919eaae6bc75098f9c81ec9895b76c0525a2ff
Infikei <hckhong@naver.com>
2023-03-25 01:38:33 +0900

    Update Template for Python

--- baekjoon_all/template.py
+++ baekjoon_all/template.py
@@ -1,6 +1,10 @@
 import sys
-from collections import deque, Counter
-from heapq import heappush, heappop, heapify
-from math import ceil, sqrt
+# from collections import deque, Counter
+# from heapq import heappush, heappop, heapify
+# from math import ceil, floor, sqrt, pi, sin, cos, tan
+# from decimal import *
+
+# getcontext().prec = 30
+# getcontext().rounding = ROUND_HALF_UP
 
 input = lambda : sys.stdin.readline().rstrip()
