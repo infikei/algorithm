08b0a03f35978f89234734239c3d7aa10620e252
Infikei <hckhong@naver.com>
2023-04-24 05:34:25 +0900

    Solve 2023-04-22

---
+++ baekjoon_all/17000+/boj_17350.cpp
@@ -0,0 +1,44 @@
+// Solve 2023-04-22
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef BOJ
+#define BOJTEST(x) ((void)0)
+#else
+#define BOJTEST(x) cout << "[Debug] " << #x << ':' << x << '\n'
+#endif
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL); // boj_15552.cpp
+#define SETPRECISION(n) cout << fixed;cout.precision(n); // boj_1008.cpp
+#define SIZE(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
+using ll = long long;
+using uint = unsigned int;
+using ull = unsigned long long;
+
+int main() {
+    FASTIO;
+
+    int n;
+    cin >> n;
+
+    bool ans = false;
+
+    for (int i = 0; i < n; i++) {
+        string s;
+        cin >> s;
+
+        if (!ans && s == "anj") {
+            ans = true;
+        }
+    }
+
+    if (ans) {
+        cout << "뭐야;\n";
+    }
+    else {
+        cout << "뭐야?\n";
+    }
+
+    return 0;
+}
