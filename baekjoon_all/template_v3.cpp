d3292456b74866cf25931bbaa1e4b5d98dc7866b
Infikei <hckhong@naver.com>
2023-06-25 03:04:24 +0900

    불필요한 cout.tie(NULL) 코드 삭제함

--- baekjoon_all/template_v3.cpp
+++ baekjoon_all/template_v3.cpp
@@ -6,7 +6,7 @@ using namespace std;
 // #else
 // #define BOJTEST(x) cout << "[Debug] " << #x << ':' << x << '\n'
 // #endif
-#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL); // boj_15552.cpp
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL); // boj_15552.cpp
 #define SETPRECISION(n) cout << fixed;cout.precision(n); // boj_1008.cpp
 // #define SETPRECISION(n) cout << fixed << setprecision(n); // boj_1008.cpp (먼저 iomanip을 include해야 한다)
 #define SIZE(v) (int)v.size()
