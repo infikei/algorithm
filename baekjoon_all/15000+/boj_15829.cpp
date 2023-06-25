68f67f1970ba6e7eb5cd41b4192325ec65234a09
Infikei <hckhong@naver.com>
2023-06-26 04:09:00 +0900

    Update 2023-06-25

--- baekjoon_all/15000+/boj_15829.cpp
+++ baekjoon_all/15000+/boj_15829.cpp
@@ -1,25 +1,33 @@
-#include <iostream>
-#include <string>
+// Solve 2022-06-10
+// Update 2023-06-25
+
+#include <bits/stdc++.h>
 using namespace std;
 
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL); // boj_15552.cpp
+#define SETPRECISION(n) cout << fixed;cout.precision(n); // boj_1008.cpp
+#define SIZE(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
+using ll = long long;
+
+const ll MOD = 1234567891;
+const ll r = 31;
+
 int main() {
-    ios_base::sync_with_stdio(false); // C++와 C 두 표준 입출력 동기화를 해제한다.
-    cout.tie(NULL);
-    cin.tie(NULL);                    // 입력과 출력이 묶여있는 것을 풀어준다.
+    FASTIO;
 
-    long long size, r = 31, m = 1234567891;
-    string input;
-    cin >> size >> input;
+    ll s_length;
+    string s;
+    cin >> s_length >> s;
 
-    long long ans = input[size - 1] - 'a' + 1;
+    ll ans = 0;
 
-    for (int i = size - 2; i >= 0; i--) {
+    for (int i = s_length - 1; i >= 0; i--) {
         ans *= r;
-        ans = ans % m;
-        ans += input[i] - 'a' + 1;
+        ans += s[i] - 'a' + 1;
+        ans %= MOD;
     }
-    
-    ans = ans % m;
+
     cout << ans << '\n';
 
     return 0;
