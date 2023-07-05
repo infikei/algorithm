bb4d88c015edbceb1333c411c867a8862d0099b9
Infikei <hckhong@naver.com>
2023-07-06 05:06:31 +0900

    Update 2023-07-04

--- baekjoon_all/05000+/boj_5639.cpp
+++ baekjoon_all/05000+/boj_5639.cpp
@@ -1,46 +1,36 @@
 // Solve 2023-02-14
+// Update 2023-07-04
 
 #include <bits/stdc++.h>
 using namespace std;
 
-#ifdef BOJ
-#define BOJTEST(x) ((void)0)
-#else
-#define BOJTEST(x) cout << "[Debug] " << #x << ':' << x << '\n'
-#endif
-#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL); // boj_15552.cpp
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL); // boj_15552.cpp
+#define SETPRECISION(n) cout << fixed;cout.precision(n); // boj_1008.cpp
 #define SIZE(v) (int)v.size()
 #define ALL(v) v.begin(),v.end()
-#define INF (int)1e9
-#define LLINF (ll)4e18
 using ll = long long;
-using uint = unsigned int;
-using ull = unsigned long long;
 
-const int MAX_N = 10000;
-int n, preorder[MAX_N];
+int arr[10000];
 
-void print_postorder(int pre_l, int pre_r) {
-    if (pre_l > pre_r) return;
+void print(int s, int e) {
+    if (s > e) return;
 
-    int root_val = preorder[pre_l];
-    int pre_next_l = lower_bound(preorder + (pre_l + 1), preorder + (pre_r + 1), root_val) - preorder;
-
-    print_postorder(pre_l + 1, pre_next_l - 1);
-    print_postorder(pre_next_l, pre_r);
-    cout << root_val << '\n';
+    int mid = upper_bound(arr + (s + 1), arr + (e + 1), arr[s]) - arr;
+    print(s + 1, mid - 1);
+    print(mid, e);
+    cout << arr[s] << '\n';
 }
 
 int main() {
     FASTIO;
 
-    int x;
+    int n = 0, x;
     while (cin >> x) {
-        preorder[n] = x;
+        arr[n] = x;
         n++;
     }
 
-    print_postorder(0, n - 1);
+    print(0, n - 1);
 
     return 0;
 }
