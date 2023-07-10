7b8a23442aa433e0a6411826c34179385ad1e786
Infikei <hckhong@naver.com>
2023-07-11 01:18:40 +0900

    Update 2023-07-09

--- baekjoon_all/15000+/boj_15700.cpp
+++ baekjoon_all/15000+/boj_15700.cpp
@@ -1,15 +1,22 @@
-#include <iostream>
+// Solve 2022-11-01
+// Update 2023-07-09
+
+#include <bits/stdc++.h>
 using namespace std;
-#define fastio ios_base::sync_with_stdio(false);cout.tie(NULL);cin.tie(NULL); // boj_15552.cpp
+
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL); // boj_15552.cpp
+#define SETPRECISION(n) cout << fixed;cout.precision(n); // boj_1008.cpp
+#define SIZE(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
 using ll = long long;
 
 int main() {
-    fastio;
+    FASTIO;
 
-    int N, M;
-    cin >> N >> M;
+    ll n, m;
+    cin >> n >> m;
 
-    ll ans = (ll)N * M / 2;
+    ll ans = n * m / 2;
     cout << ans << '\n';
 
     return 0;
