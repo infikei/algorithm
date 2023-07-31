9d1feb95f94d600f80b89348a0dbfa5570f05d66
Infikei <hckhong@naver.com>
2023-08-01 06:56:33 +0900

    Update 2023-07-31

--- baekjoon_all/26000+/boj_26517_contest16_A(P).cpp
+++ baekjoon_all/26000+/boj_26517_contest16_A(P).cpp
@@ -1,17 +1,27 @@
-#include <iostream>
-#define fastio ios_base::sync_with_stdio(false);cout.tie(NULL);cin.tie(NULL); // boj_15552.cpp
+// Solve 2022-12-24
+// Update 2023-07-31
+
+#include <bits/stdc++.h>
 using namespace std;
+
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL); // boj_15552.cpp
+#define SETPRECISION(n) cout << fixed;cout.precision(n); // boj_1008.cpp
+#define SIZE(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
 using ll = long long;
 
 int main() {
-    fastio;
+    FASTIO;
 
     ll k, a, b, c, d;
-
     cin >> k >> a >> b >> c >> d;
 
-    if (a * k + b == c * k + d) cout << "Yes " << a * k + b << '\n';
-    else cout << "No\n";
+    if (a * k + b == c * k + d) {
+        cout << "Yes " << a * k + b << '\n';
+    }
+    else {
+        cout << "No\n";
+    }
 
     return 0;
 }
--- baekjoon_all/26000+/boj_26518_contest16_B(P).cpp
+++ baekjoon_all/26000+/boj_26518_contest16_B(P).cpp
@@ -1,19 +1,25 @@
-#include <iostream>
-#include <cmath>
-#define fastio ios_base::sync_with_stdio(false);cout.tie(NULL);cin.tie(NULL); // boj_15552.cpp
+// Solve 2022-12-24
+// Update 2023-07-31
+
+#include <bits/stdc++.h>
 using namespace std;
 
-int b, c, a1, a2;
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL); // boj_15552.cpp
+#define SETPRECISION(n) cout << fixed;cout.precision(n); // boj_1008.cpp
+#define SIZE(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
+using ll = long long;
 
 int main() {
-    fastio;
+    FASTIO;
 
+    int b, c, a1, a2;
     cin >> b >> c >> a1 >> a2;
 
     double b2 = (double)b / 2;
     double ans = b2 + sqrt(b2 * b2 + c);
-    cout << fixed;
-    cout.precision(9);
+
+    SETPRECISION(9);
     cout << ans << '\n';
 
     return 0;
