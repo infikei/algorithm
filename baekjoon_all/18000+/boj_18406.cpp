cff8ad65c75b1e6b1eadf7e00c8bb7866d49dcb2
Infikei <hckhong@naver.com>
2023-08-13 02:06:27 +0900

    Update 2023-08-12

--- baekjoon_all/18000+/boj_18406.cpp
+++ baekjoon_all/18000+/boj_18406.cpp
@@ -1,20 +1,36 @@
-#include <iostream>
-#include <string>
-#define fastio ios_base::sync_with_stdio(false);cout.tie(NULL);cin.tie(NULL); // boj_15552.cpp
+// Solve 2023-01-15
+// Update 2023-08-12
+
+#include <bits/stdc++.h>
 using namespace std;
 
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL); // boj_15552.cpp
+#define SETPRECISION(n) cout << fixed;cout.precision(n); // boj_1008.cpp
+#define SIZE(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
+using ll = long long;
+
 int main() {
-    fastio;
+    FASTIO;
 
     string n;
     cin >> n;
-    int n_size = n.length();
 
     int sum1 = 0, sum2 = 0;
-    for (int i = 0; i < n_size / 2; i++) sum1 += n[i] - '0';
-    for (int i = n_size / 2; i < n_size; i++) sum2 += n[i] - '0';
 
-    cout << (sum1 == sum2 ? "LUCKY\n" : "READY\n");
+    for (int i = 0, i_end = SIZE(n) / 2; i < i_end; i++) {
+        sum1 += n[i] - '0';
+    }
+    for (int i = SIZE(n) / 2, i_end = SIZE(n); i < i_end; i++) {
+        sum2 += n[i] - '0';
+    }
+
+    if (sum1 == sum2) {
+        cout << "LUCKY\n";
+    }
+    else {
+        cout << "READY\n";
+    }
 
     return 0;
 }
--- baekjoon_all/18000+/boj_18409.cpp
+++ baekjoon_all/18000+/boj_18409.cpp
@@ -1,22 +1,29 @@
-#include <iostream>
-#include <string>
-using namespace std;
-#define fastio ios_base::sync_with_stdio(false);cout.tie(NULL);cin.tie(NULL); // boj_15552.cpp
+// Solve 2022-11-17
+// Update 2023-08-12
 
-char aeiou[5] = {'a', 'e', 'i', 'o', 'u'};
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL); // boj_15552.cpp
+#define SETPRECISION(n) cout << fixed;cout.precision(n); // boj_1008.cpp
+#define SIZE(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
+using ll = long long;
+
+char aeiou[5] = { 'a', 'e', 'i', 'o', 'u' };
 
 int main() {
-    fastio;
+    FASTIO;
 
-    int N;
-    string S;
-    cin >> N >> S;
+    int n;
+    string s;
+    cin >> n >> s;
 
     int cnt = 0;
 
-    for (auto s : S) {
-        for (auto a : aeiou) {
-            if (s == a) {
+    for (const char ch : s) {
+        for (const char a : aeiou) {
+            if (ch == a) {
                 cnt++;
                 break;
             }
