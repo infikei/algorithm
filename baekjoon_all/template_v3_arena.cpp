3b87dd2fe90dd328ef24ef6c50698cdae33798db
Infikei <hckhong@naver.com>
2023-08-14 01:21:09 +0900

    솔브닥 아레나 전용 템플릿 추가

---
+++ baekjoon_all/template_v3_arena.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef BOJ
+#define BOJTEST(x) ((void)0)
+#else
+#define BOJTEST(x) cout << "[Debug] " << #x << ':' << x << '\n'
+#endif
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL); // boj_15552.cpp
+#define SETPRECISION(n) cout << fixed;cout.precision(n); // boj_1008.cpp
+#define SIZE(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
+#define FOR(i, ie, a, b) for (int i = (a), ie = (b); i <= ie; i++)
+#define ROF(i, ie, a, b) for (int i = (a), ie = (b); i >= ie; i--)
+#define FORR(i, ie, a, b) for (ll i = (a), ie = (b); i <= ie; i++)
+#define ROFF(i, ie, a, b) for (ll i = (a), ie = (b); i >= ie; i--)
+#define COUT(a) cout << (a);
+#define COUTT(a, c) cout << (a) << (c);
+using ll = long long;
+using uint = unsigned int;
+using ull = unsigned long long;
+using ld = long double;
+// using pii = pair<int, int>;
+// using pi3 = pair<int, pii>;
+// using pi4 = pair<pii, pii>;
+// using pll = pair<ll, ll>;
+// using pl3 = pair<ll, pll>;
+// using pl4 = pair<pll, pll>;
+// using pdd = pair<double, double>;
+// using matrix = vector<vector<int> >;
+// using matrixll = vector<vector<ll> >;
+// using cpx = complex<double>;
+
+// const int INF = 1e9;
+// const ll LLINF = 4e18;
+// const ll MOD = 1000000007;
+// const double PI = acos(-1);
+// const int MAX_SIZE = 1000;
+// const int MATRIX_SIZE = 2;
+// const int FFT_SIZE = 1 << 21;
+
+int main() {
+    FASTIO;
+
+    return 0;
+}
