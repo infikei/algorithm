222a43262f8c665202954cee022de310d6959f16
Infikei <hckhong@naver.com>
2023-08-16 02:13:15 +0900

    Update 2023-08-15

--- baekjoon_all/10000+/boj_10179.cpp
+++ baekjoon_all/10000+/boj_10179.cpp
@@ -1,21 +1,29 @@
-#include <iostream>
+// Solve 2022-11-16
+// Update 2023-08-15
+
+#include <bits/stdc++.h>
 using namespace std;
-#define fastio ios_base::sync_with_stdio(false);cout.tie(NULL);cin.tie(NULL); // boj_15552.cpp
+
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL); // boj_15552.cpp
+#define SETPRECISION(n) cout << fixed;cout.precision(n); // boj_1008.cpp
+#define SIZE(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
+using ll = long long;
 
 int main() {
-    fastio;
+    FASTIO;
 
-    int T;
-    cin >> T;
+    SETPRECISION(2);
 
-    for (int t = 0; t < T; t++) {
+    int t;
+    cin >> t;
+
+    for (int ti = 0; ti < t; ti++) {
         double d;
         cin >> d;
 
         d *= 0.8;
 
-        cout << fixed;
-        cout.precision(2);
         cout << '$' << d << '\n';
     }
 
--- baekjoon_all/10000+/boj_10181.cpp
+++ baekjoon_all/10000+/boj_10181.cpp
@@ -1,56 +1,64 @@
+// Solve 2023-01-09
+// Update 2023-08-15
+
 // 백준 9506번과 같은 문제
 
-#include <iostream>
-#include <cmath>
-#include <queue>
-#include <stack>
+#include <bits/stdc++.h>
 using namespace std;
-#define fastio ios_base::sync_with_stdio(false);cout.tie(NULL);cin.tie(NULL); // boj_15552.cpp
+
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL); // boj_15552.cpp
+#define SETPRECISION(n) cout << fixed;cout.precision(n); // boj_1008.cpp
+#define SIZE(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
+using ll = long long;
 
 int main() {
-    fastio;
+    FASTIO;
 
     while (true) {
-        int N;
-        cin >> N;
-        if (N == -1) {
-            break;
-        }
+        int n;
+        cin >> n;
+
+        if (n == -1) break;
 
         int sum = 0;
-        queue<int> q;
+        queue<int> que;
         stack<int> stck;
 
-        for (int i = 1; i <= sqrt(N); i++) {
-            if (N % i == 0) {
+        for (int i = 1, ie = sqrt(n); i <= ie; i++) {
+            if (n % i == 0) {
                 if (i == 1) {
                     sum += 1;
                     continue;
                 }
+
                 sum += i;
-                q.push(i);
-                if (i == N / i) {
-                    break;
-                }
-                sum += N / i;
-                stck.push(N / i);
+                que.push(i);
+
+                int j = n / i;
+                if (i == j) break;
+                sum += j;
+                stck.push(j);
             }
         }
 
-        if (N == sum) {
-            cout << N << " = 1";
-            while (!q.empty()) {
-                cout << " + " << q.front();
-                q.pop();
+        if (n == sum) {
+            cout << n << " = 1";
+
+            while (!que.empty()) {
+                cout << " + " << que.front();
+                que.pop();
             }
+
             while (!stck.empty()) {
                 cout << " + " << stck.top();
                 stck.pop();
             }
+
             cout << '\n';
         }
         else {
-            cout << N << " is NOT perfect.\n";
+            cout << n << " is NOT perfect.\n";
         }
     }
 
