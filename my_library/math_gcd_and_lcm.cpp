8bb508c23ab10a4b6295a1b30b2813dc1642c920
Infikei <hckhong@naver.com>
2023-08-29 04:57:33 +0900

    최대공약수와 최소공배수

--- my_library/math_gcd_and_lcm.cpp
+++ my_library/math_gcd_and_lcm.cpp
@@ -3,19 +3,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL); // boj_15552.cpp
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL); // boj_15552.cpp
 #define SETPRECISION(n) cout << fixed;cout.precision(n); // boj_1008.cpp
 #define SIZE(v) (int)v.size()
 #define ALL(v) v.begin(),v.end()
 using ll = long long;
-using uint = unsigned int;
-using ull = unsigned long long;
 
 int calc_gcd(int a, int b) {
-    if (a % b == 0) {
-        return b;
-    }
-    return calc_gcd(b, a % b);
+    int r = a % b;
+    if (r == 0) return b;
+    return calc_gcd(b, r);
 }
 
 int calc_lcm(int a, int b) {
