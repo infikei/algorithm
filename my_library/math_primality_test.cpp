98aff5a7ef0445c8c33561086a44e612be715fcb
Infikei <hckhong@naver.com>
2023-08-29 04:56:42 +0900

    소수 판정

--- my_library/math_primality_test.cpp
+++ my_library/math_primality_test.cpp
@@ -3,24 +3,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL); // boj_15552.cpp
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL); // boj_15552.cpp
 #define SETPRECISION(n) cout << fixed;cout.precision(n); // boj_1008.cpp
 #define SIZE(v) (int)v.size()
 #define ALL(v) v.begin(),v.end()
 using ll = long long;
-using uint = unsigned int;
-using ull = unsigned long long;
 
 bool is_prime(int k) {
-    if (k < 2) {
-        return false;
-    }
+    if (k < 2) return false;
 
-    int i_end = sqrt(k);
-    for (int i = 2; i <= i_end; i++) {
-        if (k % i == 0) {
-            return false;
-        }
+    for (int i = 2, ie = sqrt(k); i <= ie; i++) {
+        if (k % i == 0) return false;
     }
 
     return true;
