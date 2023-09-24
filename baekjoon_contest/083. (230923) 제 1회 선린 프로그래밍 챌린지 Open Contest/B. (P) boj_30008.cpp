85faff51c40725fd13e312f7850edfdb68d33f7e
Infikei <hckhong@naver.com>
2023-09-25 00:50:32 +0900

    Solve 2023-09-23

---
+++ baekjoon_contest/083. (230923) 제 1회 선린 프로그래밍 챌린지 Open Contest/B. (P) boj_30008.cpp
@@ -0,0 +1,41 @@
+// Solve 2023-09-23
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL); // boj_15552.cpp
+#define SETPRECISION(n) cout << fixed;cout.precision(n); // boj_1008.cpp
+#define SIZE(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
+using ll = long long;
+
+int main() {
+    FASTIO;
+
+    int n, k;
+    cin >> n >> k;
+
+    for (int ki = 0; ki < k; ki++) {
+        int g;
+        cin >> g;
+
+        int p = g * 100 / n;
+        int ans = 0;
+
+        if (p <= 4) ans = 1;
+        else if (p <= 11) ans = 2;
+        else if (p <= 23) ans = 3;
+        else if (p <= 40) ans = 4;
+        else if (p <= 60) ans = 5;
+        else if (p <= 77) ans = 6;
+        else if (p <= 89) ans = 7;
+        else if (p <= 96) ans = 8;
+        else ans = 9;
+
+        cout << ans << ' ';
+    }
+
+    cout << '\n';
+
+    return 0;
+}
