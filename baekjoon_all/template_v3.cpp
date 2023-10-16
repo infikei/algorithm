9c38bb52b13b7917c7e36914e81582ec4d31c623
Infikei <hckhong@naver.com>
2023-10-17 04:07:06 +0900

    C++ 템플릿을 재정비함 (lite를 a로 개편하고, arena를 b로 개편함)

--- baekjoon_all/template_v3.cpp
+++ baekjoon_all/template_v3.cpp
@@ -1,23 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// #ifdef BOJ
-// #define BOJTEST(x) ((void)0)
-// #else
-// #define BOJTEST(x) cout << "[Debug] " << #x << ':' << x << '\n'
-// #endif
+#ifdef BOJ
+#define BOJTEST(x) ((void)0)
+#else
+#define BOJTEST(x) cout << "[Debug] " << #x << ':' << x << '\n'
+#endif
 #define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL); // boj_15552.cpp
 #define SETPRECISION(n) cout << fixed;cout.precision(n); // boj_1008.cpp
-// #define SETPRECISION(n) cout << fixed << setprecision(n); // boj_1008.cpp (먼저 iomanip을 include해야 한다)
 #define SIZE(v) (int)v.size()
 #define ALL(v) v.begin(),v.end()
-// #define FOR(i,a,b) for(int i=(a);i<=(b);i++)
-// #define ROF(i,a,b) for(int i=(a);i>=(b);i--)
-// #define UNIQUE(v) v.erase(unique(v.begin(),v.end()),v.end()); // 정렬이 이미 되어있는 상태에서 중복 제거
+#define UNIQUE(v) v.erase(unique(v.begin(),v.end()),v.end()); // 정렬이 이미 되어있는 상태에서 중복 제거
+#define FOR(i, ie, a, b) for (int i = (a), ie = (b); i <= ie; i++)
+#define FORR(i, ie, a, b) for (ll i = (a), ie = (b); i <= ie; i++)
+#define ROF(i, ie, a, b) for (int i = (a), ie = (b); i >= ie; i--)
+#define ROFF(i, ie, a, b) for (ll i = (a), ie = (b); i >= ie; i--)
 using ll = long long;
-// using uint = unsigned int;
-// using ull = unsigned long long;
-// using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+using ld = long double;
+// using cpx = complex<double>;
 // using pii = pair<int, int>;
 // using pi3 = pair<int, pii>;
 // using pi4 = pair<pii, pii>;
@@ -27,12 +29,11 @@ using ll = long long;
 // using pdd = pair<double, double>;
 // using matrix = vector<vector<int> >;
 // using matrixll = vector<vector<ll> >;
-// using cpx = complex<double>;
 
+const double PI = M_PI;
 // const int INF = 1e9;
 // const ll LLINF = 4e18;
 // const ll MOD = 1000000007;
-// const double PI = M_PI;
 // const int MAX_SIZE = 1000;
 // const int MATRIX_SIZE = 2;
 // const int FFT_SIZE = 1 << 21;
--- baekjoon_all/template_v3b.cpp
+++ baekjoon_all/template_v3b.cpp
@@ -10,32 +10,17 @@ using namespace std;
 #define SETPRECISION(n) cout << fixed;cout.precision(n); // boj_1008.cpp
 #define SIZE(v) (int)v.size()
 #define ALL(v) v.begin(),v.end()
+#define UNIQUE(v) v.erase(unique(v.begin(),v.end()),v.end()); // 정렬이 이미 되어있는 상태에서 중복 제거
 #define FOR(i, ie, a, b) for (int i = (a), ie = (b); i <= ie; i++)
-#define ROF(i, ie, a, b) for (int i = (a), ie = (b); i >= ie; i--)
 #define FORR(i, ie, a, b) for (ll i = (a), ie = (b); i <= ie; i++)
+#define ROF(i, ie, a, b) for (int i = (a), ie = (b); i >= ie; i--)
 #define ROFF(i, ie, a, b) for (ll i = (a), ie = (b); i >= ie; i--)
 using ll = long long;
 using uint = unsigned int;
 using ull = unsigned long long;
 using ld = long double;
-// using pii = pair<int, int>;
-// using pi3 = pair<int, pii>;
-// using pi4 = pair<pii, pii>;
-// using pll = pair<ll, ll>;
-// using pl3 = pair<ll, pll>;
-// using pl4 = pair<pll, pll>;
-// using pdd = pair<double, double>;
-// using matrix = vector<vector<int> >;
-// using matrixll = vector<vector<ll> >;
-// using cpx = complex<double>;
 
-// const int INF = 1e9;
-// const ll LLINF = 4e18;
-// const ll MOD = 1000000007;
-// const double PI = acos(-1);
-// const int MAX_SIZE = 1000;
-// const int MATRIX_SIZE = 2;
-// const int FFT_SIZE = 1 << 21;
+const double PI = M_PI;
 
 int main() {
     FASTIO;
