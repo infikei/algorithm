7d3054743b8f47f6ba60dbf24146a92e51a1967b
Infikei <hckhong@naver.com>
2023-11-16 05:32:02 +0900

    Solve 2023-11-14

---
+++ baekjoon_all/21000+/boj_21964_solution1.cpp
@@ -0,0 +1,22 @@
+// Solve 2023-11-14
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL); // boj_15552.cpp
+#define SETPRECISION(n) cout << fixed;cout.precision(n); // boj_1008.cpp
+#define SIZE(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
+using ll = long long;
+
+int main() {
+    FASTIO;
+
+    int n;
+    string s;
+    cin >> n >> s;
+
+    cout << s.substr(n - 5, 5) << '\n';
+
+    return 0;
+}
