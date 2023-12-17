d3073d13950f57897106bbe4ca54351ecacbd931
Infikei <hckhong@naver.com>
2023-12-18 04:53:00 +0900

    Update 2023-12-17

--- baekjoon_all/10000+/boj_10026.cpp
+++ baekjoon_all/10000+/boj_10026.cpp
@@ -1,110 +1,98 @@
 // Solve 2023-04-11
+// Update 2023-12-17
 
 #include <bits/stdc++.h>
 using namespace std;
 
-#ifdef BOJ
-#define BOJTEST(x) ((void)0)
-#else
-#define BOJTEST(x) cout << "[Debug] " << #x << ':' << x << '\n'
-#endif
-#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL); // boj_15552.cpp
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL); // boj_15552.cpp
 #define SETPRECISION(n) cout << fixed;cout.precision(n); // boj_1008.cpp
 #define SIZE(v) (int)v.size()
 #define ALL(v) v.begin(),v.end()
 using ll = long long;
-using uint = unsigned int;
-using ull = unsigned long long;
-using pii = pair<int, int>;
 
-int dx[4] = { 1, -1, 0, 0 };
-int dy[4] = { 0, 0, 1, -1 };
-bool visited[100][100];
+struct Point{
+    int r, c;
+    Point(int r, int c) : r(r), c(c) {}
+};
 
 int main() {
     FASTIO;
 
+    int dr[4] = { 0, 0, 1, -1 };
+    int dc[4] = { 1, -1, 0, 0 };
+
     int n;
     cin >> n;
 
-    vector<string> graph(n);
-    for (int i = 0; i < n; i++) {
-        cin >> graph[i];
+    vector<string> board(n);
+
+    for (string &row : board) {
+        cin >> row;
     }
 
-    int ans = 0, ans2 = 0;
-    stack<pii> stck;
+    vector<vector<bool>> visited(n, vector<bool>(n, false));
+    queue<Point> bfs_que;
+    int ans = 0;
 
-    for (int row = 0; row < n; row++) {
-        for (int col = 0; col < n; col++) {
-            if (visited[row][col]) {
-                continue;
-            }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (visited[i][j]) continue;
 
-            visited[row][col] = true;
-            stck.push({ row, col });
+            visited[i][j] = true;
+            bfs_que.emplace(i, j);
             ans++;
-            char ch = graph[row][col];
+            char color = board[i][j];
 
-            while (!stck.empty()) {
-                pii cur = stck.top();
-                stck.pop();
+            while (!bfs_que.empty()) {
+                Point now = bfs_que.front();
+                bfs_que.pop();
 
-                for (int i = 0; i < 4; i++) {
-                    int nrow = cur.first + dx[i];
-                    int ncol = cur.second + dy[i];
-                    if (nrow < 0 || nrow >= n || ncol < 0 || ncol >= n) {
-                        continue;
-                    }
-                    if (visited[nrow][ncol] || graph[nrow][ncol] != ch) {
-                        continue;
-                    }
-                    visited[nrow][ncol] = true;
-                    stck.push({ nrow, ncol });
+                for (int d = 0; d < 4; d++) {
+                    int nr = now.r + dr[d];
+                    int nc = now.c + dc[d];
+
+                    if (nr < 0 || nr >= n || nc < 0 || nc >= n) continue;
+                    if (visited[nr][nc] || board[nr][nc] != color) continue;
+
+                    visited[nr][nc] = true;
+                    bfs_que.emplace(nr, nc);
                 }
             }
         }
     }
 
-    for (int row = 0; row < n; row++) {
-        for (int col = 0; col < n; col++) {
-            visited[row][col] = false;
-        }
-    }
+    visited.assign(n, vector<bool>(n, false));
+    int ans2 = 0;
 
-    for (int row = 0; row < n; row++) {
-        for (int col = 0; col < n; col++) {
-            if (visited[row][col]) {
-                continue;
-            }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (visited[i][j]) continue;
 
-            visited[row][col] = true;
-            stck.push({ row, col });
+            visited[i][j] = true;
+            bfs_que.emplace(i, j);
             ans2++;
-            char ch = graph[row][col];
-            if (ch == 'G') {
-                ch = 'R';
-            }
+            char color = board[i][j];
 
-            while (!stck.empty()) {
-                pii cur = stck.top();
-                stck.pop();
+            if (color == 'G') color = 'R';
 
-                for (int i = 0; i < 4; i++) {
-                    int nrow = cur.first + dx[i];
-                    int ncol = cur.second + dy[i];
-                    if (nrow < 0 || nrow >= n || ncol < 0 || ncol >= n) {
-                        continue;
-                    }
-                    char ch2 = graph[nrow][ncol];
-                    if (ch2 == 'G') {
-                        ch2 = 'R';
-                    }
-                    if (visited[nrow][ncol] || ch2 != ch) {
-                        continue;
-                    }
-                    visited[nrow][ncol] = true;
-                    stck.push({ nrow, ncol });
+            while (!bfs_que.empty()) {
+                Point now = bfs_que.front();
+                bfs_que.pop();
+
+                for (int d = 0; d < 4; d++) {
+                    int nr = now.r + dr[d];
+                    int nc = now.c + dc[d];
+
+                    if (nr < 0 || nr >= n || nc < 0 || nc >= n) continue;
+
+                    char color2 = board[nr][nc];
+
+                    if (color2 == 'G') color2 = 'R';
+
+                    if (visited[nr][nc] || color2 != color) continue;
+
+                    visited[nr][nc] = true;
+                    bfs_que.emplace(nr, nc);
                 }
             }
         }
