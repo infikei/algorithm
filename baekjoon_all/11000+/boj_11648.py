a7d288f7f95df0eeaf4292c775c44815dfeafe24
Infikei <hckhong@naver.com>
2023-12-23 01:27:16 +0900

    Update 2023-12-22

--- baekjoon_all/11000+/boj_11648.py
+++ baekjoon_all/11000+/boj_11648.py
@@ -1,16 +1,13 @@
 # Solve 2023-12-21
+# Update 2023-12-22
+
+from math import prod
 
 n = input()
 ans = 0
 
 while len(n) > 1:
-    n = map(int, list(n))
-    digit_prod = 1
-
-    for d in n:
-        digit_prod *= d
-
-    n = str(digit_prod)
+    n = str(prod(map(int, list(n))))
     ans += 1
 
 print(ans)
