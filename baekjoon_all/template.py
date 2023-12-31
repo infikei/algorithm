5145cbf7e936c138b2e22e556387144658636397
Infikei <hckhong@naver.com>
2024-01-01 03:36:36 +0900

    백준 파이썬 템플릿 업데이트

--- baekjoon_all/template.py
+++ baekjoon_all/template.py
@@ -1,13 +1,15 @@
 # Solve 2023-
 
 import sys
+# from itertools import product
 # from bisect import bisect_left, bisect_right
-# from collections import deque, Counter
+# from collections import Counter, deque
 # from heapq import heappush, heappop, heapify
-# from math import ceil, floor, sqrt, pi, sin, cos, tan
+# from math import prod, perm, comb, ceil, floor, sqrt, pi, sin, cos, tan, gcd
 # from decimal import *
 
 # getcontext().prec = 30
 # getcontext().rounding = ROUND_HALF_UP
 
 input = lambda : sys.stdin.readline().rstrip()
+flush = lambda : sys.stdout.flush()
