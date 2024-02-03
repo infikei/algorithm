4b7bc65d758fff6ea00c75d0e59f9e4d554472fd
홍창기 <hckhong@naver.com>
2024-02-04 03:22:02 +0900

    Update 2024-02-03

--- baekjoon_all/15000+/boj_15552.java
+++ baekjoon_all/15000+/boj_15552.java
@@ -1,31 +1,29 @@
 // Solve 2023-10-18
-// Update 2023-10-23
+// Update 2024-02-03
 
-// 백준에 제출할 때는 class 이름을 Main으로 설정해야 함.
+// 백준에 제출할 때는 class 이름을 Main으로 설정해야 한다.
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class boj_15552 {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int testNum = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        int t = Integer.parseInt(br.readLine());
 
-        for (int testIdx = 0; testIdx < testNum; testIdx++) {
+        while (t-- > 0) {
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-
-            bw.write((a + b) + "\n");
+            sb.append(a + b).append("\n");
         }
 
+        System.out.print(sb);
         br.close();
-        bw.flush();
-        bw.close();
     }
+
 }
