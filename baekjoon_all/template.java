c3e987ef3a6eca8008005ace0073325097dc1061
홍창기 <hckhong@naver.com>
2024-02-04 19:07:35 +0900

    Create template.java

---
+++ baekjoon_all/template.java
@@ -0,0 +1,18 @@
+// Solve 2024-
+
+// 백준에 제출할 때는 class 이름을 Main으로 설정해야 한다.
+// SWEA에 제출할 때는 class 이름을 Solution으로 설정해야 한다.
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class template {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        br.close();
+    }
+
+}
