b6eb49b22868695fabd1d84f656068bde999fc9e
홍창기 <hckhong@naver.com>
2024-02-10 16:31:19 +0900

    Update 2024-02-09

--- baekjoon_all/09000+/boj_9012.java
+++ baekjoon_all/09000+/boj_9012.java
@@ -1,21 +1,23 @@
 // Solve 2024-01-21
+// Update 2024-02-09
 
-// 백준에 제출할 때는 class 이름을 Main으로 설정해야 함.
+// 백준에 제출할 때는 class 이름을 Main으로 설정해야 한다.
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class boj_9012 {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        int testCase = Integer.parseInt(br.readLine());
+        int t = Integer.parseInt(br.readLine());
 
-        while (testCase-- > 0) {
+        while (t-- > 0) {
             String ps = br.readLine();
             int psDepth = 0;
-            boolean isVps = true;
+            boolean validPs = true;
 
             for (int i = 0; i < ps.length(); i++) {
                 char ch = ps.charAt(i);
@@ -27,19 +29,20 @@ public class boj_9012 {
                 }
 
                 if (psDepth < 0) {
-                    isVps = false;
+                    validPs = false;
                     break;
                 }
             }
 
             if (psDepth != 0) {
-                isVps = false;
+                validPs = false;
             }
 
-            sb.append(isVps ? "YES\n" : "NO\n");
+            sb.append(validPs ? "YES\n" : "NO\n");
         }
 
-        System.out.println(sb);
+        System.out.print(sb);
         br.close();
     }
+
 }
