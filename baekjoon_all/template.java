6abaf1d7073b24705dcd6e7505a5071591221cbe
홍창기 <hckhong@naver.com>
2024-02-18 18:07:53 +0900

    Update template.java (정올 주석 추가)

--- baekjoon_all/template.java
+++ baekjoon_all/template.java
@@ -3,6 +3,7 @@
 // 백준에 제출할 때는 class 이름을 Main으로 설정해야 한다.
 // SWEA에 제출할 때는 class 이름을 Solution으로 설정해야 한다.
 // 코드트리에 제출할 때는 class 이름을 Main으로 설정해야 한다.
+// 정올에 제출할 때는 class 이름을 Main으로 설정해야 한다.
 
 import java.io.BufferedReader;
 import java.io.IOException;
