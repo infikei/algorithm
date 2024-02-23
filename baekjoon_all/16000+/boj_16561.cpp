b63de446d6f55dc2e87f498018a65fbed8640320
홍창기 <hckhong@naver.com>
2024-02-23 09:56:25 +0900

    Update 2024-02-22

--- baekjoon_all/16000+/boj_16561.cpp
+++ baekjoon_all/16000+/boj_16561.cpp
@@ -1,15 +1,22 @@
-#include <iostream>
-#define fastio ios_base::sync_with_stdio(false);cout.tie(NULL);cin.tie(NULL); // boj_15552.cpp
+// Solve 2023-01-03
+// Update 2024-02-22
+
+#include <bits/stdc++.h>
 using namespace std;
 
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL); // boj_15552.cpp
+#define SETPRECISION(n) cout << fixed;cout.precision(n); // boj_1008.cpp
+#define SIZE(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
+using ll = long long;
+
 int main() {
-    fastio;
+    FASTIO;
 
-    int N;
-    cin >> N;
+    int n;
+    cin >> n;
 
-    int N3 = N / 3;
-    int ans = (N3 - 2) * (N3 - 1) / 2;
+    int ans = (n / 3 - 1) * (n / 3 - 2) / 2;
     cout << ans << '\n';
 
     return 0;
