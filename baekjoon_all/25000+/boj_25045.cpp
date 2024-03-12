37ddd19f7e2c66df0aa330beb2e3f560e09aede5
홍창기 <hckhong@naver.com>
2024-03-12 14:20:25 +0900

    Update 2024-03-10

--- baekjoon_all/25000+/boj_25045.cpp
+++ baekjoon_all/25000+/boj_25045.cpp
@@ -1,35 +1,41 @@
-#include <iostream>
-#include <queue>
-#define fastio ios_base::sync_with_stdio(false);cout.tie(NULL);cin.tie(NULL); // boj_15552.cpp
-using namespace std;
-using ull = unsigned long long;
+// Solve 2022-12-21
+// Update 2024-03-10
 
-int N, M;
-ull ans;
-priority_queue<ull> A;
-priority_queue<ull, vector<ull>, greater<ull> > B;
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL); // boj_15552.cpp
+#define SETPRECISION(n) cout << fixed;cout.precision(n); // boj_1008.cpp
+#define SIZE(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
+using ll = long long;
+
+int a[200000];
+int b[200000];
 
 int main() {
-    fastio;
+    FASTIO;
 
-    cin >> N >> M;
+    int n, m;
+    cin >> n >> m;
 
-    ull tmp;
-    for (int i = 0; i < N; i++) {
-        cin >> tmp;
-        A.push(tmp);
-    }
-    for (int i = 0; i < M; i++) {
-        cin >> tmp;
-        B.push(tmp);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
     }
 
-    while (!A.empty() && !B.empty()) {
-        if (A.top() < B.top()) {
-            break;
-        }
-        ans += (A.top() - B.top());
-        A.pop(); B.pop();
+    for (int i = 0; i < m; i++) {
+        cin >> b[i];
+    }
+
+    sort(a, a + n, greater<int>());
+    sort(b, b + m);
+
+    ll ans = 0;
+
+    for (int i = 0; i < min(n, m); i++) {
+        if (a[i] <= b[i]) break;
+
+        ans += a[i] - b[i];
     }
 
     cout << ans << '\n';
