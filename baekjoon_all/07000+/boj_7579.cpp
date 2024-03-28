1be3eaf4263257b0e831235b63573dcc71c681ec
홍창기 <hckhong@naver.com>
2024-03-28 15:15:58 +0900

    Update 2024-03-27

--- baekjoon_all/07000+/boj_7579.cpp
+++ baekjoon_all/07000+/boj_7579.cpp
@@ -1,5 +1,5 @@
 // Solve 2023-01-05
-// Update 2023-11-28
+// Update 2024-03-27
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -16,44 +16,29 @@ int main() {
     int n, m;
     cin >> n >> m;
 
-    int memory[101];
+    int memory[100];
 
-    for (int i = 1; i <= n; i++) {
+    for (int i = 0; i < n; i++) {
         cin >> memory[i];
     }
 
-    int cost[101];
-    int cost_sum = 0;
+    int cost[100];
 
-    for (int i = 1; i <= n; i++) {
+    for (int i = 0; i < n; i++) {
         cin >> cost[i];
-
-        cost_sum += cost[i];
     }
 
-    // dp 배열 : app번째 앱까지 확인했을 때 c의 비용으로 확보할 수 있는 최대한의 메모리를 저장하는 배열
-    vector<vector<int>> dp(101, vector<int>(10100, 0));
+    // memo[c] : c의 비용으로 확보할 수 있는 최대한의 메모리
+    int memo[10001] = { 0 };
 
-    for (int app = 1; app <= n; app++) {
-        for (int c = 0; c < cost[app]; c++) {
-            dp[app][c] = dp[app - 1][c];
-        }
-
-        for (int c = cost[app]; c <= cost_sum; c++) {
-            dp[app][c] = max(dp[app - 1][c], dp[app - 1][c - cost[app]] + memory[app]);
+    for (int i = 0; i < n; i++) {
+        for (int j = 10000; j >= cost[i]; j--) {
+            memo[j] = max(memo[j], memo[j - cost[i]] + memory[i]);
         }
     }
 
-    int ans = 0;
-
-    for (int c = 0; c <= cost_sum; c++) {
-        if (dp[n][c] >= m) {
-            ans = c;
-            break;
-        }
-    }
-
-    cout << ans << '\n';
+    int idx = lower_bound(memo, memo + 10001, m) - memo;
+    cout << idx << '\n';
 
     return 0;
 }
--- baekjoon_all/12000+/boj_12865.cpp
+++ baekjoon_all/12000+/boj_12865.cpp
@@ -1,47 +1,37 @@
-#include <iostream>
+// Solve 2022-08-03
+// Update 2024-03-27
+
+#include <bits/stdc++.h>
 using namespace std;
 
-int Weight[100], Value[100];
-int DP[101][100001];
-int N, K, ans;
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL); // boj_15552.cpp
+#define SETPRECISION(n) cout << fixed;cout.precision(n); // boj_1008.cpp
+#define SIZE(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
+using ll = long long;
 
-void input() {
-    cin >> N >> K;
-    for (int i = 0; i < N; i++) {
-        cin >> Weight[i] >> Value[i];
-    }
-}
+int memo[100001];
 
-void solve() {
-    for (int n = 0; n <= N; n++) {
-        DP[n][0] = 0;
-    }
-    for (int k = 0; k <= K; k++) {
-        DP[0][k] = 0;
-    }
+int main() {
+    FASTIO;
 
-    for (int k = 1; k <= K; k++) {
-        for (int n = 1; n <= N; n++) {
-            if (Weight[n - 1] > k) {
-                DP[n][k] = DP[n - 1][k];
-            }
-            else {
-                DP[n][k] = max(DP[n - 1][k - Weight[n - 1]] + Value[n - 1], DP[n - 1][k]);
+    int n, k;
+    cin >> n >> k;
+
+    for (int i = 0; i < n; i++) {
+        int wi, vi;
+        cin >> wi >> vi;
+
+        for (int j = k; j >= wi; j--) {
+            int tmp = memo[j - wi] + vi;
+
+            if (tmp > memo[j]) {
+                memo[j] = tmp;
             }
         }
     }
-}
 
-int main() {
-    ios_base::sync_with_stdio(false); // C++와 C 두 표준 입출력 동기화를 해제한다.
-    cout.tie(NULL);
-    cin.tie(NULL);                    // 입력과 출력이 묶여있는 것을 풀어준다.
-
-    input();
-
-    solve();
-
-    cout << DP[N][K] << '\n';
+    cout << memo[k] << '\n';
 
     return 0;
 }
