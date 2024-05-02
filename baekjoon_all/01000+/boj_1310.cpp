258dd4b8a02f2ca2eac49a63ec6eb568ed7b2ede
홍창기 <hckhong@naver.com>
2024-05-03 00:11:22 +0900

    Update 2024-05-02

--- baekjoon_all/01000+/boj_1310.cpp
+++ baekjoon_all/01000+/boj_1310.cpp
@@ -1,9 +1,10 @@
 // Solve 2023-05-29
+// Update 2024-05-02
 
 #include <bits/stdc++.h>
 using namespace std;
 
-#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL); // boj_15552.cpp
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL); // boj_15552.cpp
 #define SETPRECISION(n) cout << fixed;cout.precision(n); // boj_1008.cpp
 #define SIZE(v) (int)v.size()
 #define ALL(v) v.begin(),v.end()
@@ -11,73 +12,76 @@ using ll = long long;
 
 struct Point{
     ll x, y;
+
     Point(ll nx = 0, ll ny = 0) : x(nx), y(ny) {}
+
     Point operator-(const Point &rhs) const {
         return { x - rhs.x, y - rhs.y };
     }
+
     bool operator<(const Point &rhs) const {
-        if (x != rhs.x) {
-            return x < rhs.x;
-        }
+        if (x != rhs.x) return x < rhs.x;
         return y < rhs.y;
     }
 };
 
-int n;
 Point points[100000];
 
-ll calc_cross(const Point &a, const Point &b) {
-    return a.x * b.y - b.x * a.y;
+ll get_cross(Point p1, Point p2) {
+    return p1.x * p2.y - p2.x * p1.y;
 }
 
-int calc_ccw(const Point &a, const Point &b, const Point &c) {
-    ll ccw = calc_cross(b - a, c - a);
+int get_ccw_sign(Point p1, Point p2, Point p3) {
+    ll ccw = get_cross(p2 - p1, p3 - p1);
+
     if (ccw > 0) return 1;
     if (ccw < 0) return -1;
     return 0;
 }
 
-bool cmp_ccw_x_y(const Point &a, const Point &b) {
-    int ccw = calc_ccw(points[0], a, b);
-    if (ccw != 0) {
-        return ccw > 0;
-    }
-    return a < b;
+bool cmp_ccw_x_y(Point p1, Point p2) {
+    int ccw_sign = get_ccw_sign(points[0], p1, p2);
+
+    if (ccw_sign != 0) return ccw_sign > 0;
+    return p1 < p2;
 }
 
-ll calc_dist2(const Point &a, const Point &b) {
-    ll dx = b.x - a.x;
-    ll dy = b.y - a.y;
+ll get_dist2(Point p1, Point p2) {
+    ll dx = p1.x - p2.x;
+    ll dy = p1.y - p2.y;
     return dx * dx + dy * dy;
 }
 
 int main() {
     FASTIO;
 
+    int n;
     cin >> n;
+
     for (int i = 0; i < n; i++) {
         cin >> points[i].x >> points[i].y;
     }
 
     swap(points[0], *min_element(points, points + n));
     sort(points + 1, points + n, cmp_ccw_x_y);
-
     vector<Point> convex_hull;
+
     for (int i = 0; i < n; i++) {
         while (SIZE(convex_hull) >= 2) {
-            Point back2 = convex_hull.back();
+            Point back = convex_hull.back();
             convex_hull.pop_back();
-            Point back1 = convex_hull.back();
 
-            if (calc_ccw(back1, back2, points[i]) > 0) {
-                convex_hull.push_back(back2);
+            if (get_ccw_sign(convex_hull.back(), back, points[i]) > 0) {
+                convex_hull.push_back(back);
                 break;
             }
         }
+
         convex_hull.push_back(points[i]);
     }
 
     ll max_dist2 = 0;
+
     for (int i = 0, j = 1; i < SIZE(convex_hull); i++) {
         int ni = i + 1;
         if (ni == SIZE(convex_hull)) ni = 0;
@@ -88,11 +92,11 @@ int main() {
             if (nj == SIZE(convex_hull)) nj = 0;
             Point vj = convex_hull[nj] - convex_hull[j];
 
-            if (calc_ccw(Point(0, 0), vi, vj) <= 0) break;
+            if (get_ccw_sign(Point(0, 0), vi, vj) <= 0) break;
             j = nj;
         }
 
-        ll now_dist2 = calc_dist2(convex_hull[i], convex_hull[j]);
+        ll now_dist2 = get_dist2(convex_hull[i], convex_hull[j]);
         max_dist2 = max(max_dist2, now_dist2);
     }
 
