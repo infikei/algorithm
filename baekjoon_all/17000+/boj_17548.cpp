99462f193914c57470073fba5d9c94313723c9ce
홍창기 <hckhong@naver.com>
2024-05-08 00:29:33 +0900

    Solve 2024-05-07

---
+++ baekjoon_all/17000+/boj_17548.cpp
@@ -0,0 +1,29 @@
+// Solve 2024-05-07
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL); // boj_15552.cpp
+#define SETPRECISION(n) cout << fixed;cout.precision(n); // boj_1008.cpp
+#define SIZE(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
+using ll = long long;
+
+int main() {
+    FASTIO;
+
+    string s;
+    cin >> s;
+
+    int i = (SIZE(s) - 2) * 2;
+
+    cout << 'h';
+
+    while (i-- > 0) {
+        cout << 'e';
+    }
+
+    cout << "y\n";
+
+    return 0;
+}
