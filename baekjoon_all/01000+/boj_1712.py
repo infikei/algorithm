8599cfd692f37a34bcc8aad701bd53a81e8625fd
홍창기 <hckhong@naver.com>
2024-05-15 00:44:15 +0900

    Update 2024-05-14

--- baekjoon_all/01000+/boj_1712.py
+++ baekjoon_all/01000+/boj_1712.py
@@ -1,5 +1,9 @@
+# Solve 2022-05-21
+# Update 2024-05-14
+
 a, b, c = map(int, input().split())
+
 if b >= c:
     print(-1)
 else:
-    print(int(a/(c-b))//1+1)
+    print(int(a / (c - b)) // 1 + 1)
