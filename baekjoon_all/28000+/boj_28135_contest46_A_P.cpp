da85fe0e26c6191eb24aaaca44971631f9f569a0
홍창기 <hckhong@naver.com>
2024-05-20 00:35:19 +0900

    Update 2024-05-19

--- baekjoon_all/28000+/boj_28135_contest46_A_P.cpp
+++ baekjoon_all/28000+/boj_28135_contest46_A_P.cpp
@@ -1,9 +1,10 @@
 // Solve 2023-05-27
+// Update 2024-05-19
 
 #include <bits/stdc++.h>
 using namespace std;
 
-#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL); // boj_15552.cpp
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL); // boj_15552.cpp
 #define SETPRECISION(n) cout << fixed;cout.precision(n); // boj_1008.cpp
 #define SIZE(v) (int)v.size()
 #define ALL(v) v.begin(),v.end()
@@ -13,24 +14,26 @@ int main() {
     FASTIO;
 
     vector<int> v;
+
     for (int i = 0; i < 10000; i++) {
-        v.push_back(i * 100 + 50);
-        v.push_back(500000 + i);
+        v.push_back(i * 100 + 50); // ????50
+        v.push_back(500000 + i); // 50????
     }
 
     for (int i = 0; i < 1000; i++) {
         for (int j = 0; j < 10; j++) {
-            v.push_back(i * 1000 + 500 + j);
-            v.push_back(j * 100000 + 50000 + i);
+            v.push_back(i * 1000 + 500 + j); // ???50?
+            v.push_back(j * 100000 + 50000 + i); // ?50???
         }
     }
 
     for (int i = 0; i < 100; i++) {
         for (int j = 0; j < 100; j++) {
-            v.push_back(i * 10000 + 5000 + j);
+            v.push_back(i * 10000 + 5000 + j); // ??50??
         }
     }
 
+    // 정렬 후 중복 제거
     sort(v.begin(), v.end());
     v.erase(unique(v.begin(), v.end()), v.end());
 
