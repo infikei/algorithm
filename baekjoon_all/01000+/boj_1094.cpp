6f3bb052e15a83fbc6a6f95eb8d5242c68f46a7b
홍창기 <hckhong@naver.com>
2024-06-02 09:14:30 +0900

    Update 2024-06-01

--- baekjoon_all/01000+/boj_1094.cpp
+++ baekjoon_all/01000+/boj_1094.cpp
@@ -1,17 +1,27 @@
-#include <iostream>
+// Solve 2022-07-30
+// Update 2024-06-01
+
+#include <bits/stdc++.h>
 using namespace std;
 
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL); // boj_15552.cpp
+#define SETPRECISION(n) cout << fixed;cout.precision(n); // boj_1008.cpp
+#define SIZE(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
+using ll = long long;
+
 int main() {
-    ios_base::sync_with_stdio(false); // C++와 C 두 표준 입출력 동기화를 해제한다.
-    cout.tie(NULL);
-    cin.tie(NULL);                    // 입력과 출력이 묶여있는 것을 풀어준다.
+    FASTIO;
 
-    int X, ans = 0;
-    cin >> X;
+    int x;
+    cin >> x;
 
-    while (X > 0) {
-        ans += X % 2;
-        X = X / 2;
+    int ans = 0;
+
+    while (x != 0) {
+        if (x & 1) ans++;
+
+        x >>= 1;
     }
 
     cout << ans << '\n';
