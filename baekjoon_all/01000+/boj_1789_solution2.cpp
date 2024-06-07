f422eea025f58305f0a8a9188926199ebe4e6f37
홍창기 <hckhong@naver.com>
2024-06-07 11:11:26 +0900

    Update 2024-06-06

--- baekjoon_all/01000+/boj_1789_solution2.cpp
+++ baekjoon_all/01000+/boj_1789_solution2.cpp
@@ -1,21 +1,15 @@
 // Solve 2022-11-08
-// Update 2023-04-30
+// Update 2024-06-06
 
 #include <bits/stdc++.h>
 using namespace std;
 
-#ifdef BOJ
-#define BOJTEST(x) ((void)0)
-#else
-#define BOJTEST(x) cout << "[Debug] " << #x << ':' << x << '\n'
-#endif
-#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL); // boj_15552.cpp
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL); // boj_15552.cpp
 #define SETPRECISION(n) cout << fixed;cout.precision(n); // boj_1008.cpp
 #define SIZE(v) (int)v.size()
 #define ALL(v) v.begin(),v.end()
 using ll = long long;
 using uint = unsigned int;
-using ull = unsigned long long;
 
 int main() {
     FASTIO;
@@ -23,10 +17,13 @@ int main() {
     uint s;
     cin >> s;
 
-    uint left = 1, right = 92682;
-    while (left + 1 < right) {
-        uint mid = (left + right) >> 1;
+    uint low = 1;
+    uint high = 92682;
+
+    while (low + 1 < high) {
+        uint mid = (low + high) >> 1;
         uint val;
+
         if (mid & 1) {
             val = (mid + 1) / 2 * mid;
         }
@@ -35,13 +32,14 @@ int main() {
         }
 
         if (s >= val) {
-            left = mid;
+            low = mid;
         }
         else {
-            right = mid;
+            high = mid;
         }
     }
-    cout << left << '\n';
+
+    cout << low << '\n';
 
     return 0;
 }
