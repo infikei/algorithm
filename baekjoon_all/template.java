962ad1a0fbe8bfa531b1826ac3a4226d94ec44a8
홍창기 <hckhong@naver.com>
2024-06-21 01:33:21 +0900

    Update template.java (주석 추가)

--- baekjoon_all/template.java
+++ baekjoon_all/template.java
@@ -3,7 +3,9 @@
 // 백준에 제출할 때는 class 이름을 Main으로 설정해야 한다.
 // SWEA에 제출할 때는 class 이름을 Solution으로 설정해야 한다.
 // 코드트리에 제출할 때는 class 이름을 Main으로 설정해야 한다.
-// LeetCode에 제출할 때는 class 이름을 Solution으로 설정해야 한다.
+// 프로그래머스에 제출할 때는 class 이름을 Solution으로 설정해야 한다.
+// 소프티어에 제출할 때는 class 이름을 Main으로 설정해야 한다.
+// 리트코드에 제출할 때는 class 이름을 Solution으로 설정해야 한다.
 // 정올에 제출할 때는 class 이름을 Main으로 설정해야 한다.
 
 import java.io.BufferedReader;
