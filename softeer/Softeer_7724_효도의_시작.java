ff9de34d5b309a637a39a09f66aa789b9bf78a5c
홍창기 <hckhong@naver.com>
2024-06-23 20:29:20 +0900

    Solve 2024-06-21

---
+++ softeer/Softeer_7724_효도의_시작.java
@@ -0,0 +1,11 @@
+// Solve 2024-06-21
+
+// 소프티어에 제출할 때는 class 이름을 Main으로 설정해야 한다.
+
+public class Softeer_7724_효도의_시작 {
+
+    public static void main(String[] args) {
+        System.out.println("엄마 아빠 사랑해요!");
+    }
+
+}
