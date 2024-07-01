796a099aa185433994f57bcf51037597e6396be4
홍창기 <hckhong@naver.com>
2024-07-01 22:15:02 +0900

    Solve 2024-06-29

---
+++ baekjoon_all/24000+/boj_24724.cpp
@@ -0,0 +1,21 @@
+// Solve 2024-06-29
+
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    for (int x = 1; x <= t; x++) {
+        cout << "Material Management " << x << '\n';
+        cout << "Classification ---- End!\n";
+    }
+
+    return 0;
+}
