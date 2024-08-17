b255fcfe879f980f20693f8b3e1590ed554be8fd
홍창기 <hckhong@naver.com>
2024-08-17 18:37:58 +0900

    Solve 2024-08-08

---
+++ baekjoon_all/21000+/boj_21553.java
@@ -0,0 +1,20 @@
+// Solve 2024-08-08
+
+// 백준에 제출할 때는 class 이름을 Main으로 설정해야 한다.
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class boj_21553 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String a = br.readLine();
+        String p = br.readLine();
+
+        System.out.println(p);
+        br.close();
+    }
+
+}
