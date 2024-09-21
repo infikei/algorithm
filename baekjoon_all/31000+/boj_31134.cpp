4214d3cbb947a203349804f505c2d85b23f48826
홍창기 <hckhong@naver.com>
2024-09-21 16:51:20 +0900

    Solve 2024-09-17

---
+++ baekjoon_all/31000+/boj_31134.cpp
@@ -0,0 +1,24 @@
+// Solve 2024-09-17
+
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+using ll = long long;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while (t-- > 0) {
+        ll x;
+        cin >> x;
+
+        cout << x * 2 - 1 << '\n';
+    }
+
+    return 0;
+}
