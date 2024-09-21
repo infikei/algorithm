76099658edab88888e61848bd5f24695e35a1f15
홍창기 <hckhong@naver.com>
2024-09-21 16:51:35 +0900

    Solve 2024-09-19

---
+++ baekjoon_all/20000+/boj_20336.cpp
@@ -0,0 +1,27 @@
+// Solve 2024-09-19
+
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    int m;
+    cin >> m;
+    cout << m << '\n';
+
+    for (int i = 0; i < m; i++) {
+        string menu;
+        cin >> menu;
+        cout << menu << '\n';
+    }
+
+    return 0;
+}
