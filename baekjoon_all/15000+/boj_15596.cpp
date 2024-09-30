aa744fd2bbdc03d2c0944c54b94da4370ee78517
홍창기 <hckhong@naver.com>
2024-09-30 23:45:03 +0900

    Update 2024-09-29

--- baekjoon_all/15000+/boj_15596.cpp
+++ baekjoon_all/15000+/boj_15596.cpp
@@ -1,9 +1,15 @@
-#include <vector>
+// Solve 2022-06-03
+// Update 2024-09-29
 
-long long sum(std::vector<int> &a) {
+#include <vector>
+using namespace std;
+
+long long sum(vector<int> &a) {
     long long ans = 0;
+
     for (int i = 0; i < a.size(); i++) {
         ans += a[i];
     }
+
     return ans;
 }
