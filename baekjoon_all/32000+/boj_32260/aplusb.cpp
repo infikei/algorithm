e48f6996aeca1d014e9c94dee28cb3c1ce9dc0da
홍창기 <hckhong@naver.com>
2024-10-02 08:54:22 +0900

    Solve 2024-09-30

---
+++ baekjoon_all/32000+/boj_32260/aplusb.cpp
@@ -0,0 +1,7 @@
+// Solve 2024-09-30
+
+#include "aplusb.h"
+
+int sum(int A, int B) {
+    return A + B;
+}
---
+++ baekjoon_all/32000+/boj_32260/grader.cpp
@@ -0,0 +1,16 @@
+#include "aplusb.h"
+#include <cassert>
+#include <cstdio>
+
+int main() {
+  int A, B;
+  assert(2 == scanf("%d%d", &A, &B));
+  fclose(stdin);
+
+  int R = sum(A, B);
+
+  printf("%d\n", R);
+  fclose(stdout);
+
+  return 0;
+}
