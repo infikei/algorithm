223ddc7bf6d3e5a8be17d83bf03b26d49f37e78a
홍창기 <hckhong@naver.com>
2024-10-13 00:32:03 +0900

    Update 2024-10-12

--- baekjoon_all/17000+/boj_17828.cpp
+++ baekjoon_all/17000+/boj_17828.cpp
@@ -1,13 +1,30 @@
 // Solve 2024-04-09
+// Update 2024-10-12
 
 #include <bits/stdc++.h>
-using namespace std;
 
-#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL); // boj_15552.cpp
-#define SETPRECISION(n) cout << fixed;cout.precision(n); // boj_1008.cpp
-#define SIZE(v) (int)v.size()
-#define ALL(v) v.begin(),v.end()
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define size(v) (int)v.size()
+#define all(v) v.begin(),v.end()
+#define setw(n, c) cout << setw(n) << setfill(c);
+#define setp(n) cout << fixed << setprecision(n);
+#define printw(x) cout << (x) << ' ';
+#define println(x) cout << (x) << '\n';
+
+#ifdef BOJ
+#define testPrint(x) ((void)0)
+#else
+#define testPrint(x) cout << "[D] " << #x << ':' << x << '\n'
+#endif
+
+using namespace std;
 using ll = long long;
+using uint = unsigned int;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+
+const double PI = M_PI;
 
 int main() {
     FASTIO;
@@ -15,27 +32,30 @@ int main() {
     int n, x;
     cin >> n >> x;
 
+    int z = (x - n) / 25;
+    int b = (x - n) % 25;
+
     if (n <= x && x <= n * 26) {
         int z = (x - n) / 25;
-        int r = (x - n) % 25;
+        int b = (x - n) % 25;
         int a = n - z - 1;
 
-        for (int i = 0; i < a; i++) {
+        while (a-- > 0) {
             cout << 'A';
         }
 
-        if (n > z) {
-            cout << (char) ('A' + r);
+        if (z < n) {
+            cout << (char) ('A' + b);
         }
 
-        for (int i = 0; i < z; i++) {
+        while (z-- > 0) {
             cout << 'Z';
         }
 
         cout << '\n';
     }
     else {
-        cout << "!\n";
+        println("!");
     }
 
     return 0;
