9e104fd3f2d2ee069b54c2bec239d5ee3017065f
홍창기 <hckhong@naver.com>
2024-10-16 04:20:53 +0900

    Update 2024-10-14

--- baekjoon_all/01000+/boj_1949.java
+++ baekjoon_all/01000+/boj_1949.java
@@ -1,4 +1,5 @@
 // Solve 2024-02-04
+// Update 2024-10-14
 
 // 백준에 제출할 때는 class 이름을 Main으로 설정해야 한다.
 
@@ -6,44 +7,43 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class boj_1949 {
 
     static int[] people;
-    static List<List<Integer>> adj;
-    static boolean[] visited;
-    static int[][] dp;
+    static ArrayList<Integer>[] adj;
 
-    static void dfs(int nowNode) {
-        visited[nowNode] = true;
-        dp[nowNode][0] = 0;
-        dp[nowNode][1] = people[nowNode];
+    static int[] dfs(int cur, int par) {
+        int[] res = new int[2];
+        res[1] = people[cur];
 
-        for (int nextNode : adj.get(nowNode)) {
-            if (visited[nextNode]) continue;
+        for (int nxt : adj[cur]) {
+            if (nxt == par) continue;
 
-            dfs(nextNode);
-            dp[nowNode][0] += Math.max(dp[nextNode][0], dp[nextNode][1]);
-            dp[nowNode][1] += dp[nextNode][0];
+            int[] nxt_res = dfs(nxt, cur);
+            res[0] += Math.max(nxt_res[0], nxt_res[1]);
+            res[1] += nxt_res[0];
         }
+
+        return res;
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
+
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         people = new int[n + 1];
 
-        for (int i = 1; i <= n; i++) {
-            people[i] = Integer.parseInt(st.nextToken());
+        for (int u = 1; u <= n; u++) {
+            people[u] = Integer.parseInt(st.nextToken());
         }
 
-        adj = new ArrayList<List<Integer>>();
+        adj = new ArrayList[n + 1];
 
-        for (int u = 0; u <= n; u++) {
-            adj.add(new ArrayList<Integer>());
+        for (int u = 1; u <= n; u++) {
+            adj[u] = new ArrayList<Integer>();
         }
 
         for (int i = 1; i < n; i++) {
@@ -51,15 +51,13 @@ public class boj_1949 {
             int u = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
 
-            adj.get(u).add(v);
-            adj.get(v).add(u);
+            adj[u].add(v);
+            adj[v].add(u);
         }
 
-        visited = new boolean[n + 1];
-        dp = new int[n + 1][2];
-        dfs(1);
+        int[] ans = dfs(1, 0);
 
-        System.out.println(Math.max(dp[1][0], dp[1][1]));
+        System.out.println(Math.max(ans[0], ans[1]));
         br.close();
     }
 
--- baekjoon_all/20000+/boj_20055_solution1.java
+++ baekjoon_all/20000+/boj_20055_solution1.java
@@ -1,4 +1,5 @@
 // Solve 2024-02-20
+// Update 2024-10-14
 
 // 백준에 제출할 때는 class 이름을 Main으로 설정해야 한다.
 
@@ -7,7 +8,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class boj_20055 {
+public class boj_20055_solution1 {
 
     static class Position {
         int durability;
