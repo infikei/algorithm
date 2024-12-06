6028e3ce9f212bc86deb31629a35684d30d1036a
홍창기 <hckhong@naver.com>
2024-12-07 04:27:47 +0900

    Solve 2024-12-04

---
+++ baekjoon_all/32000+/boj_32184.cpp
@@ -0,0 +1,46 @@
+// Solve 2024-12-04
+
+#include <bits/stdc++.h>
+
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define size(v) (int)v.size()
+#define all(v) v.begin(),v.end()
+#define setw(n, c) cout << setw(n) << setfill(c);
+#define setp(n) cout << fixed << setprecision(n);
+#define printw(x) cout << (x) << ' ';
+#define println(x) cout << (x) << '\n';
+
+#ifdef BOJ
+#define testPrint(x) ((void)0)
+#else
+#define testPrint(x) cout << "[D] " << #x << ':' << x << '\n'
+#endif
+
+using namespace std;
+using ll = long long;
+using uint = unsigned int;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+
+const double PI = M_PI;
+
+int main() {
+    FASTIO;
+
+    int a, b;
+    cin >> a >> b;
+
+    if (b % 2 == 1) {
+        b++;
+    }
+
+    if (a % 2 == 0) {
+        a--;
+    }
+
+    int ans = (b - a + 1) / 2;
+    println(ans);
+
+    return 0;
+}
