072df60c65d726c2e066e1c28a8dd5c5c65172c5
홍창기 <hckhong@naver.com>
2024-12-13 01:21:37 +0900

    Update 2024-12-12

--- baekjoon_all/11000+/boj_11066.cpp
+++ baekjoon_all/11000+/boj_11066.cpp
@@ -1,39 +1,68 @@
-#include <iostream>
-#define fastio ios_base::sync_with_stdio(false);cout.tie(NULL);cin.tie(NULL); // boj_15552.cpp
-using namespace std;
+// Solve 2023-01-01
+// Update 2024-12-12
 
-const int K_MAX = 500;
-int file[K_MAX + 1], file_sum[K_MAX + 1], dp[K_MAX + 1][K_MAX + 1];
+#include <bits/stdc++.h>
+
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define size(v) (int)v.size()
+#define all(v) v.begin(),v.end()
+#define setw(n, c) cout << setw(n) << setfill(c);
+#define setp(n) cout << fixed << setprecision(n);
+#define printw(x) cout << (x) << ' ';
+#define println(x) cout << (x) << '\n';
+
+#ifdef BOJ
+#define testPrint(x) ((void)0)
+#else
+#define testPrint(x) cout << "[D] " << #x << ':' << x << '\n'
+#endif
+
+using namespace std;
+using ll = long long;
+using uint = unsigned int;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+
+const double PI = M_PI;
+
+int file[501];
+int file_sum[501];
+int memo[501][501];
 
 int main() {
-    fastio;
+    FASTIO;
 
-    int T;
-    cin >> T;
+    int t;
+    cin >> t;
 
-    for (int t = 0; t < T; t++) {
-        int K;
-        cin >> K;
+    while (t-- > 0) {
+        int k;
+        cin >> k;
 
-        for (int i = 1; i <= K; i++) {
+        for (int i = 1; i <= k; i++) {
             cin >> file[i];
+        }
+
+        for (int i = 1; i <= k; i++) {
             file_sum[i] = file_sum[i - 1] + file[i];
         }
 
-        for (int ilen = 2; ilen <= K; ilen++) {
-            for (int ib = 1; ib <= K + 1 - ilen; ib++) {
-                int ie = ib + ilen - 1;
+        for (int len = 2; len <= k; len++) {
+            for (int left = 1; left <= k + 1 - len; left++) {
+                int right = left + len - 1;
 
-                dp[ib][ie] = dp[ib][ib] + dp[ib + 1][ie];
-                for (int k = ib + 2; k < ib + ilen; k++) {
-                    dp[ib][ie] = min(dp[ib][ie], dp[ib][k - 1] + dp[k][ie]);
+                memo[left][right] = memo[left + 1][right];
+
+                for (int mid = left + 2; mid <= right; mid++) {
+                    memo[left][right] = min(memo[left][right], memo[left][mid - 1] + memo[mid][right]);
                 }
 
-                dp[ib][ie] += file_sum[ie] - file_sum[ib - 1];
+                memo[left][right] += file_sum[right] - file_sum[left - 1];
             }
         }
 
-        cout << dp[1][K] << '\n';
+        println(memo[1][k]);
     }
 
     return 0;
