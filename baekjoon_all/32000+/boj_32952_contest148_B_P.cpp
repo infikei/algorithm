75f74bf843004e55566a58a7c3a549ef360c53ad
홍창기 <hckhong@naver.com>
2024-12-23 03:13:43 +0900

    Solve 2024-12-21

---
+++ baekjoon_all/32000+/boj_32952_contest148_B_P.cpp
@@ -0,0 +1,44 @@
+// Solve 2024-12-21
+
+#include <bits/stdc++.h>
+
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define size(v) (int)v.size()
+#define all(v) v.begin(),v.end()
+#define setw(n, c) cout << setw(n) << setfill(c);
+#define setp(n) cout << fixed << setprecision(n);
+#define printw(x) cout << (x) << ' ';
+#define println(x) cout << (x) << '\n';
+
+#ifdef BOJ
+#define testPrint(x) ((void)0)
+#else
+#define testPrint(x) cout << "[D] " << #x << ':' << x << '\n'
+#endif
+
+using namespace std;
+using ll = long long;
+using uint = unsigned int;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+
+const double PI = M_PI;
+
+int main() {
+    FASTIO;
+
+    ll r, k, m;
+    cin >> r >> k >> m;
+
+    ll x = m / k;
+
+    if (x >= 40) {
+        println(0);
+    }
+    else {
+        println(r >> x);
+    }
+
+    return 0;
+}
