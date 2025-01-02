f2af5295fc0f3c1d8789dab2e4cf80401f8c25a9
홍창기 <hckhong@naver.com>
2025-01-02 14:29:10 +0900

    Solve 2025-01-01

---
+++ baekjoon_all/23000+/boj_23968.cpp
@@ -0,0 +1,68 @@
+// Solve 2025-01-01
+
+#include <bits/stdc++.h>
+
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define size(v) (int)v.size()
+#define all(v) v.begin(),v.end()
+#define setw(n, c) cout << setw(n) << setfill(c);
+#define setp(n) cout << fixed << setprecision(n);
+#define printw(x) cout << (x) << ' ';
+#define println(x) cout << (x) << '\n';
+
+#ifdef BOJ
+#define testPrint(x) ((void)0)
+#else
+#define testPrint(x) cout << "[D] " << #x << ':' << x << '\n'
+#endif
+
+using namespace std;
+using ll = long long;
+using uint = unsigned int;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+
+const double PI = M_PI;
+
+int arr[10000];
+
+int main() {
+    FASTIO;
+
+    int n, k;
+    cin >> n >> k;
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int swapped_cnt = 0;
+    bool ans_found = false;
+
+    for (int i = 0; i < n - 1; i++) {
+        bool swapped = false;
+
+        for (int j = 0; j < n - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+                swapped = true;
+
+                if (++swapped_cnt == k) {
+                    printw(arr[j]);
+                    println(arr[j + 1]);
+                    ans_found = true;
+                    break;
+                }
+            }
+        }
+
+        if (!swapped || ans_found) break;
+    }
+
+    if (!ans_found) {
+        println(-1);
+    }
+
+    return 0;
+}
