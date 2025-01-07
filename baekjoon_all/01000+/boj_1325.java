418ae709ffcc9078b073d90a37b130587f88cb5b
홍창기 <hckhong@naver.com>
2025-01-08 06:07:50 +0900

    Update 2025-01-06

--- baekjoon_all/01000+/boj_1325.java
+++ baekjoon_all/01000+/boj_1325.java
@@ -1,4 +1,5 @@
 // Solve 2024-03-06
+// Update 2025-01-06
 
 // 백준에 제출할 때는 class 이름을 Main으로 설정해야 한다.
 
@@ -8,16 +9,18 @@ import java.util.*;
 
 public class boj_1325 {
 
-    private static int[] hackCount = new int[10001];
-    private static boolean[] visited = new boolean[10001];
-    private static ArrayList<Integer>[] adj = new ArrayList[10001];
+    private static int n;
+    private static ArrayList<Integer>[] adj;
+    private static boolean[] visited;
+    private static int[] hackCount;
     private static ArrayDeque<Integer> bfsQue = new ArrayDeque<Integer>();
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
+        adj = new ArrayList[n + 1];
 
         for (int u = 1; u <= n; u++) {
             adj[u] = new ArrayList<>();
@@ -27,23 +30,20 @@ public class boj_1325 {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            adj[a].add(b);
+            adj[b].add(a);
         }
 
-        for (int u = 1; u <= n; u++) {
-            for (int u2 = 1; u2 <= n; u2++) {
-                visited[u2] = false;
-            }
+        visited = new boolean[n + 1];
+        hackCount = new int[n + 1];
 
-            bfs(u);
+        for (int u = 1; u <= n; u++) {
+            hackCount[u] = bfs(u);
         }
 
         int maxHackCount = 0;
 
         for (int u = 1; u <= n; u++) {
-            if (maxHackCount < hackCount[u]) {
-                maxHackCount = hackCount[u];
-            }
+            maxHackCount = Math.max(maxHackCount, hackCount[u]);
         }
 
         StringBuilder sb = new StringBuilder();
@@ -57,21 +57,28 @@ public class boj_1325 {
         System.out.println(sb);
     }
 
-    private static void bfs(int u) {
+    private static int bfs(int u) {
+        for (int u2 = 1; u2 <= n; u2++) {
+            visited[u2] = false;
+        }
+
         visited[u] = true;
         bfsQue.add(u);
+        int hacked = 1;
 
         while (!bfsQue.isEmpty()) {
             int cur = bfsQue.poll();
 
-            for (int next : adj[cur]) {
-                if (!visited[next]) {
-                    visited[next] = true;
-                    hackCount[next]++;
-                    bfsQue.add(next);
+            for (int nxt : adj[cur]) {
+                if (!visited[nxt]) {
+                    visited[nxt] = true;
+                    bfsQue.add(nxt);
+                    hacked++;
                 }
             }
         }
+
+        return hacked;
     }
 
 }
