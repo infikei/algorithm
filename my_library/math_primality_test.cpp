c5c59d79e1a61335c3a9968a8ee327e5fdeefbd9
홍창기 <hckhong@naver.com>
2025-01-21 20:47:06 +0900

    소수 판정, 에라토스테네스의 체

--- my_library/math_primality_test.cpp
+++ my_library/math_primality_test.cpp
@@ -1,37 +1,108 @@
-// 어떤 수가 소수(prime)인지 아닌지를 판별한다.
-
 #include <bits/stdc++.h>
+
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define size(v) (int)v.size()
+#define all(v) v.begin(),v.end()
+#define setw(n, c) cout << setw(n) << setfill(c);
+#define setp(n) cout << fixed << setprecision(n);
+#define printw(x) cout << (x) << ' ';
+#define println(x) cout << (x) << '\n';
+
+#ifdef BOJ
+#define testPrint(x) ((void)0)
+#else
+#define testPrint(x) cout << "[D] " << #x << ':' << x << '\n'
+#endif
+
 using namespace std;
-
-#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL); // boj_15552.cpp
-#define SETPRECISION(n) cout << fixed;cout.precision(n); // boj_1008.cpp
-#define SIZE(v) (int)v.size()
-#define ALL(v) v.begin(),v.end()
 using ll = long long;
+using uint = unsigned int;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
 
-bool is_prime(int k) {
-    if (k < 2) return false;
+const double PI = M_PI;
 
-    for (int i = 2, ie = sqrt(k); i <= ie; i++) {
-        if (k % i == 0) return false;
+/**
+ * @brief 주어진 정수가 소수인지 판별한다.
+ * 
+ * 이 함수는 주어진 정수가 소수인지 확인한다. 소수는 1과 자기 자신으로만 나누어떨어지는 1보다 큰 자연수이다.
+ * 
+ * @param n 판별할 정수
+ * @return true 주어진 정수가 양수이고 소수인 경우
+ * @return false 주어진 정수가 양수가 아니거나 소수가 아닌 경우
+ * @note 이 함수는 정수 k의 제곱근까지만 검사하여 최적화되었다.
+ */
+bool is_prime(ll n) {
+    if (n <= 1) return false;
+
+    ll sqrt_n = sqrt(n);
+
+    for (ll i = 2; i <= sqrt_n; i++) {
+        if (n % i == 0) {
+            return false;
+        }
     }
 
     return true;
 }
 
+/**
+ * @brief 에라토스테네스의 체를 사용하여 소수를 판별한다.
+ * 
+ * 이 함수는 에라토스테네스의 체 알고리즘을 이용하여 `max_num` 이하의 모든 소수를 판별한다.
+ * 반환되는 벡터의 각 인덱스 i는 정수 i가 소수인지 여부를 나타낸다.
+ * 
+ * @param max_num 소수를 판별할 최대 숫자 (0 이상)
+ * @return vector<bool> 크기가 `max_num + 1`인 벡터로,
+ *         인덱스 i가 true이면 i는 소수, false이면 소수가 아님을 의미한다.
+ * @note 0과 1은 항상 소수가 아니다.
+ */
+vector<bool> create_sieve_of_eratosthenes_v1(int max_num) {
+    vector<bool> sieve(max_num + 1, true);
+    sieve[0] = sieve[1] = false; // 0과 1은 소수가 아님을 기록한다.
+
+    for (int i = 2; i <= max_num; i++) {
+        if (!sieve[i]) continue; // i가 소수가 아니면 skip한다.
+
+        for (int j = i + i; j <= max_num; j += i) {
+            sieve[j] = false; // 소수가 아님을 기록한다.
+        }
+    }
+
+    return sieve;
+}
+
+/**
+ * @brief 에라토스테네스의 체를 사용하여 소수를 판별한다.
+ * 
+ * 이 함수는 에라토스테네스의 체 알고리즘을 이용하여 `max_num` 이하의 모든 소수를 판별한다.
+ * 반환되는 벡터의 각 인덱스 i는 정수 i가 소수인지 여부를 나타낸다.
+ * 
+ * @param max_num 소수를 판별할 최대 숫자 (0 이상)
+ * @return vector<bool> 크기가 `max_num + 1`인 벡터로,
+ *         인덱스 i가 true이면 i는 소수, false이면 소수가 아님을 의미한다.
+ * @note 0과 1은 항상 소수가 아니다.
+ */
+vector<bool> create_sieve_of_eratosthenes_v2(int max_num) {
+    vector<bool> sieve(max_num + 1, true);
+    sieve[0] = sieve[1] = false; // 0과 1은 소수가 아님을 기록한다.
+
+    int sqrt_max_num = sqrt(max_num);
+
+    for (int i = 2; i <= sqrt_max_num; i++) {
+        if (!sieve[i]) continue; // i가 소수가 아니면 skip한다.
+
+        for (int j = i * i; j <= max_num; j += i) {
+            sieve[j] = false; // 소수가 아님을 기록한다.
+        }
+    }
+
+    return sieve;
+}
+
 int main() {
     FASTIO;
-
-    cout << "수를 입력하세요 : ";
-    int n;
-    cin >> n;
-
-    if (is_prime(n)) {
-        cout << n << "은 소수이다.\n";
-    }
-    else {
-        cout << n << "은 소수가 아니다.\n";
-    }
 
     return 0;
 }
