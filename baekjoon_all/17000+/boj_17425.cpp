998d07c3c51f48bef370b5e88921567e61d0a882
홍창기 <hckhong@naver.com>
2025-01-26 07:49:41 +0900

    Update 2025-01-25

--- baekjoon_all/17000+/boj_17425.cpp
+++ baekjoon_all/17000+/boj_17425.cpp
@@ -1,35 +1,52 @@
 // Solve 2023-08-26
+// Update 2025-01-25
 
 #include <bits/stdc++.h>
+
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define size(v) (int)v.size()
+#define all(v) v.begin(),v.end()
+#define setw(n, c) cout << setw(n) << setfill(c);
+#define setp(n) cout << fixed << setprecision(n);
+#define printw(x) cout << (x) << ' ';
+#define println(x) cout << (x) << '\n';
+
+#ifdef BOJ
+#define testPrint(x) ((void)0)
+#else
+#define testPrint(x) cout << "[D] " << #x << ':' << x << '\n'
+#endif
+
 using namespace std;
-
-#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL); // boj_15552.cpp
-#define SETPRECISION(n) cout << fixed;cout.precision(n); // boj_1008.cpp
-#define SIZE(v) (int)v.size()
-#define ALL(v) v.begin(),v.end()
 using ll = long long;
+using uint = unsigned int;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
 
-ll dp[1000001];
+const double PI = M_PI;
+
+ll memo[1000001];
 
 int main() {
     FASTIO;
 
     for (int i = 1; i <= 1000000; i++) {
         for (int j = i; j <= 1000000; j += i) {
-            dp[j] += i;
+            memo[j] += i;
         }
 
-        dp[i] += dp[i - 1];
+        memo[i] += memo[i - 1];
     }
 
     int t;
     cin >> t;
 
-    for (int ti = 0; ti < t; ti++) {
+    while (t-- > 0) {
         int n;
         cin >> n;
 
-        cout << dp[n] << '\n';
+        println(memo[n]);
     }
 
     return 0;
