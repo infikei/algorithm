ab56a4ca8af60b4cf2bdb83325a5b1387b6234d9
홍창기 <hckhong@naver.com>
2025-01-29 03:54:25 +0900

    Update 2025-01-28

--- baekjoon_all/02000+/boj_2775.py
+++ baekjoon_all/02000+/boj_2775.py
@@ -1,8 +1,17 @@
-for i in range(int(input())):
+# Solve 2022-05-23
+# Update 2025-01-28
+
+import sys
+
+input = lambda : sys.stdin.readline().rstrip()
+
+building = [list(range(15)) for _ in range(15)]
+
+for i in range(1, 15):
+    for j in range(1, 15):
+        building[i][j] = building[i][j - 1] + building[i - 1][j]
+
+for _ in range(int(input())):
     k = int(input())
     n = int(input())
-    li = list(range(1, n+1))
-    for ii in range(1, k):
-        for iii in range(1, n):
-            li[iii] += li[iii-1]
-    print(sum(li))
+    print(building[k][n])
