8e6ecbfcccfebb2f24dc36d85890af10a0de6c90
홍창기 <hckhong@naver.com>
2025-02-15 03:18:01 +0900

    Update 2025-02-13

--- baekjoon_all/01000+/boj_1145.cpp
+++ baekjoon_all/01000+/boj_1145.cpp
@@ -1,33 +1,58 @@
-#include <iostream>
-using namespace std;
-#define fastio ios_base::sync_with_stdio(false);cout.tie(NULL);cin.tie(NULL); // boj_15552.cpp
+// Solve 2022-11-07
+// Update 2025-02-13
 
-int gcd(int a, int b) {
-    if (a % b == 0) {
-        return b;
-    }
-    return gcd(b, a % b);
+#include <bits/stdc++.h>
+
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define size(v) (int)v.size()
+#define all(v) v.begin(),v.end()
+#define setw(n, c) cout << setw(n) << setfill(c);
+#define setp(n) cout << fixed << setprecision(n);
+#define printw(x) cout << (x) << ' ';
+#define println(x) cout << (x) << '\n';
+
+#ifdef BOJ
+#define testPrint(x) ((void)0)
+#else
+#define testPrint(x) cout << "[D] " << #x << ':' << x << '\n'
+#endif
+
+using namespace std;
+using ll = long long;
+using uint = unsigned int;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+
+const double PI = M_PI;
+
+int get_gcd(int a, int b) {
+    if (b == 0) return a;
+
+    return get_gcd(b, a % b);
 }
 
-int lcm(int a, int b) {
-    return a / gcd(a, b) * b;
+int get_lcm(int a, int b) {
+    return a / get_gcd(a, b) * b;
 }
 
 int main() {
-    fastio;
+    FASTIO;
 
-    int numbers[5];
+    int nums[5];
+
     for (int i = 0; i < 5; i++) {
-        cin >> numbers[i];
+        cin >> nums[i];
     }
 
-    int ans = numbers[0] * numbers[1] * numbers[2];
+    int ans = nums[0] * nums[1] * nums[2];
 
     for (int i = 0; i < 5; i++) {
         for (int j = i + 1; j < 5; j++) {
-            int lcm2 = lcm(numbers[i], numbers[j]);
+            int lcm2 = get_lcm(nums[i], nums[j]);
+
             for (int k = j + 1; k < 5; k++) {
-                ans = min(ans, lcm(lcm2, numbers[k]));
+                ans = min(ans, get_lcm(lcm2, nums[k]));
             }
         }
     }
