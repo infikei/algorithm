18bcf0ae7983e140ec0444aaeb1a106d2a7bbf16
홍창기 <hckhong@naver.com>
2025-02-25 03:37:33 +0900

    Update 2025-02-24

--- baekjoon_all/04000+/boj_4108.cpp
+++ baekjoon_all/04000+/boj_4108.cpp
@@ -1,20 +1,13 @@
 // Solve 2025-02-06
+// Update 2025-02-24
 
 #include <bits/stdc++.h>
 
 #define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);
-#define size(v) (int)v.size()
-#define all(v) v.begin(),v.end()
-#define setw(n, c) cout << setw(n) << setfill(c);
-#define setp(n) cout << fixed << setprecision(n);
-#define printw(x) cout << (x) << ' ';
-#define println(x) cout << (x) << '\n';
-
-#ifdef BOJ
-#define testPrint(x) ((void)0)
-#else
-#define testPrint(x) cout << "[D] " << #x << ':' << x << '\n'
-#endif
+#define SIZE(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
+#define SETW(n, c) cout << setw(n) << setfill(c);
+#define SETP(n) cout << fixed << setprecision(n);
 
 using namespace std;
 using ll = long long;
@@ -22,8 +15,6 @@ using uint = unsigned int;
 using ull = unsigned long long;
 using ld = long double;
 using pii = pair<int, int>;
-
-const double PI = M_PI;
 
 int dx[8] = { -1, -1, -1, 0, 0, 1, 1, 1 };
 int dy[8] = { -1, 0, 1, -1, 1, -1, 0, 1 };
@@ -63,7 +54,7 @@ int main() {
         }
 
         for (string &row : board) {
-            println(row);
+            cout << row << '\n';
         }
     }
 
