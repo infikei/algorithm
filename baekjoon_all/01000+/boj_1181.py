21870622333f0e09796fc8bf74eefc3545cb0b97
홍창기 <hckhong@naver.com>
2025-04-16 05:40:53 +0900

    Update 2025-04-15

--- baekjoon_all/01000+/boj_1181.py
+++ baekjoon_all/01000+/boj_1181.py
@@ -1,12 +1,20 @@
+# Solve 2022-05-30
+# Update 2025-04-15
+
+import sys
+
+input = lambda : sys.stdin.readline().rstrip()
+
 n = int(input())
-li = [[] for _ in range(50)]
+li = [[] for _ in range(51)]
 
 for i in range(n):
-    st = input()
-    if st not in li[len(st)-1]:
-        li[len(st)-1].append(st)
+    word = input()
 
-for i in li:
-    if len(i) != 0:
-        for j in sorted(i):
-            print(j)
+    if word not in li[len(word)]:
+        li[len(word)].append(word)
+
+for words in li:
+    if len(words) != 0:
+        for word in sorted(words):
+            print(word)
--- baekjoon_all/02000+/boj_2141.cpp
+++ baekjoon_all/02000+/boj_2141.cpp
@@ -1,20 +1,13 @@
 // Solve 2025-02-01
+// Update 2025-04-15
 
 #include <bits/stdc++.h>
 
 #define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);
-#define size(v) (int)v.size()
-#define all(v) v.begin(),v.end()
-#define setw(n, c) cout << setw(n) << setfill(c);
-#define setp(n) cout << fixed << setprecision(n);
-#define printw(x) cout << (x) << ' ';
-#define println(x) cout << (x) << '\n';
-
-#ifdef BOJ
-#define testPrint(x) ((void)0)
-#else
-#define testPrint(x) cout << "[D] " << #x << ':' << x << '\n'
-#endif
+#define SIZE(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
+#define SETW(n, c) cout << setw(n) << setfill(c);
+#define SETP(n) cout << fixed << setprecision(n);
 
 using namespace std;
 using ll = long long;
@@ -23,23 +16,18 @@ using ull = unsigned long long;
 using ld = long double;
 using pii = pair<int, int>;
 
-const double PI = M_PI;
-
 struct Village{
     int pos;
     ll people;
 
-    Village() {
-    }
-
-    Village(int pos, ll people) : pos(pos), people(people) {
+    Village(int pos = 0, ll people = 0) : pos(pos), people(people) {
     }
 
     bool operator<(const Village &rhs) const {
         return pos < rhs.pos;
     }
 
-    void add(ll people_add) {
+    void add_people(ll people_add) {
         people += people_add;
     }
 };
@@ -53,10 +41,7 @@ int main() {
     cin >> n;
 
     for (int i = 0; i < n; i++) {
-        int xi, ai;
-        cin >> xi >> ai;
-
-        villages[i] = { xi, ai };
+        cin >> villages[i].pos >> villages[i].people;
     }
 
     // 마을을 위치에 따라 오름차순 정렬한다.
@@ -64,7 +49,7 @@ int main() {
 
     // 인구 수를 누적합으로 변환
     for (int i = 1; i < n; i++) {
-        villages[i].add(villages[i - 1].people);
+        villages[i].add_people(villages[i - 1].people);
     }
 
     ll people_sum = villages[n - 1].people;
@@ -85,7 +70,7 @@ int main() {
         }
     }
 
-    println(villages[high].pos);
+    cout << villages[high].pos << '\n';
 
     return 0;
 }
