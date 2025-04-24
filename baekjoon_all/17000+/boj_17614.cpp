65c93a4b40612228b27c82893c2d55c18ad063f8
홍창기 <hckhong@naver.com>
2025-04-25 01:53:12 +0900

    Update 2025-04-23

--- baekjoon_all/17000+/boj_17614.cpp
+++ baekjoon_all/17000+/boj_17614.cpp
@@ -1,25 +1,39 @@
-#include <iostream>
+// Solve 2022-11-06
+// Update 2025-04-23
+
+#include <bits/stdc++.h>
+
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define SIZE(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
+#define SETW(n, c) cout << setw(n) << setfill(c);
+#define SETP(n) cout << fixed << setprecision(n);
+
 using namespace std;
-#define fastio ios_base::sync_with_stdio(false);cout.tie(NULL);cin.tie(NULL); // boj_15552.cpp
+using ll = long long;
+using uint = unsigned int;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
 
 int main() {
-    fastio;
+    FASTIO;
 
-    int N;
-    cin >> N;
+    int n;
+    cin >> n;
 
+    int i = 1, i10 = 10, k = n;
     int cnt = 0;
 
-    int i = 1, i10 = 10, K = N;
-    while (K > 0) {
-        cnt += (N / i10) * 3 * i;
-        cnt += min(max(0, N % i10 - 3 * i + 1), i);
-        cnt += min(max(0, N % i10 - 6 * i + 1), i);
-        cnt += min(max(0, N % i10 - 9 * i + 1), i);
+    while (k > 0) {
+        cnt += (n / i10) * 3 * i;
+        cnt += min(max(0, n % i10 - 3 * i + 1), i);
+        cnt += min(max(0, n % i10 - 6 * i + 1), i);
+        cnt += min(max(0, n % i10 - 9 * i + 1), i);
 
         i *= 10;
         i10 *= 10;
-        K /= 10;
+        k /= 10;
     }
 
     cout << cnt << '\n';
