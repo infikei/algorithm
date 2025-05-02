d4298ccd952fa4a3276dc00b31f62f439d8dc184
홍창기 <hckhong@naver.com>
2025-05-03 05:43:12 +0900

    Update 2025-05-02

--- baekjoon_all/14000+/boj_14910.java
+++ baekjoon_all/14000+/boj_14910.java
@@ -1,6 +1,5 @@
 // Solve 2024-08-15
-
-// 백준에 제출할 때는 class 이름을 Main으로 설정해야 한다.
+// Update 2025-05-02
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -11,23 +10,22 @@ public class boj_14910 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        boolean ans = true;
         int x0 = Integer.parseInt(st.nextToken());
 
         while (st.hasMoreTokens()) {
             int x = Integer.parseInt(st.nextToken());
 
             if (x0 > x) {
-                ans = false;
-                break;
+                System.out.println("Bad");
+                return;
             }
 
             x0 = x;
         }
 
-        System.out.println(ans ? "Good" : "Bad");
-        br.close();
+        System.out.println("Good");
     }
 
 }
