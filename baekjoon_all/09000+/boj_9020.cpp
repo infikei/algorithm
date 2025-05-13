76cd37f672c1d9290d96da2d8b9e8868f49784ae
홍창기 <hckhong@naver.com>
2025-05-14 06:15:28 +0900

    Update 2025-05-13

--- baekjoon_all/09000+/boj_9020.cpp
+++ baekjoon_all/09000+/boj_9020.cpp
@@ -1,46 +1,47 @@
 // Solve 2023-12-11
+// Update 2025-05-13
 
 #include <bits/stdc++.h>
-using namespace std;
 
-#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL); // boj_15552.cpp
-#define SETPRECISION(n) cout << fixed;cout.precision(n); // boj_1008.cpp
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);
 #define SIZE(v) (int)v.size()
 #define ALL(v) v.begin(),v.end()
+#define SETW(n, c) cout << setw(n) << setfill(c);
+#define SETP(n) cout << fixed << setprecision(n);
+
+using namespace std;
 using ll = long long;
+using uint = unsigned int;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
 
 int main() {
     FASTIO;
 
-    vector<bool> is_prime(10001, true);
-    is_prime[1] = false;
+    vector<bool> sieve(10001, true);
+    sieve[1] = false;
 
     for (int i = 2; i < 10001; i++) {
-        if (!is_prime[i]) continue;
+        if (!sieve[i]) continue;
 
         for (int j = i + i; j < 10001; j += i) {
-            is_prime[j] = false;
+            sieve[j] = false;
         }
     }
 
     int t;
     cin >> t;
 
-    for (int ti = 0; ti < t; ti++) {
+    while (t-- > 0) {
         int n;
         cin >> n;
 
-        int k = n / 2;
-
-        while (k > 1) {
-            int kk = n - k;
-
-            if (is_prime[k] && is_prime[kk]) {
-                cout << k << ' ' << kk << '\n';
+        for (int i = n / 2; i >= 2; i--) {
+            if (sieve[i] && sieve[n - i]) {
+                cout << i << ' ' << n - i << '\n';
                 break;
             }
-
-            k--;
         }
     }
 
--- baekjoon_all/09000+/boj_9020.py
+++ baekjoon_all/09000+/boj_9020.py
@@ -1,34 +1,23 @@
 # Solve 2022-05-25
-# Update 2023-12-11
+# Update 2025-05-13
 
 import sys
 
-input = lambda : sys.stdin.readline().rstrip()
+input = lambda: sys.stdin.readline().rstrip()
 
-# 소수를 2 이상의 범위에서 찾을 것이므로, 소수 조사 시 1에 대한 코드를 생략할 수 있다.
-def isPrime(n):
-    for i in range(2, int(n ** 0.5) + 1):
-        if n % i == 0:
-            return False
-
-    return True
-
-
-# n의 최댓값이 10000이므로 2부터 10000까지의 소수를 list에 미리 저장한다.
-prime_numbers = []
+sieve = [False] + [True] * 10000
 
 for i in range(2, 10001):
-    if isPrime(i):
-        prime_numbers.append(i)
+    if not sieve[i]:
+        continue
 
-# 각각의 n 값을 입력받고, 답을 출력한다.
-# 예를 들어 n = 9인 경우, 4+5, 3+6, 2+7의 순서로 조사한다.
+    for j in range(i + i, 10001, i):
+        sieve[j] = False
+
 for _ in range(int(input())):
     n = int(input())
 
-    for k in range(n // 2, 1, -1):
-        kk = n - k
-
-        if k in prime_numbers and kk in prime_numbers:
-            print(k, kk)
+    for i in range(n // 2, 1, -1):
+        if sieve[i] and sieve[n - i]:
+            print(i, n - i)
             break
