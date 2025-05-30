81c92cea981f4f8e0e533bb084f6802cf323f5af
홍창기 <hckhong@naver.com>
2025-05-31 02:12:54 +0900

    Update 2025-05-27

--- baekjoon_all/02000+/boj_2133.cpp
+++ baekjoon_all/02000+/boj_2133.cpp
@@ -1,14 +1,20 @@
 // Solve 2023-07-09
-// Update 2023-07-26
+// Update 2025-05-27
 
 #include <bits/stdc++.h>
-using namespace std;
 
-#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL); // boj_15552.cpp
-#define SETPRECISION(n) cout << fixed;cout.precision(n); // boj_1008.cpp
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);
 #define SIZE(v) (int)v.size()
 #define ALL(v) v.begin(),v.end()
+#define SETW(n, c) cout << setw(n) << setfill(c);
+#define SETP(n) cout << fixed << setprecision(n);
+
+using namespace std;
 using ll = long long;
+using uint = unsigned int;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
 
 int main() {
     FASTIO;
@@ -18,19 +24,16 @@ int main() {
 
     if (n & 1) {
         cout << 0 << '\n';
-    }
-    else {
-        int a = 3, s = 4;
-
-        int i = 1, i_end = n >> 1;
-        while (i < i_end) {
-            a += s << 1; // a_n = a_(n-1) + 2 * S_(n-1)
-            s += a; // S_n = S_(n-1) + a_n
-            i++;
-        }
-
-        cout << a << '\n';
+        return 0;
     }
 
+    int a = 3, s = 4;
+
+    for (int i = 1; i < (n >> 1); i++) {
+        a += s << 1; // a_n = a_(n-1) + 2 * S_(n-1)
+        s += a; // S_n = S_(n-1) + a_n
+    }
+
+    cout << a << '\n';
     return 0;
 }
