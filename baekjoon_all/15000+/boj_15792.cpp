8fcd3a6e5bd84044671d332329ebfff8a4537a14
홍창기 <hckhong@naver.com>
2025-06-15 18:32:11 +0900

    Update 2025-06-14

--- baekjoon_all/15000+/boj_15792.cpp
+++ baekjoon_all/15000+/boj_15792.cpp
@@ -1,19 +1,32 @@
 // Solve 2024-06-19
+// Update 2025-06-14
 
-#include <iostream>
-#include <cmath>
-#include <algorithm>
+#include <bits/stdc++.h>
+
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define SIZE(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
+#define SETW(n, c) cout << setw(n) << setfill(c);
+#define SETP(n) cout << fixed << setprecision(n);
+
 using namespace std;
+using ll = long long;
+using uint = unsigned int;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
 
 int main() {
     int a, b;
     cin >> a >> b;
 
     cout << a / b << '.';
+    a %= b;
 
     for (int i = 0; i < 1002; i++) {
-        a = a % b * 10;
+        a *= 10;
         cout << a / b;
+        a %= b;
     }
 
     cout << '\n';
