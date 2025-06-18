3a7653001337d45dec9315abf2157050bed9e4a9
홍창기 <hckhong@naver.com>
2025-06-19 02:06:04 +0900

    Update 2025-06-16

--- baekjoon_all/16000+/boj_16463.cpp
+++ baekjoon_all/16000+/boj_16463.cpp
@@ -1,10 +1,11 @@
 // Solve 2025-06-14
+// Update 2025-06-16
 
 #include <bits/stdc++.h>
 
 #define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);
-#define SIZE(v) (int)v.size()
 #define ALL(v) v.begin(),v.end()
+#define UNIQUE(v) v.erase(unique(v.begin(),v.end()),v.end());
 #define SETW(n, c) cout << setw(n) << setfill(c);
 #define SETP(n) cout << fixed << setprecision(n);
 
@@ -14,6 +15,9 @@ using uint = unsigned int;
 using ull = unsigned long long;
 using ld = long double;
 using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1000000007;
 
 int month_days[13] = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
