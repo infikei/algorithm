41595ca04435c7a52d3f2cf99467635214f1627d
홍창기 <hckhong@naver.com>
2025-07-13 16:07:28 +0900

    Update 2025-07-08

--- baekjoon_all/01000+/boj_1915.cpp
+++ baekjoon_all/01000+/boj_1915.cpp
@@ -1,15 +1,25 @@
 // Solve 2023-07-26
+// Update 2025-07-08
 
 #include <bits/stdc++.h>
-using namespace std;
 
-#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL); // boj_15552.cpp
-#define SETPRECISION(n) cout << fixed;cout.precision(n); // boj_1008.cpp
-#define SIZE(v) (int)v.size()
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);
 #define ALL(v) v.begin(),v.end()
-using ll = long long;
+#define UNIQUE(v) v.erase(unique(v.begin(),v.end()),v.end());
+#define SETW(n, c) cout << setw(n) << setfill(c);
+#define SETP(n) cout << fixed << setprecision(n);
 
-int dp[1001][1001];
+using namespace std;
+using ll = long long;
+using uint = unsigned int;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1000000007;
+
+int memo[1001][1001];
 
 int main() {
     FASTIO;
@@ -19,19 +29,18 @@ int main() {
 
     int ans = 0;
 
-    for (int i = 1; i <= n; i++) {
+    for (int x = 1; x <= n; x++) {
         string s;
         cin >> s;
 
-        for (int j = 1; j <= m; j++) {
-            if (s[j - 1] == '1') {
-                dp[i][j] = min(min(dp[i - 1][j - 1], dp[i - 1][j]), dp[i][j - 1]) + 1;
-                ans = max(ans, dp[i][j]);
+        for (int y = 1; y <= m; y++) {
+            if (s[y - 1] == '1') {
+                memo[x][y] = min(memo[x][y - 1], min(memo[x - 1][y - 1], memo[x - 1][y])) + 1;
+                ans = max(ans, memo[x][y]);
             }
         }
     }
 
     cout << ans * ans << '\n';
-
     return 0;
 }
