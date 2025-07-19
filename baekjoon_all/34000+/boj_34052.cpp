1f4b6cae0b2b85f0a3531694055d2c57154cec89
홍창기 <hckhong@naver.com>
2025-07-20 03:15:07 +0900

    Solve 2025-07-19

---
+++ baekjoon_all/34000+/boj_34052.cpp
@@ -0,0 +1,35 @@
+// Solve 2025-07-19
+
+#include <bits/stdc++.h>
+
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define ALL(v) v.begin(),v.end()
+#define UNIQUE(v) v.erase(unique(v.begin(),v.end()),v.end());
+#define SETW(n, c) cout << setw(n) << setfill(c);
+#define SETP(n) cout << fixed << setprecision(n);
+
+using namespace std;
+using ll = long long;
+using uint = unsigned int;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1000000007;
+
+int main() {
+    FASTIO;
+
+    int time_sum = 300;
+
+    for (int i = 0; i < 4; i++) {
+        int x;
+        cin >> x;
+        time_sum += x;
+    }
+
+    cout << (time_sum <= 1800 ? "Yes" : "No") << '\n';
+
+    return 0;
+}
