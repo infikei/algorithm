805a9b0d0437fd57abff47d744996b52b70bd91c
홍창기 <hckhong@naver.com>
2025-08-03 23:06:24 +0900

    Update 2025-07-31

--- baekjoon_all/01000+/boj_1422.cpp
+++ baekjoon_all/01000+/boj_1422.cpp
@@ -1,25 +1,35 @@
 // Solve 2024-01-14
+// Update 2025-07-31
 
 #include <bits/stdc++.h>
-using namespace std;
 
-#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL); // boj_15552.cpp
-#define SETPRECISION(n) cout << fixed;cout.precision(n); // boj_1008.cpp
-#define SIZE(v) (int)v.size()
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);
 #define ALL(v) v.begin(),v.end()
-using ll = long long;
+#define UNIQUE(v) v.erase(unique(v.begin(),v.end()),v.end());
+#define SETW(n, c) cout << setw(n) << setfill(c);
+#define SETP(n) cout << fixed << setprecision(n);
 
-struct cmp_len{
+using namespace std;
+using ll = long long;
+using uint = unsigned int;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1000000007;
+
+struct CmpLen{
     bool operator()(string a, string b) {
-        if (SIZE(a) != SIZE(b)) {
-            return SIZE(a) > SIZE(b);
+        if (size(a) != size(b)) {
+            return size(a) > size(b);
         }
 
         return a > b;
     }
 };
 
-struct cmp_plus{
+struct CmpPlus{
     bool operator()(string a, string b) {
         return a + b > b + a;
     }
@@ -30,28 +40,25 @@ int main() {
 
     int k, n;
     cin >> k >> n;
-
     vector<string> numbers(k);
 
-    for (string &num : numbers) {
+    for (string& num : numbers) {
         cin >> num;
     }
 
-    sort(numbers.begin(), numbers.end(), cmp_len());
-
+    sort(numbers.begin(), numbers.end(), CmpLen());
     int iter = n - k;
 
-    while (--iter >= 0) {
+    while (iter-- > 0) {
         numbers.push_back(numbers[0]);
     }
 
-    sort(numbers.begin(), numbers.end(), cmp_plus());
+    sort(numbers.begin(), numbers.end(), CmpPlus());
 
-    for (string &num : numbers) {
+    for (string& num : numbers) {
         cout << num;
     }
 
     cout << '\n';
-
     return 0;
 }
