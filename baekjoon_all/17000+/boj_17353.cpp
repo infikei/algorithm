1f147100babd20ae5dba4b140993a4c3ff5045a2
홍창기 <hckhong@naver.com>
2025-08-12 23:50:44 +0900

    Update 2025-08-10

--- baekjoon_all/17000+/boj_17353.cpp
+++ baekjoon_all/17000+/boj_17353.cpp
@@ -1,58 +1,76 @@
 // Solve 2023-07-15
+// Update 2025-08-10
 
 #include <bits/stdc++.h>
-using namespace std;
 
-#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL); // boj_15552.cpp
-#define SETPRECISION(n) cout << fixed;cout.precision(n); // boj_1008.cpp
-#define SIZE(v) (int)v.size()
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);
 #define ALL(v) v.begin(),v.end()
+#define UNIQUE(v) v.erase(unique(v.begin(),v.end()),v.end());
+#define SETW(n, c) cout << setw(n) << setfill(c);
+#define SETP(n) cout << fixed << setprecision(n);
+
+using namespace std;
 using ll = long long;
+using uint = unsigned int;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1000000007;
 
-ll arr[100001];
-ll seg[400001], lazy[400001];
+int arr[100001];
+int len = 1 << 17;
+ll seg[1 << 18];
+ll lazy[1 << 18];
 
-void seg_lazy_update(int node, int s, int e) {
-    if (lazy[node] != 0) {
-        seg[node] += lazy[node] * (e - s + 1);
+void apply_lazy(int idx, int l, int r) {
+    seg[idx] += (r - l + 1) * lazy[idx];
 
-        if (s != e) {
-            lazy[node << 1] += lazy[node];
-            lazy[(node << 1) + 1] += lazy[node];
-        }
-
-        lazy[node] = 0;
+    if (l < r) {
+        lazy[idx * 2] += lazy[idx];
+        lazy[idx * 2 + 1] += lazy[idx];
     }
+
+    lazy[idx] = 0;
 }
 
-void seg_update(int node, int s, int e, int qs, int qe, int qval) {
-    if (qe < s || e < qs) {
-        seg_lazy_update(node, s, e);
+void update_seg(int idx, int l, int r, int tl, int tr, ll val) {
+    if (tr < l || r < tl) {
+        apply_lazy(idx, l, r);
         return;
     }
-    if (qs <= s && e <= qe) {
-        lazy[node] += qval;
-        seg_lazy_update(node, s, e);
+    if (tl <= l && r <= tr) {
+        lazy[idx] += val;
+        apply_lazy(idx, l, r);
         return;
     }
 
-    int mid = (s + e) >> 1;
-    seg_lazy_update(node, s, e);
-    seg_update(node << 1, s, mid, qs, qe, qval);
-    seg_update((node << 1) + 1, mid + 1, e, qs, qe, qval);
-    seg[node] = seg[node << 1] + seg[(node << 1) + 1];
+    apply_lazy(idx, l, r);
+    int mid = (l + r) / 2;
+    update_seg(idx * 2, l, mid, tl, tr, val);
+    update_seg(idx * 2 + 1, mid + 1, r, tl, tr, val);
+    seg[idx] = seg[idx * 2] + seg[idx * 2 + 1];
 }
 
-ll seg_query(int node, int s, int e, int qs, int qe) {
-    seg_lazy_update(node, s, e);
+void update_seg(int tl, int tr, ll val) {
+    update_seg(1, 0, len - 1, tl, tr, val);
+}
 
-    if (qe < s || e < qs) return 0;
-    if (qs <= s && e <= qe) return seg[node];
+ll get_from_seg(int idx, int l, int r, int tl, int tr) {
+    apply_lazy(idx, l, r);
 
-    int mid = (s + e) >> 1;
-    ll left_val = seg_query(node << 1, s, mid, qs, qe);
-    ll right_val = seg_query((node << 1) + 1, mid + 1, e, qs, qe);
-    return left_val + right_val;
+    if (tr < l || r < tl) return 0;
+    if (tl <= l && r <= tr) return seg[idx];
+
+    int mid = (l + r) / 2;
+    ll l_ret = get_from_seg(idx * 2, l, mid, tl, tr);
+    ll r_ret = get_from_seg(idx * 2 + 1, mid + 1, r, tl, tr);
+    return l_ret + r_ret;
+}
+
+ll get_from_seg(int tl, int tr) {
+    return get_from_seg(1, 0, len - 1, tl, tr);
 }
 
 int main() {
@@ -65,25 +83,23 @@ int main() {
         cin >> arr[i];
     }
 
-    int m;
-    cin >> m;
+    int q;
+    cin >> q;
 
-    for (int i = 0; i < m; i++) {
+    while (q-- > 0) {
         int cmd;
         cin >> cmd;
 
         if (cmd == 1) {
-            int qs, qe;
-            cin >> qs >> qe;
-
-            seg_update(1, 1, n, qs, qe, 1);
-            if (qe < n) seg_update(1, 1, n, qe + 1, qe + 1, -(qe - qs + 1));
+            int l, r;
+            cin >> l >> r;
+            update_seg(l, r, 1);
+            update_seg(r + 1, r + 1, -(r - l + 1));
         }
         else {
-            int qidx;
-            cin >> qidx;
-
-            cout << arr[qidx] + seg_query(1, 1, n, 1, qidx) << '\n';
+            int x;
+            cin >> x;
+            cout << get_from_seg(1, x) + arr[x] << '\n';
         }
     }
 
