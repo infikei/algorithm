6a860f3e63fc1cec561def4778ea943e5a7941a4
홍창기 <hckhong@naver.com>
2025-08-20 22:51:24 +0900

    Update 2025-08-17

--- baekjoon_all/10000+/boj_10886.cpp
+++ baekjoon_all/10000+/boj_10886.cpp
@@ -1,27 +1,34 @@
 // Solve 2022-10-23
-// Update 2023-12-20
+// Update 2025-08-17
 
 #include <bits/stdc++.h>
-using namespace std;
 
-#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL); // boj_15552.cpp
-#define SETPRECISION(n) cout << fixed;cout.precision(n); // boj_1008.cpp
-#define SIZE(v) (int)v.size()
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);
 #define ALL(v) v.begin(),v.end()
+#define UNIQUE(v) v.erase(unique(v.begin(),v.end()),v.end());
+#define SETW(n, c) cout << setw(n) << setfill(c);
+#define SETP(n) cout << fixed << setprecision(n);
+
+using namespace std;
 using ll = long long;
+using uint = unsigned int;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1000000007;
 
 int main() {
     FASTIO;
 
     int n;
     cin >> n;
-
-    int cnt[2] = { 0 };
+    int cnt[2] = {0};
 
     for (int i = 0; i < n; i++) {
         int x;
         cin >> x;
-
         cnt[x]++;
     }
 
