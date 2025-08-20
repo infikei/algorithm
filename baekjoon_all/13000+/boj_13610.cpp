db8220e8fa035b90217abdbae9772d1d22a60f66
홍창기 <hckhong@naver.com>
2025-08-20 22:53:13 +0900

    Solve 2025-08-19

---
+++ baekjoon_all/13000+/boj_13610.cpp
@@ -0,0 +1,30 @@
+// Solve 2025-08-19
+
+#include <bits/stdc++.h>
+
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define ALL(v) v.begin(),v.end()
+#define UNIQUE(v) v.erase(unique(v.begin(),v.end()),v.end());
+#define SETW(n, c) cout << setw(n) << setfill(c);
+#define SETP(n) cout << fixed << setprecision(n);
+
+using namespace std;
+using ll = long long;
+using uint = unsigned int;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1000000007;
+
+int main() {
+    FASTIO;
+
+    int x, y;
+    cin >> x >> y;
+
+    int ans = (y - 1) / (y - x) + 1; // ans = ceil(y / (y - x))
+    cout << ans << '\n';
+    return 0;
+}
