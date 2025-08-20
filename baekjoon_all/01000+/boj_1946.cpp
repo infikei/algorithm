f74cf134b056ca01f26c9892473eba32c68b2dae
홍창기 <hckhong@naver.com>
2025-08-20 22:52:58 +0900

    Update 2025-08-19

--- baekjoon_all/01000+/boj_1946.cpp
+++ baekjoon_all/01000+/boj_1946.cpp
@@ -1,30 +1,39 @@
 // Solve 2024-03-24
+// Update 2025-08-19
 
 #include <bits/stdc++.h>
-using namespace std;
 
-#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL); // boj_15552.cpp
-#define SETPRECISION(n) cout << fixed;cout.precision(n); // boj_1008.cpp
-#define SIZE(v) (int)v.size()
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);
 #define ALL(v) v.begin(),v.end()
+#define UNIQUE(v) v.erase(unique(v.begin(),v.end()),v.end());
+#define SETW(n, c) cout << setw(n) << setfill(c);
+#define SETP(n) cout << fixed << setprecision(n);
+
+using namespace std;
 using ll = long long;
+using uint = unsigned int;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1000000007;
+
+int arr[100001];
 
 int main() {
     FASTIO;
 
-    int arr[100001];
-
     int t;
     cin >> t;
 
-    for (int ti = 0; ti < t; ti++) {
+    while (t-- > 0) {
         int n;
         cin >> n;
 
         for (int i = 0; i < n; i++) {
             int a, b;
             cin >> a >> b;
-
             arr[a] = b;
         }
 
