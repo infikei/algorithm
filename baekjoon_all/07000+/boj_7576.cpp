8c51c5e4e2823aff87e99f46b2a2f74077842175
홍창기 <hckhong@naver.com>
2025-09-10 03:41:13 +0900

    Update 2025-09-09

--- baekjoon_all/07000+/boj_7576.cpp
+++ baekjoon_all/07000+/boj_7576.cpp
@@ -1,14 +1,23 @@
 // Solve 2022-12-07
-// Update 2024-02-16
+// Update 2025-09-09
 
 #include <bits/stdc++.h>
-using namespace std;
 
-#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL); // boj_15552.cpp
-#define SETPRECISION(n) cout << fixed;cout.precision(n); // boj_1008.cpp
-#define SIZE(v) (int)v.size()
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);
 #define ALL(v) v.begin(),v.end()
+#define UNIQUE(v) v.erase(unique(v.begin(),v.end()),v.end());
+#define SETW(n, c) cout << setw(n) << setfill(c);
+#define SETP(n) cout << fixed << setprecision(n);
+
+using namespace std;
 using ll = long long;
+using uint = unsigned int;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1000000007;
 
 struct Point{
     int x, y;
@@ -16,25 +25,24 @@ struct Point{
     Point(int x, int y) : x(x), y(y) {}
 };
 
+int board[1000][1000];
+int dx[4] = {0, 0, -1, 1};
+int dy[4] = {-1, 1, 0, 0};
+
 int main() {
     FASTIO;
 
-    int dx[4] = { 0, 0, -1, 1 };
-    int dy[4] = { -1, 1, 0, 0 };
-
     int n, m;
     cin >> m >> n;
-
-    int board[1000][1000];
     queue<Point> bfs_que;
-    int not_ripe = 0;
+    int not_ripe_cnt = 0;
 
     for (int x = 0; x < n; x++) {
         for (int y = 0; y < m; y++) {
             cin >> board[x][y];
 
             if (board[x][y] == 0) {
-                not_ripe++;
+                not_ripe_cnt++;
             }
             else if (board[x][y] == 1) {
                 bfs_que.emplace(x, y);
@@ -44,28 +52,29 @@ int main() {
 
     int cur_day = 0;
 
-    while (not_ripe > 0 && !bfs_que.empty()) {
+    while (not_ripe_cnt > 0 && !bfs_que.empty()) {
         cur_day++;
-        int cur_iter = SIZE(bfs_que);
+        int iter = size(bfs_que);
 
-        while (cur_iter-- > 0) {
-            Point cur_pos = bfs_que.front();
+        while (iter-- > 0) {
+            Point cur = bfs_que.front();
             bfs_que.pop();
 
             for (int d = 0; d < 4; d++) {
-                int nx = cur_pos.x + dx[d];
-                int ny = cur_pos.y + dy[d];
+                int nx = cur.x + dx[d];
+                int ny = cur.y + dy[d];
 
-                if (nx >= 0 && nx < n && ny >= 0 && ny < m && board[nx][ny] == 0) {
+                if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+
+                if (board[nx][ny] == 0) {
                     board[nx][ny] = 1;
                     bfs_que.emplace(nx, ny);
-                    not_ripe--;
+                    not_ripe_cnt--;
                 }
             }
         }
     }
 
-    cout << (not_ripe > 0 ? -1 : cur_day) << '\n';
-
+    cout << (not_ripe_cnt > 0 ? -1 : cur_day) << '\n';
     return 0;
 }
