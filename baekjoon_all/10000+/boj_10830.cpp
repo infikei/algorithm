8ceca026a6d08eb33b59dfff586e3ebbea24c950
홍창기 <hckhong@naver.com>
2025-09-18 04:55:24 +0900

    Update 2025-09-16

--- baekjoon_all/10000+/boj_10830.cpp
+++ baekjoon_all/10000+/boj_10830.cpp
@@ -1,53 +1,62 @@
 // Solve 2022-12-06
-// Update 2023-12-20
+// Update 2025-09-16
 
 #include <bits/stdc++.h>
-using namespace std;
 
-#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL); // boj_15552.cpp
-#define SETPRECISION(n) cout << fixed;cout.precision(n); // boj_1008.cpp
-#define SIZE(v) (int)v.size()
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);
 #define ALL(v) v.begin(),v.end()
-using ll = long long;
-using matrix = vector<vector<int>>;
+#define UNIQUE(v) v.erase(unique(v.begin(),v.end()),v.end());
+#define SETW(n, c) cout << setw(n) << setfill(c);
+#define SETP(n) cout << fixed << setprecision(n);
 
+using namespace std;
+using ll = long long;
+using uint = unsigned int;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using matrix = vector<vector<int>>;
+const int INF = 0x3f3f3f3f;
 const int MOD = 1000;
 
-matrix calc_mat_mul(const matrix &a, const matrix &b) {
-    int n = SIZE(a);
-    matrix res(n, vector<int>(n, 0));
+matrix mat_mul(const matrix& a, const matrix& b) {
+    int n1 = size(a);
+    int n2 = size(b);
+    int n3 = size(b[0]);
+    matrix ret(n3, vector<int>(n1, 0));
 
-    for (int row = 0; row < n; row++) {
-        for (int col = 0; col < n; col++) {
-            for (int idx = 0; idx < n; idx++) {
-                res[row][col] += a[row][idx] * b[idx][col] % MOD;
+    for (int i = 0; i < n1; i++) {
+        for (int j = 0; j < n3; j++) {
+            for (int k = 0; k < n2; k++) {
+                ret[i][j] += a[i][k] * b[k][j];
             }
 
-            res[row][col] %= MOD;
+            ret[i][j] %= MOD;
         }
     }
 
-    return res;
+    return ret;
 }
 
-matrix calc_mat_power(matrix a, ll b) {
-    int n = SIZE(a);
-    matrix res(n, vector<int>(n, 0));
+matrix mat_pow(matrix a, ll b) {
+    int n = size(a);
+    matrix ret(n, vector<int>(n, 0));
 
     for (int i = 0; i < n; i++) {
-        res[i][i] = 1;
+        ret[i][i] = 1;
     }
 
     while (b > 0) {
         if (b & 1) {
-            res = calc_mat_mul(res, a);
+            ret = mat_mul(ret, a);
         }
 
-        a = calc_mat_mul(a, a);
+        a = mat_mul(a, a);
         b >>= 1;
     }
 
-    return res;
+    return ret;
 }
 
 int main() {
@@ -56,19 +65,18 @@ int main() {
     int n;
     ll b;
     cin >> n >> b;
-
     matrix a(n, vector<int>(n, 0));
 
-    for (vector<int> &row : a) {
-        for (int &x : row) {
+    for (vector<int>& row : a) {
+        for (int& x : row) {
             cin >> x;
         }
     }
 
-    matrix ans = calc_mat_power(a, b);
+    matrix ans = mat_pow(a, b);
 
-    for (vector<int> &row : ans) {
-        for (int &x : row) {
+    for (vector<int>& row : ans) {
+        for (int& x : row) {
             cout << x << ' ';
         }
 
