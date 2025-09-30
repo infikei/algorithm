4376f1d01f01c0f7c7bfaa5f2e32cf1831d19458
홍창기 <hckhong@naver.com>
2025-09-30 20:15:22 +0900

    Update 2025-09-29

--- baekjoon_all/10000+/boj_10871.py
+++ baekjoon_all/10000+/boj_10871.py
@@ -1,12 +1,9 @@
 # Solve 2022-05-16
-# Update 2023-10-10
+# Update 2025-09-29
 
 import sys
 
-input = lambda : sys.stdin.readline().rstrip()
+input = lambda: sys.stdin.readline().rstrip()
 
 n, x = map(int, input().split())
-nums = list(map(int, input().split()))
-ans = filter(lambda num: num < x, nums)
-
-print(*ans)
+print(*filter(lambda v: v < x, map(int, input().split())))
