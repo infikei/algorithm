50c68b1202cf007bccdd51f38aae28b96d84050b
홍창기 <hckhong@naver.com>
2025-10-03 00:14:41 +0900

    Update 2025-10-02

--- baekjoon_all/11000+/boj_11660.py
+++ baekjoon_all/11000+/boj_11660.py
@@ -1,15 +1,19 @@
 # Solve 2023-03-04
+# Update 2025-10-02
 
 import sys
 
-input = lambda : sys.stdin.readline().rstrip()
+input = lambda: sys.stdin.readline().rstrip()
 
 n, m = map(int, input().split())
-li = [[0] * (n + 1)] + [[0] + list(map(int, input().split())) for _ in range(n)]
-for row in range(1, n + 1):
-    for col in range(1, n + 1):
-        li[row][col] += li[row][col - 1] + li[row - 1][col] - li[row - 1][col - 1]
+board = [[0] * (n + 1)] + [list(map(int, ("0 " + input()).split())) for _ in range(n)]
+
+for x in range(1, n + 1):
+    for y in range(1, n + 1):
+        board[x][y] += board[x][y - 1] + board[x - 1][y] - board[x - 1][y - 1]
 
 for _ in range(m):
     x1, y1, x2, y2 = map(int, input().split())
-    print(li[x2][y2] - li[x2][y1 - 1] - li[x1 - 1][y2] + li[x1 - 1][y1 - 1])
+    x1 -= 1
+    y1 -= 1
+    print(board[x2][y2] - board[x2][y1] - board[x1][y2] + board[x1][y1])
