3f339c75c14715b671c38347d68e27228ba34cec
홍창기 <hckhong@naver.com>
2025-10-17 02:01:26 +0900

    Update 2025-10-14

--- baekjoon_all/15000+/boj_15683.java
+++ baekjoon_all/15000+/boj_15683.java
@@ -1,7 +1,5 @@
 // Solve 2024-02-19
-// Update 2024-02-20
-
-// 백준에 제출할 때는 class 이름을 Main으로 설정해야 한다.
+// Update 2025-10-14
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -20,59 +18,59 @@ public class boj_15683 {
         }
     }
 
-    static int[] dx = { -1, 0, 1, 0 };
-    static int[] dy = { 0, 1, 0, -1 };
     static int n, m;
     static int[][] board;
+    static int cctvCount = 0;
+    static CCTV[] cctvs = new CCTV[8];
+    static int[] dx = { -1, 0, 1, 0 };
+    static int[] dy = { 0, 1, 0, -1 };
     static int[][][] combs = {
         {},
-        { { 0 }, { 1 }, { 2 }, { 3 } },
-        { { 0, 2 }, { 1, 3 } },
-        { { 0, 1 }, { 1, 2 }, { 2, 3 }, { 3, 0 } },
-        { { 0, 1, 2 }, { 1, 2, 3 }, { 2, 3, 0 }, { 3, 0, 1 } },
-        { { 0, 1, 2, 3 } }
+        {{0}, {1}, {2}, {3}},
+        {{0, 2}, {1, 3}},
+        {{0, 1}, {1, 2}, {2, 3}, {3, 0}},
+        {{0, 1, 2}, {1, 2, 3}, {2, 3, 0}, {3, 0, 1}},
+        {{0, 1, 2, 3}}
     };
-    static CCTV[] cctvs = new CCTV[8];
-    static int cctvCount = 0;
     static int minHiddenArea;
 
-    static void dfs(int depth, int hiddenArea) {
-        if (depth == cctvCount) {
-            minHiddenArea = Math.min(minHiddenArea, hiddenArea);
-            return;
-        }
-
-        for (int[] comb : combs[cctvs[depth].type]) {
-            for (int d : comb) {
-                hiddenArea -= change(cctvs[depth].x, cctvs[depth].y, d, 0, -(depth + 1));
-            }
-
-            dfs(depth + 1, hiddenArea);
-
-            for (int d : comb) {
-                hiddenArea += change(cctvs[depth].x, cctvs[depth].y, d, -(depth + 1), 0);
-            }
-        }
-    }
-
-    static int change(int x, int y, int d, int value, int newValue) {
-        int areaChanged = 0;
+    static int change(int x, int y, int d, int val, int newVal) {
+        int ret = 0;
         x += dx[d];
         y += dy[d];
 
-        while (x >= 0 && x < n && y >= 0 && y < m) {
-            if (board[x][y] == 6) break;
-
-            if (board[x][y] == value) {
-                board[x][y] = newValue;
-                areaChanged++;
+        while (x >= 0 && x < n && y >= 0 && y < m && board[x][y] != 6) {
+            if (board[x][y] == val) {
+                board[x][y] = newVal;
+                ret++;
             }
 
             x += dx[d];
             y += dy[d];
         }
 
-        return areaChanged;
+        return ret;
+    }
+
+    static void recur(int depth, int hiddenArea) {
+        if (depth == cctvCount) {
+            minHiddenArea = Math.min(minHiddenArea, hiddenArea);
+            return;
+        }
+
+        CCTV curCctv = cctvs[depth];
+
+        for (int[] dir : combs[curCctv.type]) {
+            for (int d : dir) {
+                hiddenArea -= change(curCctv.x, curCctv.y, d, 0, -(depth + 1));
+            }
+
+            recur(depth + 1, hiddenArea);
+
+            for (int d : dir) {
+                hiddenArea += change(curCctv.x, curCctv.y, d, -(depth + 1), 0);
+            }
+        }
     }
 
     public static void main(String[] args) throws IOException {
@@ -98,9 +96,8 @@ public class boj_15683 {
         }
 
         minHiddenArea = hiddenArea;
-        dfs(0, hiddenArea);
+        recur(0, hiddenArea);
         System.out.println(minHiddenArea);
-        br.close();
     }
 
 }
