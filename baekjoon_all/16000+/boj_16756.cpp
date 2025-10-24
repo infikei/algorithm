5c37da56e0a5d689364bda00bd73af7fe9170c92
홍창기 <hckhong@naver.com>
2025-10-25 02:37:10 +0900

    Update 2025-10-21

--- baekjoon_all/16000+/boj_16756.cpp
+++ baekjoon_all/16000+/boj_16756.cpp
@@ -1,20 +1,13 @@
 // Solve 2025-01-13
+// Update 2025-10-21
 
 #include <bits/stdc++.h>
 
 #define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);
-#define size(v) (int)v.size()
-#define all(v) v.begin(),v.end()
-#define setw(n, c) cout << setw(n) << setfill(c);
-#define setp(n) cout << fixed << setprecision(n);
-#define printw(x) cout << (x) << ' ';
-#define println(x) cout << (x) << '\n';
-
-#ifdef BOJ
-#define testPrint(x) ((void)0)
-#else
-#define testPrint(x) cout << "[D] " << #x << ':' << x << '\n'
-#endif
+#define ALL(v) v.begin(),v.end()
+#define UNIQUE(v) v.erase(unique(v.begin(),v.end()),v.end());
+#define SETW(n, c) cout << setw(n) << setfill(c);
+#define SETP(n) cout << fixed << setprecision(n);
 
 using namespace std;
 using ll = long long;
@@ -22,28 +15,26 @@ using uint = unsigned int;
 using ull = unsigned long long;
 using ld = long double;
 using pii = pair<int, int>;
-
-const double PI = M_PI;
-
-int arr[100000];
+using pll = pair<ll, ll>;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1000000007;
 
 int main() {
     FASTIO;
 
     int n;
     cin >> n;
-
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
-    }
-
-    int ans = abs(arr[0] - arr[1]);
+    int mn = 2000000000;
+    int prv;
+    cin >> prv;
 
     for (int i = 1; i < n; i++) {
-        ans = min(ans, abs(arr[i - 1] - arr[i]));
+        int x;
+        cin >> x;
+        mn = min(mn, abs(prv - x));
+        prv = x;
     }
 
-    println(ans);
-
+    cout << mn << '\n';
     return 0;
 }
