a135588dd00ec53bea55d9171f82bae0e4d4c4fa
홍창기 <hckhong@naver.com>
2025-11-04 00:24:53 +0900

    Update 2025-10-30

--- baekjoon_all/07000+/boj_7662.cpp
+++ baekjoon_all/07000+/boj_7662.cpp
@@ -1,14 +1,23 @@
 // Solve 2022-08-30
-// Update 2023-10-25
+// Update 2025-10-30
 
 #include <bits/stdc++.h>
-using namespace std;
 
-#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL); // boj_15552.cpp
-#define SETPRECISION(n) cout << fixed;cout.precision(n); // boj_1008.cpp
-#define SIZE(v) (int)v.size()
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);
 #define ALL(v) v.begin(),v.end()
+#define UNIQUE(v) v.erase(unique(v.begin(),v.end()),v.end());
+#define SETW(n, c) cout << setw(n) << setfill(c);
+#define SETP(n) cout << fixed << setprecision(n);
+
+using namespace std;
 using ll = long long;
+using uint = unsigned int;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1000000007;
 
 int main() {
     FASTIO;
@@ -16,64 +25,56 @@ int main() {
     int t;
     cin >> t;
 
-    for (int ti = 0; ti < t; ti++) {
+    while (t-- > 0) {
+        int n;
+        cin >> n;
+
         priority_queue<int> pq_maxtop;
-        priority_queue<int, vector<int>, greater<int> > pq_mintop;
-        map<int, int> mp; // 저장한 수와 그 중복된 개수를 저장하는 map
+        priority_queue<int, vector<int>, greater<int>> pq_mintop;
+        map<int, int> cnt; // 저장한 수와 그 중복된 개수를 저장하는 map
 
-        int k;
-        cin >> k;
+        while (n-- > 0) {
+            char cmd;
+            int x;
+            cin >> cmd >> x;
 
-        for (int ki = 0; ki < k; ki++) {
-            int n;
-            char ch;
-            cin >> ch >> n;
+            if (cmd == 'I') {
+                pq_maxtop.push(x);
+                pq_mintop.push(x);
+                cnt[x]++;
+            }
+            else if (x == 1) {
+                while (!pq_maxtop.empty() && cnt[pq_maxtop.top()] == 0) {
+                    pq_maxtop.pop();
+                }
 
-            if (ch == 'I') {
-                pq_maxtop.push(n);
-                pq_mintop.push(n);
-                mp[n]++;
+                if (!pq_maxtop.empty()) {
+                    cnt[pq_maxtop.top()]--;
+                    pq_maxtop.pop();
+                }
             }
             else {
-                if (!mp.empty()) {
-                    if (n == 1) {
-                        while (!pq_maxtop.empty() && mp[pq_maxtop.top()] == 0) {
-                            pq_maxtop.pop();
-                        }
+                while (!pq_mintop.empty() && cnt[pq_mintop.top()] == 0) {
+                    pq_mintop.pop();
+                }
 
-                        if (!pq_maxtop.empty()) {
-                            mp[pq_maxtop.top()]--;
-                            pq_maxtop.pop();
-                        }
-                    }
-                    else {
-                        while (!pq_mintop.empty() && mp[pq_mintop.top()] == 0) {
-                            pq_mintop.pop();
-                        }
-
-                        if (!pq_mintop.empty()) {
-                            mp[pq_mintop.top()]--;
-                            pq_mintop.pop();
-                        }
-                    }
+                if (!pq_mintop.empty()) {
+                    cnt[pq_mintop.top()]--;
+                    pq_mintop.pop();
                 }
             }
         }
 
-        // 현재 저장되어있는 최댓값과 최솟값이 유효한지 검사한다.
-
-        while (!pq_maxtop.empty() && mp[pq_maxtop.top()] == 0) {
+        while (!pq_maxtop.empty() && cnt[pq_maxtop.top()] == 0) {
             pq_maxtop.pop();
         }
 
-        while (!pq_mintop.empty() && mp[pq_mintop.top()] == 0) {
+        while (!pq_mintop.empty() && cnt[pq_mintop.top()] == 0) {
             pq_mintop.pop();
         }
 
-        // 결과 출력
-
-        if (pq_mintop.empty() || pq_maxtop.empty()) {
-            cout << "EMPTY\n";
+        if (pq_maxtop.empty()) {
+            cout << "EMPTY" << '\n';
         }
         else {
             cout << pq_maxtop.top() << ' ' << pq_mintop.top() << '\n';
