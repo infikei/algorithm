e28456d483009392a2cc81c86a3416b1801d8be9
홍창기 <hckhong@naver.com>
2025-11-08 02:34:15 +0900

    Update 2025-11-06

--- baekjoon_all/01000+/boj_1912.cpp
+++ baekjoon_all/01000+/boj_1912.cpp
@@ -1,14 +1,23 @@
 // Solve 2022-06-03
-// Update 2023-07-22
+// Update 2025-11-06
 
 #include <bits/stdc++.h>
-using namespace std;
 
-#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL); // boj_15552.cpp
-#define SETPRECISION(n) cout << fixed;cout.precision(n); // boj_1008.cpp
-#define SIZE(v) (int)v.size()
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);
 #define ALL(v) v.begin(),v.end()
+#define UNIQUE(v) v.erase(unique(v.begin(),v.end()),v.end());
+#define SETW(n, c) cout << setw(n) << setfill(c);
+#define SETP(n) cout << fixed << setprecision(n);
+
+using namespace std;
 using ll = long long;
+using uint = unsigned int;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1000000007;
 
 int main() {
     FASTIO;
@@ -16,22 +25,17 @@ int main() {
     int n;
     cin >> n;
 
-    int val = 0, max_val = -1000;
+    int cur = 0;
+    int mx = -INF;
+
     for (int i = 0; i < n; i++) {
-        int x;
-        cin >> x;
-
-        val += x;
-        if (val > max_val) {
-            max_val = val;
-        }
-
-        if (val < 0) {
-            val = 0;
-        }
+        int v;
+        cin >> v;
+        cur += v;
+        if (cur > mx) mx = cur;
+        if (cur < 0) cur = 0;
     }
 
-    cout << max_val << '\n';
-
+    cout << mx << '\n';
     return 0;
 }
