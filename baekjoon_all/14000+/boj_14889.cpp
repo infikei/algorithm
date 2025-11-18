3a52cc5761e0cc75f42f31f13d94b105c768c04f
홍창기 <hckhong@naver.com>
2025-11-18 16:41:02 +0900

    Update 2025-11-17

--- baekjoon_all/14000+/boj_14889.cpp
+++ baekjoon_all/14000+/boj_14889.cpp
@@ -1,82 +1,71 @@
-#include <iostream>
+// Solve 2022-07-23
+// Update 2025-11-17
+
+#include <bits/stdc++.h>
+
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define ALL(v) v.begin(),v.end()
+#define UNIQUE(v) v.erase(unique(v.begin(),v.end()),v.end());
+#define SETW(n, c) cout << setw(n) << setfill(c);
+#define SETP(n) cout << fixed << setprecision(n);
+
 using namespace std;
+using ll = long long;
+using uint = unsigned int;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1000000007;
 
-int values[21][21];
-int visited[21] = {0};
-int N, result = 10000;
+int n;
+int board[20][20];
+bool selected[20];
+int mn = INF;
 
-int calculate_diff() {
+int get_score(bool team) {
+    int ret = 0;
 
-    // 첫번째 팀의 능력치 계산
-    int team1_value = 0;
-    for (int i = 1; i <= N; i++) {
-        if (visited[i] == 0) {
-            continue;
-        }
-        for (int j = i + 1; j <= N; j++) {
-            if (visited[j] == 0) {
-                continue;
-            }
-            team1_value += values[i][j];
-            team1_value += values[j][i];
-        }
-    }
+    for (int i = 0; i < n; i++) {
+        if (selected[i] != team) continue;
 
-    // 두번째 팀의 능력치 계산
-    int team0_value = 0;
-    for (int i = 1; i <= N; i++) {
-        if (visited[i] == 1) {
-            continue;
-        }
-        for (int j = i + 1; j <= N; j++) {
-            if (visited[j] == 1) {
-                continue;
-            }
-            team0_value += values[i][j];
-            team0_value += values[j][i];
+        for (int j = i + 1; j < n; j++) {
+            if (selected[j] != team) continue;
+
+            ret += board[i][j] + board[j][i];
         }
     }
 
-    // 두 팀의 능력치 차이를 return
-    return max(team1_value - team0_value, team0_value - team1_value);
+    return ret;
 }
 
-void dfs(int depth = 1, int pos = 1) {
-    if (depth == N / 2) {
-        result = min(result, calculate_diff());
-        // 답이 0보다 작은 경우는 없으므로, 0이 나온다면 다른 경우를 더 이상 탐색하지 않고 탐색을 종료한다.
-        if (result == 0) {
-            return;
-        }
+void recur(int depth, int idx_begin) {
+    if (depth == n / 2) {
+        mn = min(mn, abs(get_score(true) - get_score(false)));
+        return;
     }
-    else {
-        for (int i = pos + 1; i <= N; i++) {
-            visited[i] = 1;
-            dfs(depth + 1, i);
-            visited[i] = 0;
-        }
+
+    for (int i = idx_begin; i < n; i++) {
+        selected[i] = true;
+        recur(depth + 1, i + 1);
+        selected[i] = false;
     }
 }
 
 int main() {
-    ios_base::sync_with_stdio(false); // C++와 C 두 표준 입출력 동기화를 해제한다.
-    cout.tie(NULL);
-    cin.tie(NULL);                    // 입력과 출력이 묶여있는 것을 풀어준다.
+    FASTIO;
 
-    // 입력
-    cin >> N;
-    for (int i = 1; i <= N; i++) {
-        for (int j = 1; j <= N; j++) {
-            cin >> values[i][j];
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> board[i][j];
         }
     }
 
-    // 가능한 모든 경우를 탐색
-    visited[1] = 1;
-    dfs();
-
-    // 출력
-    cout << result << '\n';
-
+    selected[0] = true; // 0번 사람은 true 팀으로 고정
+    recur(1, 1);
+    cout << mn << '\n';
     return 0;
 }
