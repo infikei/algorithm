a74e0aa95f8f9d8a42a692a36a155e69c1c27795
홍창기 <hckhong@naver.com>
2025-11-28 01:00:05 +0900

    Update 2025-11-27

--- baekjoon_all/05000+/boj_5430.cpp
+++ baekjoon_all/05000+/boj_5430.cpp
@@ -1,21 +1,84 @@
 // Solve 2022-06-06
-// Update 2023-03-01
+// Update 2025-11-27
 
 #include <bits/stdc++.h>
-using namespace std;
 
-#ifdef BOJ
-#define BOJTEST(x) ((void)0)
-#else
-#define BOJTEST(x) cout << "[Debug] " << #x << ':' << x << '\n'
-#endif
-#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL); // boj_15552.cpp
-#define SETPRECISION(n) cout << fixed;cout.precision(n); // boj_1008.cpp
-#define SIZE(v) (int)v.size()
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);
 #define ALL(v) v.begin(),v.end()
+#define UNIQUE(v) v.erase(unique(v.begin(),v.end()),v.end());
+#define SETW(n, c) cout << setw(n) << setfill(c);
+#define SETP(n) cout << fixed << setprecision(n);
+
+using namespace std;
 using ll = long long;
 using uint = unsigned int;
 using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1000000007;
+
+void solution() {
+    string cmds, nums;
+    int n;
+    cin >> cmds >> n >> nums;
+
+    deque<int> dq;
+    string tmp;
+
+    for (char ch : nums) {
+        if (isdigit(ch)) {
+            tmp += ch;
+        }
+        else {
+            if (!tmp.empty()) {
+                dq.push_back(stoi(tmp));
+                tmp = "";
+            }
+        }
+    }
+
+    bool reversed = false;
+
+    for (char cmd : cmds) {
+        if (cmd == 'R') {
+            reversed = !reversed;
+            continue;
+        }
+
+        if (dq.empty()) {
+            cout << "error" << '\n';
+            return;
+        }
+
+        if (reversed) {
+            dq.pop_back();
+        }
+        else {
+            dq.pop_front();
+        }
+    }
+
+    cout << '[';
+
+    if (reversed) {
+        for (int i = size(dq) - 1; i >= 0; i--) {
+            cout << dq[i];
+
+            if (i > 0) cout << ',';
+        }
+    }
+    else {
+        for (int i = 0; i < size(dq); i++) {
+            cout << dq[i];
+
+            if (i < size(dq) - 1) cout << ',';
+        }
+    }
+
+    cout << "]\n";
+}
 
 int main() {
     FASTIO;
@@ -23,65 +86,8 @@ int main() {
     int t;
     cin >> t;
 
-    for (int ti = 0; ti < t; ti++) {
-        string cmds, nums;
-        int n;
-        cin >> cmds >> n >> nums;
-
-        deque<int> dq;
-        string tmp = "";
-        for (char ch : nums) {
-            if (ch >= '0' && ch <= '9') {
-                tmp += ch;
-            }
-            else {
-                if (!tmp.empty()) {
-                    dq.push_back(stoi(tmp));
-                    tmp = "";
-                }
-            }
-        }
-
-        bool error = false, reversed = false;
-        for (char cmd : cmds) {
-            if (cmd == 'D') {
-                if (dq.empty()) {
-                    error = true;
-                    break;
-                }
-                if (reversed) {
-                    dq.pop_back();
-                }
-                else {
-                    dq.pop_front();
-                }
-            }
-            else {
-                reversed = !reversed;
-            }
-        }
-
-        if (error) {
-            cout << "error\n";
-        }
-        else if (dq.empty()) {
-            cout << "[]\n";
-        }
-        else if (reversed) {
-            cout << '[' << dq.back();
-            for (int i = SIZE(dq) - 2; i >= 0; i--) {
-                cout << ',' << dq[i];
-            }
-            cout << "]\n";
-        }
-        else {
-            cout << '[' << dq[0];
-            int i_end = SIZE(dq);
-            for (int i = 1; i < i_end; i++) {
-                cout << ',' << dq[i];
-            }
-            cout << "]\n";
-        }
+    while (t-- > 0) {
+        solution();
     }
 
     return 0;
