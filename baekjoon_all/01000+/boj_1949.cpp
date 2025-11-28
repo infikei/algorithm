0621351b98eb045a436a5704a6748b0175dcf026
홍창기 <hckhong@naver.com>
2025-11-29 07:23:52 +0900

    Update 2025-11-28

--- baekjoon_all/01000+/boj_1949.cpp
+++ baekjoon_all/01000+/boj_1949.cpp
@@ -1,20 +1,13 @@
 // Solve 2024-10-14
+// Update 2025-11-28
 
 #include <bits/stdc++.h>
 
 #define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);
-#define size(v) (int)v.size()
-#define all(v) v.begin(),v.end()
-#define setw(n, c) cout << setw(n) << setfill(c);
-#define setp(n) cout << fixed << setprecision(n);
-#define printw(x) cout << (x) << ' ';
-#define println(x) cout << (x) << '\n';
-
-#ifdef BOJ
-#define BOJTEST(x) ((void)0)
-#else
-#define BOJTEST(x) cout << "[D] " << #x << ':' << x << '\n'
-#endif
+#define ALL(v) v.begin(),v.end()
+#define UNIQUE(v) v.erase(unique(v.begin(),v.end()),v.end());
+#define SETW(n, c) cout << setw(n) << setfill(c);
+#define SETP(n) cout << fixed << setprecision(n);
 
 using namespace std;
 using ll = long long;
@@ -22,25 +15,26 @@ using uint = unsigned int;
 using ull = unsigned long long;
 using ld = long double;
 using pii = pair<int, int>;
-
-const double PI = M_PI;
+using pll = pair<ll, ll>;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1000000007;
 
 int people[10001];
 vector<int> adj[10001];
 
 pii dfs(int cur, int par) {
-    pii res = { 0, people[cur] };
+    int o = people[cur];
+    int x = 0;
 
     for (int nxt : adj[cur]) {
         if (nxt == par) continue;
 
-        pii nxt_res = dfs(nxt, cur);
-
-        res.first += max(nxt_res.first, nxt_res.second);
-        res.second += nxt_res.first;
+        auto [nxt_o, nxt_x] = dfs(nxt, cur);
+        o += nxt_x;
+        x += max(nxt_o, nxt_x);
     }
 
-    return res;
+    return {o, x};
 }
 
 int main() {
@@ -56,14 +50,11 @@ int main() {
     for (int i = 1; i < n; i++) {
         int u, v;
         cin >> u >> v;
-
         adj[u].push_back(v);
         adj[v].push_back(u);
     }
 
-    pii ans = dfs(1, 0);
-
-    println(max(ans.first, ans.second));
-
+    auto [o, x] = dfs(1, 0);
+    cout << max(o, x) << '\n';
     return 0;
 }
--- baekjoon_all/01000+/boj_1949.java
+++ baekjoon_all/01000+/boj_1949.java
@@ -1,7 +1,5 @@
 // Solve 2024-02-04
-// Update 2024-10-14
-
-// 백준에 제출할 때는 class 이름을 Main으로 설정해야 한다.
+// Update 2025-11-28
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -12,18 +10,17 @@ import java.util.StringTokenizer;
 public class boj_1949 {
 
     static int[] people;
-    static ArrayList<Integer>[] adj;
+    static ArrayList<ArrayList<Integer>> adj;
 
     static int[] dfs(int cur, int par) {
-        int[] res = new int[2];
-        res[1] = people[cur];
+        int[] res = new int[] {people[cur], 0};
 
-        for (int nxt : adj[cur]) {
+        for (int nxt : adj.get(cur)) {
             if (nxt == par) continue;
 
             int[] nxt_res = dfs(nxt, cur);
-            res[0] += Math.max(nxt_res[0], nxt_res[1]);
-            res[1] += nxt_res[0];
+            res[0] += nxt_res[1];
+            res[1] += Math.max(nxt_res[0], nxt_res[1]);
         }
 
         return res;
@@ -32,33 +29,29 @@ public class boj_1949 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
+        people = new int[n + 1];
+        adj = new ArrayList<>();
+
+        for (int i = 0; i <= n; i++) {
+            adj.add(new ArrayList<>());
+        }
 
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        people = new int[n + 1];
 
         for (int u = 1; u <= n; u++) {
             people[u] = Integer.parseInt(st.nextToken());
-        }
-
-        adj = new ArrayList[n + 1];
-
-        for (int u = 1; u <= n; u++) {
-            adj[u] = new ArrayList<Integer>();
         }
 
         for (int i = 1; i < n; i++) {
             st = new StringTokenizer(br.readLine(), " ");
             int u = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
-
-            adj[u].add(v);
-            adj[v].add(u);
+            adj.get(u).add(v);
+            adj.get(v).add(u);
         }
 
         int[] ans = dfs(1, 0);
-
         System.out.println(Math.max(ans[0], ans[1]));
-        br.close();
     }
 
 }
