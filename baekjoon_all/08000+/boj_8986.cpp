27c84f19586005b871f5d01037a60ab5a284883a
홍창기 <hckhong@naver.com>
2025-12-09 02:21:24 +0900

    Update 2025-12-07

--- baekjoon_all/08000+/boj_8986.cpp
+++ baekjoon_all/08000+/boj_8986.cpp
@@ -1,28 +1,35 @@
 // Solve 2023-03-16
-// Update 2024-05-04
+// Update 2025-12-07
 
 #include <bits/stdc++.h>
-using namespace std;
 
-#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL); // boj_15552.cpp
-#define SETPRECISION(n) cout << fixed;cout.precision(n); // boj_1008.cpp
-#define SIZE(v) (int)v.size()
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);
 #define ALL(v) v.begin(),v.end()
+#define UNIQUE(v) v.erase(unique(v.begin(),v.end()),v.end());
+#define SETW(n, c) cout << setw(n) << setfill(c);
+#define SETP(n) cout << fixed << setprecision(n);
+
+using namespace std;
 using ll = long long;
+using uint = unsigned int;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1000000007;
 
 int n;
 ll x[100000];
 
-inline ll calc_val(ll k) {
-    ll res = 0;
-    ll cur = 0;
+ll get_val(ll gap) {
+    ll ret = 0;
 
     for (int i = 0; i < n; i++) {
-        res += abs(x[i] - cur);
-        cur += k;
+        ret += abs(x[i] - i * gap);
     }
 
-    return res;
+    return ret;
 }
 
 int main() {
@@ -38,11 +45,11 @@ int main() {
     ll high = 1e9;
 
     while (low + 2 < high) {
-        ll mid1 = ((low << 1) + high) / 3;
-        ll mid2 = (low + (high << 1)) / 3;
+        ll mid1 = (low * 2 + high) / 3;
+        ll mid2 = (low + high * 2) / 3;
 
-        ll mid1_val = calc_val(mid1);
-        ll mid2_val = calc_val(mid2);
+        ll mid1_val = get_val(mid1);
+        ll mid2_val = get_val(mid2);
 
         if (mid1_val < mid2_val) {
             high = mid2;
@@ -52,13 +59,12 @@ int main() {
         }
     }
 
-    ll ans = 1e18;
+    ll mn = 1e18;
 
-    for (int k = low; k <= high; k++) {
-        ans = min(ans, calc_val(k));
+    for (int i = low; i <= high; i++) {
+        mn = min(mn, get_val(i));
     }
 
-    cout << ans << '\n';
-
+    cout << mn << '\n';
     return 0;
 }
