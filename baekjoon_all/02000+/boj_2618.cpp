e29424be033eea8e1589eb40dc06366dd8e80007
홍창기 <hckhong@naver.com>
2025-12-21 01:39:46 +0900

    Update 2025-12-17

--- baekjoon_all/02000+/boj_2618.cpp
+++ baekjoon_all/02000+/boj_2618.cpp
@@ -1,69 +1,62 @@
 // Solve 2023-02-10
+// Update 2025-12-17
 
 #include <bits/stdc++.h>
-using namespace std;
 
-#ifdef BOJ
-#define BOJTEST(x) ((void)0)
-#else
-#define BOJTEST(x) cout << "[Debug] " << #x << ':' << x << '\n'
-#endif
-#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL); // boj_15552.cpp
-#define SIZE(v) (int)v.size()
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);
 #define ALL(v) v.begin(),v.end()
-#define INF (int)1e9
-#define LLINF (ll)4e18
+#define UNIQUE(v) v.erase(unique(v.begin(),v.end()),v.end());
+#define SETW(n, c) cout << setw(n) << setfill(c);
+#define SETP(n) cout << fixed << setprecision(n);
+
+using namespace std;
 using ll = long long;
 using uint = unsigned int;
 using ull = unsigned long long;
+using ld = long double;
 using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1000000007;
 
 int n, w;
-pii events[1001];
-int total_dist[1001][1001];
+pii points[1001];
+int memo[1001][1001];
 
-int calc_dist(pii pt1, pii pt2) {
-    return abs(pt1.first - pt2.first) + abs(pt1.second - pt2.second);
+int get_dist(pii a, pii b) {
+    return abs(a.first - b.first) + abs(a.second - b.second);
 }
 
-int calc_total_dist(int event1, int event2) {
-    if (event1 == w || event2 == w) return 0;
-    if (total_dist[event1][event2] != -1) return total_dist[event1][event2];
+int recur(int a, int b) {
+    int cur = max(a, b) + 1;
 
-    int next_event = max(event1, event2) + 1;
+    if (cur > w) return 0;
+    if (memo[a][b] != -1) return memo[a][b];
 
-    int res1 = calc_total_dist(next_event, event2);
-    if (event1 == 0) res1 += calc_dist({ 1, 1 }, events[next_event]);
-    else res1 += calc_dist(events[event1], events[next_event]);
+    int x = recur(cur, b);
+    x += get_dist(points[cur], (a == 0 ? pii(1, 1) : points[a]));
 
-    int res2 = calc_total_dist(event1, next_event);
-    if (event2 == 0) res2 += calc_dist({ n, n }, events[next_event]);
-    else res2 += calc_dist(events[event2], events[next_event]);
+    int y = recur(a, cur);
+    y += get_dist(points[cur], (b == 0 ? pii(n, n) : points[b]));
 
-    total_dist[event1][event2] = min(res1, res2);
-    return total_dist[event1][event2];
+    return memo[a][b] = min(x, y);
 }
 
-void find_path(int event1, int event2) {
-    if (event1 == w || event2 == w) return;
+void find_path(int a, int b) {
+    int cur = max(a, b) + 1;
 
-    int next_event = max(event1, event2) + 1;
+    if (cur > w) return;
 
-    int res1 = total_dist[next_event][event2];
-    if (event1 == 0) res1 += calc_dist({ 1, 1 }, events[next_event]);
-    else res1 += calc_dist(events[event1], events[next_event]);
+    int x = recur(cur, b);
+    x += get_dist(points[cur], (a == 0 ? pii(1, 1) : points[a]));
 
-    int res2 = total_dist[event1][next_event];
-    if (event2 == 0) res2 += calc_dist({ n, n }, events[next_event]);
-    else res2 += calc_dist(events[event2], events[next_event]);
-
-    if (res1 < res2) {
+    if (memo[a][b] == x) {
         cout << 1 << '\n';
-        find_path(next_event, event2);
+        find_path(cur, b);
     }
     else {
         cout << 2 << '\n';
-        find_path(event1, next_event);
+        find_path(a, cur);
     }
 }
 
@@ -71,21 +64,15 @@ int main() {
     FASTIO;
 
     cin >> n >> w;
+
     for (int i = 1; i <= w; i++) {
         int x, y;
         cin >> x >> y;
-        events[i] = { x, y };
+        points[i] = {x, y};
     }
 
-    for (int i = 0; i <= w; i++) {
-        for (int j = 0; j <= w; j++) {
-            total_dist[i][j] = -1;
-        }
-    }
-
-    cout << calc_total_dist(0, 0) << '\n';
-
+    memset(memo, -1, sizeof memo);
+    cout << recur(0, 0) << '\n';
     find_path(0, 0);
-
     return 0;
 }
