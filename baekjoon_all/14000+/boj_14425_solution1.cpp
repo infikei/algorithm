c1b59a279e42734390f4bd1b4935b326d68c47d7
홍창기 <hckhong@naver.com>
2026-02-16 15:13:37 +0900

    Update 2026-02-15

--- baekjoon_all/14000+/boj_14425_solution1.cpp
+++ baekjoon_all/14000+/boj_14425_solution1.cpp
@@ -1,42 +1,48 @@
 // Solve 2022-06-03
-// Update 2023-10-09
+// Update 2026-02-15
 
 #include <bits/stdc++.h>
-using namespace std;
 
-#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL); // boj_15552.cpp
-#define SETPRECISION(n) cout << fixed;cout.precision(n); // boj_1008.cpp
-#define SIZE(v) (int)v.size()
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);
 #define ALL(v) v.begin(),v.end()
+#define UNIQUE(v) v.erase(unique(v.begin(),v.end()),v.end());
+#define SETW(n, c) cout << setw(n) << setfill(c);
+#define SETP(n) cout << fixed << setprecision(n);
+
+using namespace std;
 using ll = long long;
+using uint = unsigned int;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1000000007;
 
 int main() {
     FASTIO;
 
     int n, m;
     cin >> n >> m;
-
-    unordered_set<string> str_set;
+    unordered_set<string> words_set;
 
     for (int i = 0; i < n; i++) {
-        string str;
-        cin >> str;
-
-        str_set.insert(str);
+        string w;
+        cin >> w;
+        words_set.insert(w);
     }
 
-    int ans = 0;
+    int cnt = 0;
 
     for (int i = 0; i < m; i++) {
-        string str;
-        cin >> str;
+        string w;
+        cin >> w;
 
-        if (str_set.find(str) != str_set.end()) {
-            ans++;
+        if (words_set.find(w) != words_set.end()) {
+            cnt++;
         }
     }
 
-    cout << ans << '\n';
-
+    cout << cnt << '\n';
     return 0;
 }
--- baekjoon_all/14000+/boj_14425_solution2.cpp
+++ baekjoon_all/14000+/boj_14425_solution2.cpp
@@ -1,14 +1,25 @@
 // Solve 2022-06-03
-// Update 2023-10-09
+// Update 2026-02-15
 
 #include <bits/stdc++.h>
-using namespace std;
 
-#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL); // boj_15552.cpp
-#define SETPRECISION(n) cout << fixed;cout.precision(n); // boj_1008.cpp
-#define SIZE(v) (int)v.size()
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);
 #define ALL(v) v.begin(),v.end()
+#define UNIQUE(v) v.erase(unique(v.begin(),v.end()),v.end());
+#define SETW(n, c) cout << setw(n) << setfill(c);
+#define SETP(n) cout << fixed << setprecision(n);
+
+using namespace std;
 using ll = long long;
+using uint = unsigned int;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1000000007;
+
+string words[10000];
 
 int main() {
     FASTIO;
@@ -16,26 +27,22 @@ int main() {
     int n, m;
     cin >> n >> m;
 
-    string str_list[10000];
-
     for (int i = 0; i < n; i++) {
-        cin >> str_list[i];
+        cin >> words[i];
     }
 
-    sort(str_list, str_list + n);
-
-    int ans = 0;
+    sort(words, words + n);
+    int cnt = 0;
 
     for (int i = 0; i < m; i++) {
         string str;
         cin >> str;
 
-        if (binary_search(str_list, str_list + n, str)) {
-            ans++;
+        if (binary_search(words, words + n, str)) {
+            cnt++;
         }
     }
 
-    cout << ans << '\n';
-
+    cout << cnt << '\n';
     return 0;
 }
