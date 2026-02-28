2f556a1d6437344fe7e137001c2caa38d1a1adda
홍창기 <hckhong@naver.com>
2026-02-28 22:59:12 +0900

    Solve 2026-02-27

---
+++ baekjoon_all/24000+/boj_24937.cpp
@@ -0,0 +1,36 @@
+// Solve 2026-02-27
+
+#include <bits/stdc++.h>
+
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define ALL(v) v.begin(),v.end()
+#define UNIQUE(v) v.erase(unique(v.begin(),v.end()),v.end());
+#define SETW(n, c) cout << setw(n) << setfill(c);
+#define SETP(n) cout << fixed << setprecision(n);
+
+using namespace std;
+using ll = long long;
+using uint = unsigned int;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1000000007;
+
+int main() {
+    FASTIO;
+
+    int n;
+    cin >> n;
+    n %= 10;
+
+    string s = "SciComLove";
+
+    for (int i = n; i < n + 10; i++) {
+        cout << s[i % 10];
+    }
+
+    cout << '\n';
+    return 0;
+}
