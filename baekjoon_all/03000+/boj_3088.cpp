22de962bf16fc195b43568232d6aaf72bf28d9dc
홍창기 <hckhong@naver.com>
2026-03-04 23:45:19 +0900

    Solve 2026-03-02

---
+++ baekjoon_all/03000+/boj_3088.cpp
@@ -0,0 +1,45 @@
+// Solve 2026-03-02
+
+#include <bits/stdc++.h>
+
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define ALL(v) v.begin(),v.end()
+#define UNIQUE(v) v.erase(unique(v.begin(),v.end()),v.end());
+#define SETW(n, c) cout << setw(n) << setfill(c);
+#define SETP(n) cout << fixed << setprecision(n);
+
+using namespace std;
+using ll = long long;
+using uint = unsigned int;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1000000007;
+
+int visited[1000001];
+
+int main() {
+    FASTIO;
+
+    int n;
+    cin >> n;
+    int cnt = 0;
+
+    for (int i = 0; i < n; i++) {
+        int a, b, c;
+        cin >> a >> b >> c;
+
+        if (!visited[a] && !visited[b] && !visited[c]) {
+            cnt++;
+        }
+
+        visited[a] = true;
+        visited[b] = true;
+        visited[c] = true;
+    }
+
+    cout << cnt << '\n';
+    return 0;
+}
