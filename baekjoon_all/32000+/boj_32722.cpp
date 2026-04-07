1ffd45f76aadc34a9af062d9fcc1a704ff4606f1
홍창기 <hckhong@naver.com>
2026-04-08 01:29:48 +0900

    Solve 2026-03-27

---
+++ baekjoon_all/32000+/boj_32722.cpp
@@ -0,0 +1,42 @@
+// Solve 2026-03-27
+
+#include <bits/stdc++.h>
+
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define ALL(v) v.begin(),v.end()
+#define UNIQUE(v) v.erase(unique(v.begin(),v.end()),v.end());
+#define SETW(n, c) cout << setw(n) << setfill(c);
+#define SETP(n) cout << fixed << setprecision(n);
+
+using namespace std;
+using ll = long long;
+using uint = unsigned int;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1000000007;
+
+int main() {
+    FASTIO;
+
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    if (a != 1 && a != 3) {
+        cout << "EI" << '\n';
+        return 0;
+    }
+    if (b != 6 && b != 8) {
+        cout << "EI" << '\n';
+        return 0;
+    }
+    if (c != 2 && c != 5) {
+        cout << "EI" << '\n';
+        return 0;
+    }
+
+    cout << "JAH" << '\n';
+    return 0;
+}
