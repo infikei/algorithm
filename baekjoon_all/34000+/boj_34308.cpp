fcf0c128063cf09dbb521af4021d18ef252dc214
홍창기 <hckhong@naver.com>
2026-04-24 23:37:13 +0900

    Solve 2026-04-17

---
+++ baekjoon_all/34000+/boj_34308.cpp
@@ -0,0 +1,41 @@
+// Solve 2026-04-17
+
+#include <bits/stdc++.h>
+
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define ALL(v) v.begin(),v.end()
+#define UNIQUE(v) v.erase(unique(v.begin(),v.end()),v.end());
+#define SETW(n, c) cout << setw(n) << setfill(c);
+#define SETP(n) cout << fixed << setprecision(n);
+
+using namespace std;
+using ll = long long;
+using uint = unsigned int;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1000000007;
+
+int main() {
+    FASTIO;
+
+    int n, k;
+    cin >> n >> k;
+
+    for (int i = 0; i < k; i++) {
+        int a;
+        cin >> a;
+
+        if (n - a < a - 1) {
+            cout << n << ' ';
+        }
+        else {
+            cout << 1 << ' ';
+        }
+    }
+
+    cout << '\n';
+    return 0;
+}
